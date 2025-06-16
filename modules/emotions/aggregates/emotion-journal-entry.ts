b17441ff1c593a7bf77b4d2b884db674647ccbc2
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-16 21:58:57 +0200

    feature: setup emotion journal entry aggregate

---
+++ modules/emotions/aggregates/emotion-journal-entry.ts
@@ -0,0 +1,79 @@
+import * as Entities from "../entities";
+import * as VO from "../value-objects";
+
+type SituationLogged = {
+  type: "situation.logged";
+  situation: {
+    id: VO.EmotionJournalEntryIdType;
+    description: VO.SituationDescriptionType;
+    location: VO.SituationLocationType;
+    kind: VO.SituationKindType;
+  };
+};
+
+type JournalEntryEventType = SituationLogged;
+
+export class EmotionJournalEntry {
+  private readonly id: VO.EmotionJournalEntryIdType;
+  private situation?: Entities.Situation;
+
+  private readonly pending: JournalEntryEventType[] = [];
+
+  private constructor(id: VO.EmotionJournalEntryIdType) {
+    this.id = id;
+  }
+
+  static build(
+    id: VO.EmotionJournalEntryIdType,
+    events: JournalEntryEventType[],
+  ): EmotionJournalEntry {
+    const entry = new EmotionJournalEntry(id);
+
+    events.forEach((event) => entry.apply(event));
+
+    return entry;
+  }
+
+  logSituation(situation: Entities.Situation) {
+    if (this.situation) {
+      throw new Error("Situation already logged for this entry.");
+    }
+
+    const SituationLoggedEvent: SituationLogged = {
+      type: "situation.logged",
+      situation: {
+        id: this.id,
+        description: situation.description.get(),
+        location: situation.location.get(),
+        kind: situation.kind.get(),
+      },
+    };
+
+    this.record(SituationLoggedEvent);
+  }
+
+  pullEvents(): JournalEntryEventType[] {
+    const events = [...this.pending];
+
+    this.pending.length = 0;
+
+    return events;
+  }
+
+  private record(event: JournalEntryEventType): void {
+    this.pending.push(event);
+    this.apply(event);
+  }
+
+  private apply(event: JournalEntryEventType): void {
+    switch (event.type) {
+      case "situation.logged": {
+        this.situation = new Entities.Situation(
+          new VO.SituationDescription(event.situation.description),
+          new VO.SituationLocation(event.situation.location),
+          new VO.SituationKind(event.situation.kind),
+        );
+      }
+    }
+  }
+}
--- modules/emotions/index.ts
+++ modules/emotions/index.ts
@@ -1,2 +1,3 @@
 export * as VO from "./value-objects";
 export * as Entities from "./entities";
+export * as Aggregates from "./aggregates";
