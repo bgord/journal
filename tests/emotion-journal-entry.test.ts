896b30d992597bb12dad65fcb6fd43a97cc2f45a
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-16 23:28:32 +0200

    step: improve tests

--- tests/emotion-journal-entry.test.ts
+++ tests/emotion-journal-entry.test.ts
@@ -3,166 +3,95 @@ import * as bg from "@bgord/bun";
 
 import * as Emotions from "../modules/emotions";
 
+const situation = new Emotions.Entities.Situation(
+  new Emotions.VO.SituationDescription("I finished a project"),
+  new Emotions.VO.SituationLocation("work"),
+  new Emotions.VO.SituationKind(Emotions.VO.SituationKindOptions.achievement),
+);
+
+const emotion = new Emotions.Entities.Emotion(
+  new Emotions.VO.EmotionLabel(Emotions.VO.GenevaWheelEmotion.gratitude),
+  new Emotions.VO.EmotionIntensity(3),
+);
+
+const id = bg.NewUUID.generate();
+
+const SituationLoggedEvent = {
+  type: "situation.logged",
+  id,
+  situation: {
+    description: "I finished a project",
+    kind: Emotions.VO.SituationKindOptions.achievement,
+    location: "work",
+  },
+} as const;
+
+const EmotionLoggedEvent = {
+  type: "emotion.logged",
+  id,
+  emotion: {
+    label: Emotions.VO.GenevaWheelEmotion.gratitude,
+    intensity: 3,
+  },
+} as const;
+
 describe("EmotionJournalEntry", () => {
   test("build new aggregate", () => {
-    const id = bg.NewUUID.generate();
-
     const emotionJournalEntry = Emotions.Aggregates.EmotionJournalEntry.build(id, []);
 
     expect(emotionJournalEntry.pullEvents()).toEqual([]);
   });
 
   test("logSituation - correct path", async () => {
-    const id = bg.NewUUID.generate();
-
-    const situation = new Emotions.Entities.Situation(
-      new Emotions.VO.SituationDescription("I finished a project"),
-      new Emotions.VO.SituationLocation("work"),
-      new Emotions.VO.SituationKind(Emotions.VO.SituationKindOptions.achievement),
-    );
-
     const emotionJournalEntry = Emotions.Aggregates.EmotionJournalEntry.build(id, []);
 
     await emotionJournalEntry.logSituation(situation);
 
-    expect(emotionJournalEntry.pullEvents()).toEqual([
-      {
-        type: "situation.logged",
-        id,
-        situation: {
-          description: "I finished a project",
-          kind: Emotions.VO.SituationKindOptions.achievement,
-          location: "work",
-        },
-      },
-    ]);
+    expect(emotionJournalEntry.pullEvents()).toEqual([SituationLoggedEvent]);
   });
 
-  test("logSituation - applied only once", async () => {
-    const id = bg.NewUUID.generate();
-
-    const situation = new Emotions.Entities.Situation(
-      new Emotions.VO.SituationDescription("I finished a project"),
-      new Emotions.VO.SituationLocation("work"),
-      new Emotions.VO.SituationKind(Emotions.VO.SituationKindOptions.achievement),
-    );
-
+  test("logSituation - Policies.OneSituationPerEmotionJournalEntry", async () => {
     const emotionJournalEntry = Emotions.Aggregates.EmotionJournalEntry.build(id, []);
 
     await emotionJournalEntry.logSituation(situation);
 
-    expect(emotionJournalEntry.pullEvents()).toEqual([
-      {
-        type: "situation.logged",
-        id,
-        situation: {
-          description: "I finished a project",
-          kind: Emotions.VO.SituationKindOptions.achievement,
-          location: "work",
-        },
-      },
-    ]);
+    expect(emotionJournalEntry.pullEvents()).toEqual([SituationLoggedEvent]);
 
     expect(async () => emotionJournalEntry.logSituation(situation)).toThrow(
       Emotions.Policies.OneSituationPerEmotionJournalEntry.error,
     );
+
+    expect(emotionJournalEntry.pullEvents()).toEqual([]);
   });
 
   test("logEmotion - correct path", async () => {
-    const id = bg.NewUUID.generate();
-
-    const situation = new Emotions.Entities.Situation(
-      new Emotions.VO.SituationDescription("I finished a project"),
-      new Emotions.VO.SituationLocation("work"),
-      new Emotions.VO.SituationKind(Emotions.VO.SituationKindOptions.achievement),
-    );
-
     const emotionJournalEntry = Emotions.Aggregates.EmotionJournalEntry.build(id, []);
 
     await emotionJournalEntry.logSituation(situation);
 
-    const emotion = new Emotions.Entities.Emotion(
-      new Emotions.VO.EmotionLabel(Emotions.VO.GenevaWheelEmotion.gratitude),
-      new Emotions.VO.EmotionIntensity(3),
-    );
-
     await emotionJournalEntry.logEmotion(emotion);
 
-    expect(emotionJournalEntry.pullEvents()).toEqual([
-      {
-        type: "situation.logged",
-        id,
-        situation: {
-          description: "I finished a project",
-          kind: Emotions.VO.SituationKindOptions.achievement,
-          location: "work",
-        },
-      },
-      {
-        type: "emotion.logged",
-        id,
-        emotion: {
-          label: Emotions.VO.GenevaWheelEmotion.gratitude,
-          intensity: 3,
-        },
-      },
-    ]);
+    expect(emotionJournalEntry.pullEvents()).toEqual([SituationLoggedEvent, EmotionLoggedEvent]);
   });
 
-  test("logEmotion - applied only once", async () => {
-    const id = bg.NewUUID.generate();
-
-    const situation = new Emotions.Entities.Situation(
-      new Emotions.VO.SituationDescription("I finished a project"),
-      new Emotions.VO.SituationLocation("work"),
-      new Emotions.VO.SituationKind(Emotions.VO.SituationKindOptions.achievement),
-    );
-
+  test("logEmotion - Policies.OneEmotionPerEmotionJournalEntry", async () => {
     const emotionJournalEntry = Emotions.Aggregates.EmotionJournalEntry.build(id, []);
 
     await emotionJournalEntry.logSituation(situation);
 
-    const emotion = new Emotions.Entities.Emotion(
-      new Emotions.VO.EmotionLabel(Emotions.VO.GenevaWheelEmotion.gratitude),
-      new Emotions.VO.EmotionIntensity(3),
-    );
-
     await emotionJournalEntry.logEmotion(emotion);
 
-    expect(emotionJournalEntry.pullEvents()).toEqual([
-      {
-        type: "situation.logged",
-        id,
-        situation: {
-          description: "I finished a project",
-          kind: Emotions.VO.SituationKindOptions.achievement,
-          location: "work",
-        },
-      },
-      {
-        type: "emotion.logged",
-        id,
-        emotion: {
-          label: Emotions.VO.GenevaWheelEmotion.gratitude,
-          intensity: 3,
-        },
-      },
-    ]);
+    expect(emotionJournalEntry.pullEvents()).toEqual([SituationLoggedEvent, EmotionLoggedEvent]);
 
     expect(async () => emotionJournalEntry.logEmotion(emotion)).toThrow(
       Emotions.Policies.OneEmotionPerEmotionJournalEntry.error,
     );
+
+    expect(emotionJournalEntry.pullEvents()).toEqual([]);
   });
 
-  test("logEmotion - emotion corresponds to situation", async () => {
-    const id = bg.NewUUID.generate();
-
+  test("logEmotion - Policies.EmotionCorrespondsToSituation", async () => {
     const emotionJournalEntry = Emotions.Aggregates.EmotionJournalEntry.build(id, []);
-
-    const emotion = new Emotions.Entities.Emotion(
-      new Emotions.VO.EmotionLabel(Emotions.VO.GenevaWheelEmotion.gratitude),
-      new Emotions.VO.EmotionIntensity(3),
-    );
 
     expect(async () => emotionJournalEntry.logEmotion(emotion)).toThrow(
       Emotions.Policies.EmotionCorrespondsToSituation.error,
