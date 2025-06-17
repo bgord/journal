9672b72e6513b503c593a4684402415147951158
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-17 18:30:24 +0200

    step: setup pattern detector

--- modules/emotions/index.ts
+++ modules/emotions/index.ts
@@ -2,3 +2,4 @@ export * as VO from "./value-objects";
 export * as Entities from "./entities";
 export * as Aggregates from "./aggregates";
 export * as Policies from "./policies";
+export * as Services from "./services";
