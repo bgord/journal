ff20a33630372e29e797ac0688991ed7b905be81
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-17 19:37:35 +0200

    feature: implement MultipleMaladaptiveReactionsInWeekPattern-n

--- modules/emotions/services/pattern-detector.ts
+++ modules/emotions/services/pattern-detector.ts
@@ -1,1 +1,31 @@
-export class PatternDetector {}
+import { EmotionJournalEntry } from "../aggregates/emotion-journal-entry";
+
+type PatternName = string;
+
+type DetectedResult = { detected: true; name: PatternName };
+
+type UndetectedResult = { detected: false; name: PatternName };
+
+abstract class Pattern {
+  abstract name: PatternName;
+
+  abstract check(entries: EmotionJournalEntry[]): DetectedResult | UndetectedResult;
+}
+
+export class MultipleMaladaptiveReactionsInWeekPattern extends Pattern {
+  name = "MultipleMaladaptiveReactionsInWeekPattern";
+
+  check(entries: EmotionJournalEntry[]): DetectedResult | UndetectedResult {
+    const detected =
+      entries.map((entry) => entry.summarize()).filter((entry) => entry.reaction?.type.isMaladaptive())
+        .length >= 3;
+
+    return { detected, name: this.name };
+  }
+}
+
+export class PatternDetector {
+  static detect(entries: EmotionJournalEntry[], patterns: Pattern[]): DetectedResult[] {
+    return patterns.map((rule) => rule.check(entries)).filter((result) => result.detected);
+  }
+}
---
+++ tests/multiple-maladaptive-reactions-in-week-pattern.test.ts
@@ -0,0 +1,85 @@
+import { describe, expect, test } from "bun:test";
+import * as bg from "@bgord/bun";
+
+import * as Emotions from "../modules/emotions";
+
+const id = bg.NewUUID.generate();
+
+const MaladaptiveSituationLoggedEvent = {
+  id: expect.any(String),
+  createdAt: expect.any(Number),
+  name: Emotions.Aggregates.SITUATION_LOGGED_EVENT,
+  stream: Emotions.Aggregates.EmotionJournalEntry.getStream(id),
+  version: 1,
+  payload: {
+    id,
+    description: "I lost a game",
+    kind: Emotions.VO.SituationKindOptions.failure,
+    location: "pitch",
+  },
+} satisfies Emotions.Aggregates.SituationLoggedEventType;
+
+const MaladaptiveEmotionLoggedEvent = {
+  id: expect.any(String),
+  createdAt: expect.any(Number),
+  name: Emotions.Aggregates.EMOTION_LOGGED_EVENT,
+  stream: Emotions.Aggregates.EmotionJournalEntry.getStream(id),
+  version: 1,
+  payload: {
+    id,
+    label: Emotions.VO.GenevaWheelEmotion.anger,
+    intensity: 4,
+  },
+} satisfies Emotions.Aggregates.EmotionLoggedEventType;
+
+const MaladaptiveReactionLoggedEvent = {
+  id: expect.any(String),
+  createdAt: expect.any(Number),
+  name: Emotions.Aggregates.REACTION_LOGGED_EVENT,
+  stream: Emotions.Aggregates.EmotionJournalEntry.getStream(id),
+  version: 1,
+  payload: {
+    id,
+    description: "Got drunk",
+    type: Emotions.VO.GrossEmotionRegulationStrategy.avoidance,
+    effectiveness: 1,
+  },
+} satisfies Emotions.Aggregates.ReactionLoggedEventType;
+
+const maladaptiveJournalEntry = Emotions.Aggregates.EmotionJournalEntry.build(
+  id,
+  [
+    MaladaptiveSituationLoggedEvent,
+    MaladaptiveEmotionLoggedEvent,
+    MaladaptiveReactionLoggedEvent,
+  ],
+);
+
+describe("MultipleMaladaptiveReactionsInWeekPattern", () => {
+  test("true", () => {
+    const result = Emotions.Services.PatternDetector.detect(
+      [
+        maladaptiveJournalEntry,
+        maladaptiveJournalEntry,
+        maladaptiveJournalEntry,
+      ],
+      [new Emotions.Services.MultipleMaladaptiveReactionsInWeekPattern()],
+    );
+
+    expect(result).toEqual([
+      {
+        detected: true,
+        name: Emotions.Services.MultipleMaladaptiveReactionsInWeekPattern.name,
+      },
+    ]);
+  });
+
+  test("false", () => {
+    const result = Emotions.Services.PatternDetector.detect(
+      [maladaptiveJournalEntry, maladaptiveJournalEntry],
+      [new Emotions.Services.MultipleMaladaptiveReactionsInWeekPattern()],
+    );
+
+    expect(result).toEqual([]);
+  });
+});
