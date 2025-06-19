599fd6dcc14dd7f0c333567e79d1d5835e372703
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-19 13:12:15 +0200

    refactor: remove drizzle migrations

--- infra/drizzle/0000_opposite_serpent_society.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `event` (
-	`id` text PRIMARY KEY DEFAULT uuid() NOT NULL,
-	`createdAt` text DEFAULT CURRENT_TIMESTAMP,
-	`name` text NOT NULL,
-	`stream` text NOT NULL,
-	`version` integer NOT NULL,
-	`payload` text NOT NULL
-);
