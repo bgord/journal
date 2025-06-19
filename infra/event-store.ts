eee96c641230c2c5a637cd4e81ddfd944e9d293a
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-19 14:47:53 +0200

    feature: implement EventStore.save

--- infra/event-store.ts
+++ infra/event-store.ts
@@ -1,7 +1,20 @@
 import { JournalEntryEventType } from "../modules/emotions/aggregates/emotion-journal-entry";
+import { db } from "./db";
+import * as schema from "./schema";
+
+type AcceptedEventType = JournalEntryEventType;
 
 export class EventStore {
   static async find(_stream: string): Promise<JournalEntryEventType[]> {
     return [];
+  }
+
+  static async save(events: AcceptedEventType[]) {
+    await db.insert(schema.events).values(
+      events.map((event) => ({
+        ...event,
+        payload: JSON.stringify(event.payload),
+      })),
+    );
   }
 }
