694ee51c7ce1f68571c39fc539656c8a51343c2e
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-19 14:09:29 +0200

    feature: define drizzle.config.ts

---
+++ drizzle.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  dialect: "sqlite",
+  schema: "./infra/schema.ts",
+  out: "./infra/drizzle",
+});
--- scripts/bun-drizzle-generate.sh
+++ scripts/bun-drizzle-generate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-bunx drizzle-kit generate --dialect sqlite --schema ./infra/schema.ts --out infra/drizzle
+bunx drizzle-kit generate --config drizzle.config.ts
