fb574e0aa1e78a9ae29f2955fb55dcbb116c30a9
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-19 15:41:18 +0200

    step: delete helper files

--- test.ts
+++
@@ -1,9 +0,0 @@
-import { db } from "./infra/db";
-import * as schema from "./infra/schema";
-
-async function main() {
-  const result = await db.select().from(schema.events);
-  console.log(result);
-}
-
-main();
