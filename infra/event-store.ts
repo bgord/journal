d19ed5df5b5c8f067af91f7a0b994f9ee34ee896
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-21 17:01:11 +0200

    step: use imported dispatching event store

--- infra/event-store.ts
+++ infra/event-store.ts
@@ -5,27 +5,20 @@ import { z } from "zod/v4";
 import { JournalEntryEvent } from "../modules/emotions/aggregates/emotion-journal-entry";
 import { PatternDetectionEvent } from "../modules/emotions/services/patterns/pattern";
 import { db } from "./db";
-import { EventBus } from "./event-bus";
+import { EventBus, EventMap } from "./event-bus";
 import * as schema from "./schema";
 
 export type AcceptedEvent = JournalEntryEvent | PatternDetectionEvent;
 
-class DispatchingEventStore extends bg.EventStore<AcceptedEvent> {
-  async save(events: z.infer<AcceptedEvent>[]) {
-    await super.save(events);
-
-    for (const event of events) {
-      EventBus.emit(event.name, event);
-    }
-  }
-}
-
-export const EventStore = new DispatchingEventStore({
-  finder: (stream: bg.EventStreamType, acceptedEventsNames: bg.EventNameType[]) =>
-    db
-      .select()
-      .from(schema.events)
-      .orderBy(asc(schema.events.createdAt))
-      .where(and(eq(schema.events.stream, stream), inArray(schema.events.name, acceptedEventsNames))),
-  inserter: (events: z.infer<bg.GenericParsedEventSchema>[]) => db.insert(schema.events).values(events),
-});
+export const EventStore = new bg.DispatchingEventStore<AcceptedEvent, EventMap>(
+  {
+    finder: (stream: bg.EventStreamType, acceptedEventsNames: bg.EventNameType[]) =>
+      db
+        .select()
+        .from(schema.events)
+        .orderBy(asc(schema.events.createdAt))
+        .where(and(eq(schema.events.stream, stream), inArray(schema.events.name, acceptedEventsNames))),
+    inserter: (events: z.infer<bg.GenericParsedEventSchema>[]) => db.insert(schema.events).values(events),
+  },
+  EventBus,
+);
