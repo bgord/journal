4ca6cb9bd6884db9c5f11b98e9d34633deb9d2f8
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-22 17:55:59 +0200

    step: define alarm check output

--- modules/emotions/services/alarms/alarm.ts
+++ modules/emotions/services/alarms/alarm.ts
@@ -1,7 +1,8 @@
+import type * as Events from "../../events";
 import * as VO from "../../value-objects";
 
 export abstract class Alarm {
   abstract name: VO.AlarmName;
 
-  abstract check(): boolean;
+  abstract check(): Events.AlarmGeneratedEventType | null;
 }
