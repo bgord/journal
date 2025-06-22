43055fdd11aee27b1f0647a0e022a69eb2f41430
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-22 21:43:12 +0200

    step: add alarm event to event store

--- infra/event-store.ts
+++ infra/event-store.ts
@@ -4,11 +4,12 @@ import { z } from "zod/v4";
 
 import type { JournalEntryEvent } from "../modules/emotions/aggregates/emotion-journal-entry";
 import type { PatternDetectionEvent } from "../modules/emotions/services/patterns/pattern";
+import type {AlarmEvent} from "../modules/emotions/aggregates/alarm"
 import { db } from "./db";
 import { EventBus } from "./event-bus";
 import * as schema from "./schema";
 
-export type AcceptedEvent = JournalEntryEvent | PatternDetectionEvent;
+export type AcceptedEvent = JournalEntryEvent | PatternDetectionEvent | AlarmEvent;
 
 export const EventStore = new bg.DispatchingEventStore<AcceptedEvent>(
   {
