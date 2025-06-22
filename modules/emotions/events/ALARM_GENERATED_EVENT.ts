1f63c5f8c6309a37dfc787cf51383fc3c967edd0
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-22 16:51:11 +0200

    step: define ALARM_GENERATED_EVENT

---
+++ modules/emotions/events/ALARM_GENERATED_EVENT.ts
@@ -0,0 +1,21 @@
+import * as bg from "@bgord/bun";
+import * as tools from "@bgord/tools";
+import { z } from "zod/v4";
+
+import * as VO from "../value-objects";
+
+export const ALARM_GENERATED_EVENT = "ALARM_GENERATED_EVENT";
+
+export const AlarmGeneratedEvent = z.object({
+  id: bg.UUID,
+  createdAt: tools.Timestamp,
+  stream: z.string().min(1),
+  name: z.literal(ALARM_GENERATED_EVENT),
+  version: z.literal(1),
+  payload: z.object({
+    entryId: VO.EmotionJournalEntryId,
+    alarmName: VO.AlarmName,
+  }),
+});
+
+export type AlarmGeneratedEventType = z.infer<typeof AlarmGeneratedEvent>;
