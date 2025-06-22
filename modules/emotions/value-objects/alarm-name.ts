fe741a5aef40fe1a915fbf8f8e0ea706325f11c6
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-22 16:41:04 +0200

    refactor: make alarm name an enum

--- modules/emotions/value-objects/alarm-name.ts
+++ modules/emotions/value-objects/alarm-name.ts
@@ -1,4 +1,7 @@
 import { z } from "zod/v4";
 
-export const AlarmName = z.string().min(1);
+export enum AlarmName {
+  HIGH_EMOTIONAL_INTENSITY = "HIGH_EMOTIONAL_INTENSITY",
+}
+
 export type AlarmNameType = z.infer<typeof AlarmName>;
