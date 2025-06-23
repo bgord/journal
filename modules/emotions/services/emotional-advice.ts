2475fbe054a50fc0f9aa27d2ea94eb127d21423b
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-23 16:22:15 +0200

    step: implement basic emotional advice service

---
+++ modules/emotions/services/emotional-advice.ts
@@ -0,0 +1,22 @@
+import type * as Aggregates from "../aggregates";
+import * as Services from "../services";
+import * as VO from "../value-objects";
+
+export class EmotionalAdvice {
+  constructor(
+    private readonly AiClient: Services.AiClient,
+    private readonly entry: Aggregates.EmotionJournalEntry,
+    private readonly alarmName: VO.AlarmNameOption,
+  ) {}
+
+  async ask() {
+    const summary = this.entry.summarize();
+
+    const prompt = new Services.EmotionalAdvicePrompt(
+      summary,
+      this.alarmName,
+    ).generate();
+
+    return await this.AiClient.request(prompt);
+  }
+}
