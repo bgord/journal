3ff3cfb8bf40b412dcbefbbf9616ff0ce48d2811
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-23 15:38:38 +0200

    feature: implement EmotionalAdvicePrompt

--- modules/emotions/services/index.ts
+++ modules/emotions/services/index.ts
@@ -2,3 +2,4 @@ export * from "./alarm-generator";
 export * as Alarms from "./alarms";
 export * from "./pattern-detector";
 export * as Patterns from "./patterns/";
+export * from "./emotional-advice-prompt";
