f3e97440401c3ff44437989e94460b2391f2d87e
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-23 23:27:42 +0200

    step: test alarm generator in on emotion reappraised event

--- tests/onEmotionReappraisedEvent.test.ts
+++ tests/onEmotionReappraisedEvent.test.ts
@@ -1,19 +1,25 @@
 import { describe, expect, jest, spyOn, test } from "bun:test";
+import { EventStore } from "../infra/event-store";
 import * as Emotions from "../modules/emotions";
 import * as mocks from "./mocks";
 
 describe("onEmotionReappraisedEvent", () => {
   test("should call repository reappraiseEmotion method with the event", async () => {
+    const eventStoreSave = spyOn(EventStore, "save").mockImplementation(jest.fn());
+
     const reappraiseEmotion = spyOn(
       Emotions.Repos.EmotionJournalEntryRepository,
       "reappraiseEmotion",
     ).mockImplementation(jest.fn());
 
-    await Emotions.Handlers.onEmotionReappraisedEvent(mocks.GenericEmotionReappraisedEvent);
+    await Emotions.Handlers.onEmotionReappraisedEvent(mocks.NegativeEmotionExtremeIntensityReappraisedEvent);
 
     expect(reappraiseEmotion).toHaveBeenCalledTimes(1);
-    expect(reappraiseEmotion).toHaveBeenCalledWith(mocks.GenericEmotionReappraisedEvent);
+    expect(reappraiseEmotion).toHaveBeenCalledWith(mocks.NegativeEmotionExtremeIntensityReappraisedEvent);
+
+    expect(eventStoreSave).toHaveBeenCalledWith([mocks.GenericAlarmGeneratedEvent]);
 
     reappraiseEmotion.mockRestore();
+    eventStoreSave.mockRestore();
   });
 });
