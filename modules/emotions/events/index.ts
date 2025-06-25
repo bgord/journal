8500c92805d1145ea3560fa03e2b86260c7c5233
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-25 23:57:49 +0200

    step: define alarm cancelled event

--- modules/emotions/events/index.ts
+++ modules/emotions/events/index.ts
@@ -1,4 +1,5 @@
 export * from "./ALARM_ADVICE_SAVED_EVENT";
+export * from "./ALARM_CANCELLED_EVENT";
 export * from "./ALARM_GENERATED_EVENT";
 export * from "./ALARM_NOTIFICATION_SENT_EVENT";
 export * from "./EMOTION_JOURNAL_ENTRY_DELETED_EVENT";
