3c133a49904d9f32a10054938a44122e91c57736
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-27 00:19:52 +0200

    refactor: use alarm creator in onEmotionReappraisedEvent

--- modules/emotions/handlers/onEmotionReappraisedEvent.ts
+++ modules/emotions/handlers/onEmotionReappraisedEvent.ts
@@ -1,28 +1,17 @@
-import * as bg from "@bgord/bun";
-import { EventStore } from "../../../infra/event-store";
-import { Alarm } from "../aggregates/alarm";
 import type * as Events from "../events";
 import * as Repos from "../repositories";
-import { AlarmGenerator } from "../services/alarm-generator";
+import { AlarmCreator, AlarmDetector } from "../services";
 import { NegativeEmotionExtremeIntensityAlarm } from "../services/alarms";
 
 export const onEmotionReappraisedEvent = async (event: Events.EmotionReappraisedEventType) => {
   await Repos.EmotionJournalEntryRepository.reappraiseEmotion(event);
 
-  const detection = AlarmGenerator.detect({
+  const detection = AlarmDetector.detect({
     event,
     alarms: [NegativeEmotionExtremeIntensityAlarm],
   });
 
   if (!detection) return;
 
-  const alarmId = bg.NewUUID.generate();
-  const emotionJournalEntryId = event.payload.id;
-
-  const alarm = Alarm.create(alarmId);
-
-  await alarm._generate(emotionJournalEntryId, detection.name);
-
-  const events = alarm.pullEvents();
-  await EventStore.save(events);
+  await AlarmCreator.create(detection, event.payload.id);
 };
