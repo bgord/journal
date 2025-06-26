22debe02ce69fdddb6b4c02b333c40778b576f18
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-26 23:44:02 +0200

    step: create daily alarm limit policy

---
+++ modules/emotions/policies/daily-alarm-limit.ts
@@ -0,0 +1,27 @@
+import * as bg from "@bgord/bun";
+import type { ContentfulStatusCode } from "hono/utils/http-status";
+
+class DailyAlarmLimitError extends Error {
+  constructor() {
+    super();
+    Object.setPrototypeOf(this, DailyAlarmLimitError.prototype);
+  }
+}
+
+type DailyAlarmLimitConfigType = {
+  count: number;
+};
+
+class DailyAlarmLimitFactory extends bg.Policy<DailyAlarmLimitConfigType> {
+  fails(config: DailyAlarmLimitConfigType) {
+    return config.count > 5;
+  }
+
+  message = "daily.alarm.limit";
+
+  error = DailyAlarmLimitError;
+
+  code = 403 as ContentfulStatusCode;
+}
+
+export const DailyAlarmLimit = new DailyAlarmLimitFactory();
--- modules/emotions/policies/index.ts
+++ modules/emotions/policies/index.ts
@@ -2,6 +2,7 @@ export * from "./alarm-advice-available";
 export * from "./alarm-already-generated";
 export * from "./alarm-generated-once";
 export * from "./alarm-is-cancellable";
+export * from "./daily-alarm-limit";
 export * from "./emotion-corresponds-to-situation";
 export * from "./emotion-for-reappraisal-exists";
 export * from "./entry-has-been-started";
