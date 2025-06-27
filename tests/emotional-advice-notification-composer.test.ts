8201878a9c0709a976fa9c7653f23dd30a6181b0
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-28 00:48:12 +0200

    fix: composer tests

--- tests/emotional-advice-notification-composer.test.ts
+++ tests/emotional-advice-notification-composer.test.ts
@@ -2,23 +2,18 @@ import { describe, expect, test } from "bun:test";
 import * as Emotions from "../modules/emotions";
 import * as mocks from "./mocks";
 
-describe.skip("EmotionalAdviceNotificationComposer", () => {
+describe("EmotionalAdviceNotificationComposer", () => {
   test("compose", () => {
-    const negativeEmotionExtremeIntensityEntry = Emotions.Aggregates.EmotionJournalEntry.build(mocks.id, [
-      mocks.GenericSituationLoggedEvent,
-      mocks.NegativeEmotionExtremeIntensityLoggedEvent,
-    ]);
-
     const advice = new Emotions.VO.EmotionalAdvice("Do something");
 
     const emotionalAdviceNotificationComposer = new Emotions.Services.EmotionalAdviceNotificationComposer(
-      negativeEmotionExtremeIntensityEntry.summarize(),
+      mocks.partialEntry,
     );
 
     const notification = emotionalAdviceNotificationComposer.compose(advice);
 
     expect(notification).toEqual(
-      `Advice for emotion entry: ${negativeEmotionExtremeIntensityEntry.summarize().emotion?.label}: ${advice.get()}`,
+      `Advice for emotion entry: ${mocks.partialEntry.emotionLabel}: ${advice.get()}`,
     );
   });
 });
