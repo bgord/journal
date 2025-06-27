e86b9e76824ed250040b710624c3eac116e7571f
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-27 15:36:33 +0200

    refactor: rename register-event-handlers

--- infra/register-event-handlers.ts
+++ infra/register-event-handlers.ts
@@ -1,6 +1,6 @@
 import * as bg from "@bgord/bun";
-import * as EmotionEventHandlers from "../modules/emotions/event-handlers";
-import * as EmotionEvents from "../modules/emotions/events";
+import * as EventHandlers from "../modules/emotions/event-handlers";
+import * as Events from "../modules/emotions/events";
 import * as Sagas from "../modules/emotions/sagas";
 import { EventBus } from "./event-bus";
 import { logger } from "./logger";
@@ -12,77 +12,53 @@ const OpenAI = new OpenAiClient();
 // Emotion journal entry
 
 EventBus.on(
-  EmotionEvents.EMOTION_JOURNAL_ENTRY_DELETED_EVENT,
-  EventHandler.handle(EmotionEventHandlers.onEmotionJournalEntryDeletedEvent),
+  Events.EMOTION_JOURNAL_ENTRY_DELETED_EVENT,
+  EventHandler.handle(EventHandlers.onEmotionJournalEntryDeletedEvent),
 );
 
-EventBus.on(
-  EmotionEvents.EMOTION_REAPPRAISED_EVENT,
-  EventHandler.handle(EmotionEventHandlers.onEmotionReappraisedEvent),
-);
+EventBus.on(Events.EMOTION_REAPPRAISED_EVENT, EventHandler.handle(EventHandlers.onEmotionReappraisedEvent));
 
-EventBus.on(
-  EmotionEvents.EMOTION_LOGGED_EVENT,
-  EventHandler.handle(EmotionEventHandlers.onEmotionLoggedEvent),
-);
+EventBus.on(Events.EMOTION_LOGGED_EVENT, EventHandler.handle(EventHandlers.onEmotionLoggedEvent));
 
-EventBus.on(
-  EmotionEvents.REACTION_EVALUATED_EVENT,
-  EventHandler.handle(EmotionEventHandlers.onReactionEvaluatedEvent),
-);
+EventBus.on(Events.REACTION_EVALUATED_EVENT, EventHandler.handle(EventHandlers.onReactionEvaluatedEvent));
 
-EventBus.on(
-  EmotionEvents.REACTION_LOGGED_EVENT,
-  EventHandler.handle(EmotionEventHandlers.onReactionLoggedEvent),
-);
+EventBus.on(Events.REACTION_LOGGED_EVENT, EventHandler.handle(EventHandlers.onReactionLoggedEvent));
 
-EventBus.on(
-  EmotionEvents.SITUATION_LOGGED_EVENT,
-  EventHandler.handle(EmotionEventHandlers.onSituationLoggedEvent),
-);
+EventBus.on(Events.SITUATION_LOGGED_EVENT, EventHandler.handle(EventHandlers.onSituationLoggedEvent));
 
 // Pattern detection
 
 EventBus.on(
-  EmotionEvents.MORE_NEGATIVE_THAN_POSITIVE_EMOTIONS_PATTERN_DETECTED_EVENT,
-  EventHandler.handle(EmotionEventHandlers.onMoreNegativeThanPositiveEmotionsPatternDetectedEvent),
+  Events.MORE_NEGATIVE_THAN_POSITIVE_EMOTIONS_PATTERN_DETECTED_EVENT,
+  EventHandler.handle(EventHandlers.onMoreNegativeThanPositiveEmotionsPatternDetectedEvent),
 );
 
 EventBus.on(
-  EmotionEvents.MULTIPLE_MALADAPTIVE_REACTIONS_PATTERN_DETECTED_EVENT,
-  EventHandler.handle(EmotionEventHandlers.onMultipleMaladaptiveReactionsPatternDetectedEvent),
+  Events.MULTIPLE_MALADAPTIVE_REACTIONS_PATTERN_DETECTED_EVENT,
+  EventHandler.handle(EventHandlers.onMultipleMaladaptiveReactionsPatternDetectedEvent),
 );
 
 EventBus.on(
-  EmotionEvents.POSITIVE_EMOTION_WITH_MALADAPTIVE_REACTION_PATTERN_DETECTED_EVENT,
-  EventHandler.handle(EmotionEventHandlers.onPositiveEmotionWithMaladaptiveReactionPatternDetectedEvent),
+  Events.POSITIVE_EMOTION_WITH_MALADAPTIVE_REACTION_PATTERN_DETECTED_EVENT,
+  EventHandler.handle(EventHandlers.onPositiveEmotionWithMaladaptiveReactionPatternDetectedEvent),
 );
 
 EventBus.on(
-  EmotionEvents.LOW_COPING_EFFECTIVENESS_PATTERN_DETECTED_EVENT,
-  EventHandler.handle(EmotionEventHandlers.onLowCopingEffectivenessPatternDetectedEvent),
+  Events.LOW_COPING_EFFECTIVENESS_PATTERN_DETECTED_EVENT,
+  EventHandler.handle(EventHandlers.onLowCopingEffectivenessPatternDetectedEvent),
 );
 
 // Alarms
 
-EventBus.on(
-  EmotionEvents.ALARM_GENERATED_EVENT,
-  EventHandler.handle(EmotionEventHandlers.onAlarmGeneratedEvent),
-);
+EventBus.on(Events.ALARM_GENERATED_EVENT, EventHandler.handle(EventHandlers.onAlarmGeneratedEvent));
+
+EventBus.on(Events.ALARM_ADVICE_SAVED_EVENT, EventHandler.handle(EventHandlers.onAlarmAdviceSavedEvent));
 
 EventBus.on(
-  EmotionEvents.ALARM_ADVICE_SAVED_EVENT,
-  EventHandler.handle(EmotionEventHandlers.onAlarmAdviceSavedEvent),
+  Events.ALARM_NOTIFICATION_SENT_EVENT,
+  EventHandler.handle(EventHandlers.onAlarmNotificationSentEvent),
 );
 
-EventBus.on(
-  EmotionEvents.ALARM_NOTIFICATION_SENT_EVENT,
-  EventHandler.handle(EmotionEventHandlers.onAlarmNotificationSentEvent),
-);
-
-EventBus.on(
-  EmotionEvents.ALARM_CANCELLED_EVENT,
-  EventHandler.handle(EmotionEventHandlers.onAlarmCancelledEvent),
-);
+EventBus.on(Events.ALARM_CANCELLED_EVENT, EventHandler.handle(EventHandlers.onAlarmCancelledEvent));
 
 new Sagas.AlarmProcessing(OpenAI).register(EventBus);
