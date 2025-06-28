dbc097267740de1e15c932ca26d789fb57c337cc
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-28 22:01:18 +0200

    step: setup weekly-review aggregate

---
+++ modules/emotions/aggregates/weekly-review.ts
@@ -0,0 +1,74 @@
+import * as bg from "@bgord/bun";
+import * as tools from "@bgord/tools";
+import { z } from "zod/v4";
+import * as Events from "../events";
+import * as VO from "../value-objects";
+
+export type WeeklyReviewEvent = (typeof WeeklyReview)["events"][number];
+type WeeklyReviewEventType = z.infer<WeeklyReviewEvent>;
+
+export class WeeklyReview {
+  static events = [Events.WeeklyReviewRequestedEvent];
+
+  private readonly id: VO.WeeklyReviewIdType;
+
+  private readonly pending: WeeklyReviewEventType[] = [];
+
+  private constructor(id: VO.WeeklyReviewIdType) {
+    this.id = id;
+  }
+
+  static create(id: VO.WeeklyReviewIdType): WeeklyReview {
+    return new WeeklyReview(id);
+  }
+
+  static build(id: VO.WeeklyReviewIdType, events: WeeklyReviewEventType[]): WeeklyReview {
+    const entry = new WeeklyReview(id);
+
+    events.forEach((event) => entry.apply(event));
+
+    return entry;
+  }
+
+  async request(weekStartedAt: tools.TimestampType) {
+    const event = Events.WeeklyReviewRequestedEvent.parse({
+      id: bg.NewUUID.generate(),
+      correlationId: bg.CorrelationStorage.get(),
+      createdAt: tools.Timestamp.parse(Date.now()),
+      name: Events.WEEKLY_REVIEW_REQUESTED_EVENT,
+      stream: WeeklyReview.getStream(this.id),
+      version: 1,
+      payload: { weeklyReviewId: this.id, weekStartedAt },
+    } satisfies Events.WeeklyReviewRequestedEventType);
+
+    this.record(event);
+  }
+
+  pullEvents(): WeeklyReviewEventType[] {
+    const events = [...this.pending];
+
+    this.pending.length = 0;
+
+    return events;
+  }
+
+  private record(event: WeeklyReviewEventType): void {
+    this.apply(event);
+    this.pending.push(event);
+  }
+
+  private apply(event: WeeklyReviewEventType): void {
+    switch (event.name) {
+      case Events.WEEKLY_REVIEW_REQUESTED_EVENT: {
+        break;
+      }
+
+      default:
+        break;
+    }
+  }
+
+  static getStream(id: VO.WeeklyReviewIdType) {
+    return `weekly_review_${id}`;
+  }
+}
