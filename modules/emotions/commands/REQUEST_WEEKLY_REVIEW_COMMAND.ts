edac5fcbda3392cb578df3a904792fa21db1be8a
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-28 22:31:01 +0200

    step: use week start in the scheduler

--- modules/emotions/commands/REQUEST_WEEKLY_REVIEW_COMMAND.ts
+++ modules/emotions/commands/REQUEST_WEEKLY_REVIEW_COMMAND.ts
@@ -1,6 +1,7 @@
 import * as bg from "@bgord/bun";
 import * as tools from "@bgord/tools";
 import { z } from "zod/v4";
+import * as VO from "../value-objects";
 
 export const REQUEST_WEEKLY_REVIEW_COMMAND = "REQUEST_WEEKLY_REVIEW_COMMAND";
 
@@ -9,7 +10,7 @@ export const RequestWeeklyReviewCommand = z.object({
   correlationId: bg.UUID,
   createdAt: tools.Timestamp,
   name: z.literal(REQUEST_WEEKLY_REVIEW_COMMAND),
-  payload: z.object({ weekStartedAt: tools.Timestamp }),
+  payload: z.object({ weekStart: z.instanceof(VO.WeekStart) }),
 });
 
 export type RequestWeeklyReviewCommandType = z.infer<typeof RequestWeeklyReviewCommand>;
--- modules/emotions/services/weekly-review-scheduler.ts
+++ modules/emotions/services/weekly-review-scheduler.ts
@@ -1,8 +1,8 @@
 import * as bg from "@bgord/bun";
 import * as tools from "@bgord/tools";
-import { startOfWeek } from "date-fns";
 import * as infra from "../../../infra";
 import * as Commands from "../commands";
+import * as VO from "../value-objects";
 
 export class WeeklyReviewScheduler {
   // Sunday at 18:00 UTC
@@ -14,16 +14,15 @@ export class WeeklyReviewScheduler {
   /** @public */
   static async process() {
     // TODO: figure out the correlationId
-    const today = new Date();
-    const weekStart = startOfWeek(today, { weekStartsOn: 1 });
-    const weekStartedAt = tools.Timestamp.parse(weekStart.getTime());
+
+    const weekStart = VO.WeekStart.fromTimestamp(Date.now());
 
     const command = Commands.RequestWeeklyReviewCommand.parse({
       id: bg.NewUUID.generate(),
       correlationId: bg.CorrelationStorage.get(),
       name: Commands.REQUEST_WEEKLY_REVIEW_COMMAND,
       createdAt: tools.Timestamp.parse(Date.now()),
-      payload: { weekStartedAt },
+      payload: { weekStart },
     } satisfies Commands.RequestWeeklyReviewCommandType);
 
     await infra.CommandBus.emit(command.name, command);
