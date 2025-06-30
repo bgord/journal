a6953a1afdaf87c0fcd64bc94376fdccfd459596
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-30 19:45:10 +0200

    step: define axiom transport

--- infra/logger.ts
+++ infra/logger.ts
@@ -1,8 +1,13 @@
+// cspell:disable-next-line
+import { WinstonTransport as AxiomTransport } from "@axiomhq/winston";
 import * as bg from "@bgord/bun";
 import { Env } from "./env";
+
+const axiom = new AxiomTransport({ token: Env.AXIOM_API_TOKEN, dataset: Env.AXIOM_DATASET_NAME });
 
 export const logger = new bg.Logger({
   app: "lobbygow",
   environment: Env.type,
   level: Env.LOGS_LEVEL,
+  transports: Env.type === bg.NodeEnvironmentEnum.production ? [axiom] : [],
 });
