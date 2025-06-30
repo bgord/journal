927937067faa8a79bf332e8fd072bdacd34f30d6
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-30 20:11:50 +0200

    step: use axioms from bgord/bun

--- infra/logger.ts
+++ infra/logger.ts
@@ -1,12 +1,12 @@
-// cspell:disable-next-line
-import { WinstonTransport as AxiomTransport } from "@axiomhq/winston";
 import * as bg from "@bgord/bun";
 import { Env } from "./env";
 
-const axiom = new AxiomTransport({ token: Env.AXIOM_API_TOKEN, dataset: Env.AXIOM_DATASET_NAME });
+const app = "lobbygow";
+
+const axiom = new bg.AxiomTransport({ token: Env.AXIOM_API_TOKEN, dataset: app });
 
 export const logger = new bg.Logger({
-  app: "lobbygow",
+  app,
   environment: Env.type,
   level: Env.LOGS_LEVEL,
   transports: Env.type === bg.NodeEnvironmentEnum.production ? [axiom] : [],
