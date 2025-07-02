6dd0b6f1ff17f06668f3a3a3e4b42c65aedf7b3e
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-02 12:05:47 +0200

    step: add revalidate on focus

---
+++ frontend/components/revalidate-on-focus.tsx
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+import { useRevalidator } from "react-router";
+
+export function RevalidateOnFocus() {
+  const revalidator = useRevalidator();
+
+  useEffect(() => {
+    const onFocus = () => revalidator.revalidate();
+
+    window.addEventListener("focus", onFocus);
+    return () => window.removeEventListener("focus", onFocus);
+  }, [revalidator]);
+
+  return null;
+}
