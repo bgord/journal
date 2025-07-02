80026978ef6bbf1acc785f323e5749ec5f0118f6
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-02 12:09:50 +0200

    step: load entries

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,15 +1,33 @@
-import { Link } from "react-router";
+import type { SelectEmotionJournalEntries } from "../../../infra/schema";
 import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "New React Router App" }, { name: "description", content: "Welcome to React Router!" }];
 }
 
-export default function Home() {
+export async function loader(): Promise<SelectEmotionJournalEntries[]> {
+  const res = await fetch("http://localhost:3001/emotions/entries");
+  return (await res.json()) as SelectEmotionJournalEntries[];
+}
+
+export default function Home({ loaderData }: Route.ComponentProps) {
   return (
     <main>
-      <header data-mb="24">Welcome</header>
-      <Link to="/about">About</Link>
+      <header data-mb="24">Entries</header>
+      <ul>
+        {loaderData.map((entry) => (
+          <li key={entry.id}>
+            <div>
+              Situation - {entry.situationKind} at {entry.situationLocation}
+            </div>
+            <div>{entry.situationDescription}</div>
+
+            <div>
+              {entry.emotionLabel} {entry.emotionIntensity}/5
+            </div>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
