19ec63cc7b05bf576e66a37d0a30bf102611efcb
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-02 14:19:53 +0200

    step: display the situation

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -21,41 +21,55 @@ export default function Home({ loaderData }: Route.ComponentProps) {
         {loaderData.map((entry) => (
           <li
             data-display="flex"
-            data-p="3"
+            data-py="12"
+            data-px="48"
             data-direction="column"
             data-fs="14"
             data-bc="gray-200"
             data-bw="1"
             key={entry.id}
           >
-            <div data-display="flex" data-gap="6">
-              <div className="c-badge">{entry.situationKind}</div>
-              <div>Situation at {entry.situationLocation}</div>
-            </div>
+            <header data-fw="700">{new Date(entry.startedAt).toLocaleString()}</header>
 
-            <div>{entry.situationDescription}</div>
+            <section
+              data-display="flex"
+              data-direction="column"
+              data-gap="6"
+              data-py="3"
+              data-bcb="gray-200"
+              data-bwb="1"
+            >
+              <div data-display="flex" data-gap="6">
+                <div data-color="gray-600">What happened?</div>
+                <div data-ml="auto" data-color="gray-800">
+                  @{entry.situationLocation}
+                </div>
+                <div className="c-badge">{entry.situationKind}</div>
+              </div>
+              <div>{entry.situationDescription}</div>
+            </section>
 
-            <div data-display="flex" data-gap="6">
-              <div data-fw="700">Emotion</div>
-              <div className="c-badge">{entry.emotionLabel}</div>
-            </div>
+            {/* <div data-display="flex" data-gap="6"> */}
+            {/*   <div data-fw="700">Emotion</div> */}
+            {/*   <div className="c-badge">{entry.emotionLabel}</div> */}
+            {/* </div> */}
 
-            <div data-display="flex" data-gap="6">
-              <div data-fw="700">Intensity:</div>
-              {entry.emotionIntensity}/5
-            </div>
+            {/* <div data-display="flex" data-gap="6"> */}
+            {/*   <div data-fw="700">Intensity:</div> */}
+            {/*   {entry.emotionIntensity}/5 */}
+            {/* </div> */}
 
-            <div data-display="flex" data-gap="6">
-              <div data-fw="700">Reacted with</div>
-              <div className="c-badge">{entry.reactionType}</div>
-            </div>
+            {/* <div data-display="flex" data-gap="6"> */}
+            {/*   <div data-fw="700">Reacted with</div> */}
+            {/*   <div className="c-badge">{entry.reactionType}</div> */}
+            {/* </div> */}
 
-            <div>{entry.reactionDescription}</div>
+            {/* <div>{entry.reactionDescription}</div> */}
 
-            <div data-display="flex" data-gap="6">
-              <div data-fw="700">Effectiveness:</div>
-              {entry.reactionEffectiveness}/5
-            </div>
+            {/* <div data-display="flex" data-gap="6"> */}
+            {/*   <div data-fw="700">Effectiveness:</div> */}
+            {/*   {entry.reactionEffectiveness}/5 */}
+            {/* </div> */}
           </li>
         ))}
       </ul>
