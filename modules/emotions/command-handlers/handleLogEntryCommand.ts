2c34ea6a3c2570f85d63b88d6a92eb00f187e36c
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-04 13:25:43 +0200

    step: implement handleLogEntryCommand

--- modules/emotions/command-handlers/handleLogEntryCommand.ts
+++ modules/emotions/command-handlers/handleLogEntryCommand.ts
@@ -1,4 +1,12 @@
 import * as Emotions from "+emotions";
-// import { EventStore } from "+infra";
+import { EventStore } from "+infra";
 
-export const handleLogEntryCommand = async (_command: Emotions.Commands.LogEntryCommandType) => {};
+export const handleLogEntryCommand = async (command: Emotions.Commands.LogEntryCommandType) => {
+  const entry = Emotions.Aggregates.EmotionJournalEntry.create(command.payload.emotionJournalEntryId);
+
+  await entry.logSituation(command.payload.situation);
+  await entry.logEmotion(command.payload.emotion);
+  await entry.logReaction(command.payload.reaction);
+
+  await EventStore.save(entry.pullEvents());
+};
