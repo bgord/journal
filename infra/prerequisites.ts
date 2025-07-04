142884f876b72a14ecf8074e1fc5ef63c8bb2529
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-04 17:02:11 +0200

    step: update packages

--- infra/prerequisites.ts
+++ infra/prerequisites.ts
@@ -24,7 +24,7 @@ export const prerequisites = [
   }),
   new bg.PrerequisiteBun({
     label: "bun",
-    version: tools.PackageVersion.fromString("1.2.17"),
+    version: tools.PackageVersion.fromString("1.2.18"),
     current: Bun.version,
   }),
   new bg.PrerequisiteMemory({
