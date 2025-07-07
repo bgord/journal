1c7c09987175151c503019d228be44bfa3401b23
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-07 14:38:00 +0200

    refactor: simplify the verify dependencies script

--- verify-dependencies.sh
+++ verify-dependencies.sh
@@ -6,28 +6,28 @@ setup_base_config
 info "Verifying dependencies"
 
 function check_dependencies() {
-  local package_json_path=$1
-  local dep_type=$2
+  local package_json_path="package.json"
+  local dep_type=$1
 
   if ! jq -e ".$dep_type" "$package_json_path" >/dev/null; then
     return
   fi
 
   while read -r name version; do
-    if [[ "$version" == *"^"* || "$version" == *"~"* ]]; then
-      error "Dependency $name@$version in $package_json_path is not pinned. Please use an exact version."
+    if test "$version" == *"^"* || test "$version" == *"~"*; then
+      error "$name@$version in $package_json_path is not pinned"
       exit 1
     fi
 
-    if [[ "$version" == *"file:"* ]]; then
-      error "Dependency $name@$version in $package_json_path is a file dependency. Please use a published version."
+    if test "$version" == *"file:"*; then
+      error "$name@$version in $package_json_path is not pinned"
       exit 1
     fi
   done < <(jq -r ".$dep_type | to_entries | .[] | .key + \" \" + .value" "$package_json_path")
 }
 
-check_dependencies "package.json" "dependencies"
-check_dependencies "package.json" "devDependencies"
-check_dependencies "package.json" "peerDependencies"
+check_dependencies "dependencies"
+check_dependencies "devDependencies"
+check_dependencies "peerDependencies"
 
 success "Dependencies verified"
