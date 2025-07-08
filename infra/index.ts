136e795d8f10cb2fb12c41ebdbde6c94475793c3
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-08 13:34:39 +0200

    refactor: remove infra barrel export

--- infra/index.ts
+++ infra/index.ts
@@ -3,23 +3,6 @@ import * as tools from "@bgord/tools";
 import { HTTPException } from "hono/http-exception";
 import type { TimingVariables } from "hono/timing";
 
-export * from "./anthropic-ai-client";
-export * from "./basic-auth-shield";
-export * from "./command-bus";
-export * from "./db";
-export * from "./env";
-export * from "./event-bus";
-export * from "./event-store";
-export * from "./healthcheck";
-export * from "./i18n";
-export * from "./logger";
-export * from "./mailer";
-export * from "./open-ai-client";
-export * from "./prerequisites";
-export * from "./register-command-handlers";
-export * from "./register-event-handlers";
-export * as Schema from "./schema";
-
 export const requestTimeoutError = new HTTPException(408, {
   message: "request_timeout_error",
 });
