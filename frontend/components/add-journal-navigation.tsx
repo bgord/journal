6e5f4237c40698e3a5d62e68fbe3e6a51b9ccd55
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-10 23:38:50 +0200

    step: improve add entry navigation

--- frontend/components/add-journal-navigation.tsx
+++ frontend/components/add-journal-navigation.tsx
@@ -4,15 +4,23 @@ export function AddJournalNavigation(props: { step: AddJournalNavigationStep }) 
   const style = { style: { color: "var(--brand-700)" } };
 
   return (
-    <div data-display="flex" data-mx="auto" data-gap="6" data-ls="0.5" data-mb="12">
+    <div data-display="flex" data-cross="center" data-mx="auto" data-gap="6" data-ls="0.5" data-mb="12">
+      {props.step === "situation" && <img src="/pencil.svg" height="15px" alt="Fill up reaction" />}
+
       <div data-fs="14" data-fw={props.step === "situation" ? "700" : "300"} {...style}>
         SITUATION
       </div>
+
       <div data-fw="700">·</div>
+      {props.step === "emotion" && <img src="/pencil.svg" height="15px" alt="Fill up reaction" />}
+
       <div data-fs="14" data-fw={props.step === "emotion" ? "700" : "300"} {...style}>
         EMOTION
       </div>
+
       <div data-fw="700">·</div>
+      {props.step === "reaction" && <img src="/pencil.svg" height="15px" alt="Fill up reaction" />}
+
       <div data-fs="14" data-fw={props.step === "reaction" ? "700" : "300"} {...style}>
         REACTION
       </div>
