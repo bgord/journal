0b151a7e699d5122db8a0ec3247afd020c6eaeb2
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-14 00:11:52 +0200

    refactor: rename home entry

--- frontend/components/index.tsx
+++ frontend/components/index.tsx
@@ -2,7 +2,7 @@ export * from "./add-entry-navigation";
 export * from "./add-entry-navigation-progress";
 export * from "./back-button";
 export * from "./clickable-rating-pills";
-export * from "./entry";
+export * from "./home-entry";
 export * from "./rating-pills";
 export * from "./revalidate-on-focus";
 export * from "./select";
