318e822e7765bc6ae2767a6eb9c04d83ded4cc0c
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-13 22:07:27 +0200

    step: add credentials include

--- frontend/api.ts
+++ frontend/api.ts
@@ -1,5 +1,6 @@
 export const API: typeof fetch = (input, init) =>
   fetch(`${import.meta.env.VITE_API_URL}${input}`, {
+    credentials: "include",
     ...init,
     headers: {
       "Content-Type": "application/json",
