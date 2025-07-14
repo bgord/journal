6f529edbd74db25e3e413e900abdcdc21c478574
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-14 22:06:20 +0200

    step: handle emotion label update

--- frontend/components/entry-emotion.tsx
+++ frontend/components/entry-emotion.tsx
@@ -1,13 +1,22 @@
 import * as UI from "@bgord/ui";
 import React from "react";
 import * as RR from "react-router";
+
 import type { types } from "../../app/services/add-entry-form";
 import type { SelectEntriesFormatted } from "../../infra/schema";
+import type { loader } from "../app/routes/home";
+
 import { ClickableRatingPills } from "./clickable-rating-pills";
+import { Select } from "./select";
+
+type LoaderData = Awaited<ReturnType<typeof loader>>;
 
 export function EntryEmotion(props: SelectEntriesFormatted) {
   const t = UI.useTranslations();
   const fetcher = RR.useFetcher();
+  const loader = RR.useLoaderData<LoaderData>();
+
+  const editingEmotionLabel = UI.useToggle({ name: "emotion-label" });
 
   const emotionLabel = UI.useField<types.EmotionLabelType>({
     name: "label",
@@ -34,9 +43,35 @@ export function EntryEmotion(props: SelectEntriesFormatted) {
     if (emotionIntensity.changed) reappraiseEmotion();
   }, [emotionIntensity.value]);
 
+  React.useEffect(() => {
+    if (emotionLabel.changed) reappraiseEmotion();
+  }, [emotionLabel.value]);
+
   return (
     <div data-display="flex" data-cross="center" data-gap="12">
-      <div className="c-badge">{t(`entry.emotion.label.value.${props.emotionLabel}`)}</div>
+      {editingEmotionLabel.off && (
+        <div className="c-badge" onClick={editingEmotionLabel.enable} data-cursor="pointer">
+          {t(`entry.emotion.label.value.${emotionLabel.value}`)}
+        </div>
+      )}
+
+      {editingEmotionLabel.on && (
+        <Select
+          {...emotionLabel.input.props}
+          onChange={(event) => {
+            emotionLabel.input.props.onChange(event);
+            editingEmotionLabel.disable();
+          }}
+        >
+          {loader.form.emotionLabels
+            .map((emotion) => emotion.option)
+            .map((label: types.EmotionLabelType) => (
+              <option key={label} value={label}>
+                {t(`entry.emotion.label.value.${label}`)}
+              </option>
+            ))}
+        </Select>
+      )}
 
       <ClickableRatingPills {...emotionIntensity} />
     </div>
