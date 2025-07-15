b99afca3ca67055412321d49a85cb6e07da14c36
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-15 16:57:47 +0200

    fix: tests

--- tests/onAlarmGeneratedEvent.test.ts
+++ tests/onAlarmGeneratedEvent.test.ts
@@ -5,11 +5,17 @@ import * as mocks from "./mocks";
 describe("onAlarmGeneratedEvent", () => {
   it("should call repository generated method with the event", async () => {
     const generate = spyOn(Emotions.Repos.AlarmRepository, "generate").mockImplementation(jest.fn());
+    const getByIdRaw = spyOn(Emotions.Repos.EntryRepository, "getByIdRaw").mockImplementation(
+      jest.fn().mockResolvedValue(mocks.fullEntry),
+    );
 
     await Emotions.EventHandlers.onAlarmGeneratedEvent(mocks.GenericAlarmGeneratedEvent);
 
     expect(generate).toHaveBeenCalledTimes(1);
-    expect(generate).toHaveBeenCalledWith(mocks.GenericAlarmGeneratedEvent);
+    expect(generate).toHaveBeenCalledWith(mocks.GenericAlarmGeneratedEvent, {
+      label: mocks.fullEntry.emotionLabel,
+      intensity: mocks.fullEntry.emotionIntensity,
+    });
 
     jest.restoreAllMocks();
   });
