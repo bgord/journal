da9b809e087b26b78f216b0fc252742d0524f0a6
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-16 13:44:58 +0200

    step: add auth shield verify

--- infra/auth-shield.ts
+++ infra/auth-shield.ts
@@ -19,4 +19,18 @@ export class AuthShield {
     c.set("session", session.session);
     return next();
   });
+
+  verify = createMiddleware(async (c: hono.Context, next: hono.Next) => {
+    const session = await this.Auth.api.getSession({ headers: c.req.raw.headers });
+
+    if (!session) {
+      c.set("user", null);
+      c.set("session", null);
+      return next();
+    }
+
+    c.set("user", session.user);
+    c.set("session", session.session);
+    return next();
+  });
 }
