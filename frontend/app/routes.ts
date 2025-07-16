1c9826f1689a2db71bd4b1b0f93e8d71c80a4b52
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-16 20:35:11 +0200

    feature: add auth-layout

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -1,9 +1,12 @@
-import { index, type RouteConfig, route } from "@react-router/dev/routes";
+import { index, layout, type RouteConfig, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/login.tsx"),
-  route("/add-entry", "routes/add-entry.tsx"),
-  route("/home", "routes/home.tsx"),
-  route("/logout", "routes/logout.tsx"),
   route("/register", "routes/register.tsx"),
+
+  layout("routes/auth-layout.tsx", [
+    route("/add-entry", "routes/add-entry.tsx"),
+    route("/home", "routes/home.tsx"),
+    route("/logout", "routes/logout.tsx"),
+  ]),
 ] satisfies RouteConfig;
---
+++ frontend/app/routes/auth-layout.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router";
+import { requireSession } from "../../auth-guard";
+import type { Route } from "./+types/auth-layout";
+
+export async function loader({ request }: Route.LoaderArgs) {
+  const session = await requireSession(request);
+  return { session };
+}
+
+export default function AuthLayout({ loaderData }: Route.ComponentProps) {
+  return <Outlet context={{ session: loaderData.session }} />;
+}
