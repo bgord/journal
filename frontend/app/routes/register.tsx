222aef991b6169a1c51facda366ff6e9f086c9b5
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-16 16:37:03 +0200

    step: add register info boxes

--- frontend/app/routes/register.tsx
+++ frontend/app/routes/register.tsx
@@ -1,32 +1,36 @@
 import * as UI from "@bgord/ui";
-import { Form } from "react-router";
+import * as Icons from "iconoir-react";
+import React from "react";
+import { Form, Link } from "react-router";
 import { authClient } from "../../auth";
+
+enum SignUpState {
+  idle = "idle",
+  success = "success",
+  error = "error",
+}
 
 // TODO: translations
 export default function SignUp() {
+  const [state, setState] = React.useState<SignUpState>(SignUpState.idle);
+
   const email = UI.useField({ name: "email", defaultValue: "" });
   const password = UI.useField({ name: "password", defaultValue: "" });
 
   const signUp = async (event: React.FormEvent) => {
     event.preventDefault();
-    console.log("submit");
+
     await authClient.signUp.email(
       { email: email.value, password: password.value, name: email.value },
       {
-        onSuccess: (ctx) => {
-          console.log("success");
-          console.log(ctx);
-        },
-        onError: (ctx) => {
-          console.log("err");
-          console.log(ctx);
-        },
+        onSuccess: () => setState(SignUpState.success),
+        onError: () => setState(SignUpState.error),
       },
     );
   };
 
   return (
-    <main data-display="flex" data-direction="column">
+    <main data-display="flex" data-direction="column" data-gap="24">
       <Form
         data-display="flex"
         data-direction="column"
@@ -50,7 +54,13 @@ export default function SignUp() {
             Email
           </label>
 
-          <input className="c-input" type="text" placeholder="admin@example.com" {...email.input.props} />
+          <input
+            className="c-input"
+            type="email"
+            placeholder="admin@example.com"
+            disabled={state === SignUpState.success}
+            {...email.input.props}
+          />
         </div>
 
         <div data-display="flex" data-direction="column">
@@ -58,12 +68,63 @@ export default function SignUp() {
             Password
           </label>
 
-          <input className="c-input" type="password" placeholder="**********" {...password.input.props} />
+          <input
+            className="c-input"
+            type="password"
+            placeholder="**********"
+            disabled={state === SignUpState.success}
+            {...password.input.props}
+          />
         </div>
 
-        <button className="c-button" data-variant="primary" data-mt="24" type="submit">
+        <button
+          className="c-button"
+          data-variant="primary"
+          data-mt="24"
+          type="submit"
+          disabled={UI.Fields.allUnchanged([email, password]) || state === SignUpState.success}
+        >
           Sign Up
         </button>
+
+        {state === SignUpState.success && (
+          <div
+            data-display="flex"
+            data-main="baseline"
+            data-gap="12"
+            data-mt="24"
+            data-mx="auto"
+            data-bg="green-100"
+            data-color="green-600"
+            data-py="12"
+            data-px="24"
+          >
+            <Icons.CheckCircle height={20} width={20} />
+            Account created successfully!
+            <br />
+            <span>
+              Check your inbox and{" "}
+              <Link data-transform="center" to="/login">
+                login
+              </Link>
+            </span>
+          </div>
+        )}
+
+        {state === SignUpState.error && (
+          <div
+            data-display="flex"
+            data-gap="12"
+            data-mt="24"
+            data-mx="auto"
+            data-bg="red-100"
+            data-color="red-600"
+            data-p="12"
+          >
+            <Icons.WarningCircle height={20} width={20} />
+            Error while creating an account
+          </div>
+        )}
       </Form>
     </main>
   );
