77f8df339caa1b635cfa446be711dee0f3ad52ec
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-17 16:11:42 +0200

    step: handle auth in tests

--- tests/list-entries.test.ts
+++ tests/list-entries.test.ts
@@ -1,4 +1,6 @@
 import { describe, expect, spyOn, test } from "bun:test";
+import * as bg from "@bgord/bun";
+import { auth } from "../infra/auth";
 import * as Emotions from "../modules/emotions";
 import { server } from "../server";
 import * as mocks from "./mocks";
@@ -6,7 +8,15 @@ import * as mocks from "./mocks";
 const url = "/entry/list";
 
 describe("GET /entry/list", () => {
+  test("validation - AccessDeniedAuthShieldError", async () => {
+    const response = await server.request(url, { method: "GET" }, mocks.ip);
+    const json = await response.json();
+    expect(response.status).toBe(403);
+    expect(json).toEqual({ message: bg.AccessDeniedAuthShieldError.message, _known: true });
+  });
+
   test("happy path ", async () => {
+    spyOn(auth.api, "getSession").mockResolvedValue(mocks.auth);
     spyOn(Emotions.Repos.EntryRepository, "list").mockResolvedValue([mocks.fullEntryFormatted]);
     const response = await server.request(url, { method: "GET" }, mocks.ip);
 
@@ -17,6 +27,7 @@ describe("GET /entry/list", () => {
   });
 
   test("happy path - empty list", async () => {
+    spyOn(auth.api, "getSession").mockResolvedValue(mocks.auth);
     spyOn(Emotions.Repos.EntryRepository, "list").mockResolvedValue([]);
     const response = await server.request(url, { method: "GET" }, mocks.ip);
 
