91947db646dceab70f337ebb6bac9115a626a85d
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-17 14:51:11 +0200

    step: improve auth layout

--- frontend/app/routes/auth-layout.tsx
+++ frontend/app/routes/auth-layout.tsx
@@ -1,5 +1,8 @@
+import * as UI from "@bgord/ui";
+import { ProfileCircle } from "iconoir-react";
 import { Outlet } from "react-router";
 import { requireSession } from "../../auth-guard";
+import { LogoutButton } from "../../components/logout-button";
 import type { Route } from "./+types/auth-layout";
 
 export async function loader({ request }: Route.LoaderArgs) {
@@ -8,5 +11,16 @@ export async function loader({ request }: Route.LoaderArgs) {
 }
 
 export default function AuthLayout({ loaderData }: Route.ComponentProps) {
-  return <Outlet context={{ ...loaderData.session }} />;
+  return (
+    <div data-display="flex" data-direction="column">
+      <div data-display="flex" data-main="end" data-cross="center" data-gap="24" data-mb="24">
+        <div data-display="flex" data-cross="center" data-gap="6" {...UI.Colorful("brand-600").style.color}>
+          <ProfileCircle height={20} width={20} /> {loaderData.session?.user.email}
+        </div>
+
+        <LogoutButton />
+      </div>
+      <Outlet context={{ ...loaderData.session }} />;
+    </div>
+  );
 }
