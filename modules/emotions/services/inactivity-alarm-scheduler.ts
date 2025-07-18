1f24ffabf73309fcbbd1f47c2275204e5fa5f3ac
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-18 23:11:23 +0200

    feature: implement inactivity alarm scheduler

--- modules/emotions/services/inactivity-alarm-scheduler.ts
+++ modules/emotions/services/inactivity-alarm-scheduler.ts
@@ -1,9 +1,12 @@
 import * as Auth from "+auth";
+import * as Commands from "+emotions/commands";
 import * as Policies from "+emotions/policies";
-// import * as Commands from "+emotions/commands";
-// import * as VO from "+emotions/queries";
 import * as Queries from "+emotions/queries";
+import * as Alarms from "+emotions/services/alarms";
+import * as VO from "+emotions/value-objects";
+import { CommandBus } from "+infra/command-bus";
 import * as bg from "@bgord/bun";
+import * as tools from "@bgord/tools";
 
 export class InactivityAlarmScheduler {
   static cron = `0 18 * * ${bg.UTC_DAY_OF_THE_WEEK.Wednesday}`; // 18:00 UTC
@@ -16,6 +19,30 @@ export class InactivityAlarmScheduler {
     for (const userId of userIds) {
       const lastEntryTimestamp = await Queries.GetLatestEntryTimestampForUser.execute(userId);
       await Policies.NoEntriesInTheLastWeek.perform({ lastEntryTimestamp });
+
+      const detection: Alarms.AlarmApplicableCheckOutputType = {
+        name: VO.AlarmNameOption.INACTIVITY_ALARM,
+        trigger: {
+          type: Alarms.AlarmTriggerEnum.inactivity,
+          inactivityDays: 7,
+          lastEntryTimestamp: tools.Timestamp.parse(lastEntryTimestamp),
+        },
+        applicable: true,
+      };
+
+      const command = Commands.GenerateAlarmCommand.parse({
+        id: bg.NewUUID.generate(),
+        correlationId: bg.CorrelationStorage.get(),
+        name: Commands.GENERATE_ALARM_COMMAND,
+        createdAt: tools.Timestamp.parse(Date.now()),
+        payload: {
+          alarmName: detection.name,
+          trigger: detection.trigger,
+          userId,
+        },
+      } satisfies Commands.GenerateAlarmCommandType);
+
+      await CommandBus.emit(command.name, command);
     }
   }
 }
