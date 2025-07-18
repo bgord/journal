dc050383e2dbad76c371f8d5c6aee7dc9274bcc8
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-18 18:10:55 +0200

    step: add GetLatestEntryTimestampForUser

---
+++ modules/emotions/queries/get-latest-entry-timestamp-for-user.ts
@@ -0,0 +1,15 @@
+import type * as Auth from "+auth";
+import { db } from "+infra/db";
+import * as Schema from "+infra/schema";
+import { eq, sql } from "drizzle-orm";
+
+export class GetLatestEntryTimestampForUser {
+  static async execute(userId: Auth.VO.UserIdType): Promise<number | undefined> {
+    const result = await db
+      .select({ max: sql<number>`max(${Schema.entries.startedAt})` })
+      .from(Schema.entries)
+      .where(eq(Schema.entries.userId, userId));
+
+    return result[0]?.max;
+  }
+}
--- modules/emotions/queries/index.ts
+++ modules/emotions/queries/index.ts
@@ -1,3 +1,4 @@
 export * from "./count-alarms-for-user";
 export * from "./count-entries-per-week-for-user";
 export * from "./count-todays-alarms-for-user";
+export * from "./get-latest-entry-timestamp-for-user";
