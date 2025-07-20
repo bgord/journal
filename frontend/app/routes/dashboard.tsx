c981321e46fc0c9d86ec6cbeeb252957b094ed4c
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-20 20:23:34 +0200

    refactor: make alarms a dashboard section

--- frontend/app/routes/dashboard.tsx
+++ frontend/app/routes/dashboard.tsx
@@ -21,79 +21,83 @@ export default function Dashboard(props: Route.ComponentProps) {
   const t = UI.useTranslations();
 
   return (
-    <main data-pb="36" data-fs="12" data-p="12" {...UI.Rhythm(475).times(1).style.width}>
-      <h2 data-mb="12">
-        {t("alarm.header")} <AlarmIcon height={20} width={20} data-color="red-500" />
-      </h2>
-
-      <div
-        data-display="flex"
-        data-direction="column"
-        data-gap="12"
-        data-p="24"
-        data-bc="gray-200"
-        data-bw="1"
-        data-br="4"
-        data-shadow="sm"
-        {...UI.Colorful("surface-card").style.background}
-      >
-        <h2 data-display="flex" data-gap="12">
-          {t("alarm.inactivity")}
-          <div className="c-badge">{props.loaderData.alarms.inactivity.length}</div>
+    <main data-display="flex">
+      <section data-pb="36" data-fs="12" data-p="12" {...UI.Rhythm(475).times(1).style.width}>
+        <h2 data-mb="12">
+          {t("dashboard.alarm.header")} <AlarmIcon height={20} width={20} data-color="red-500" />
         </h2>
 
-        <ul data-display="flex" data-direction="column" data-gap="12" data-mt="12">
-          {props.loaderData.alarms.inactivity.map((alarm) => (
-            <li key={alarm.id}>
-              <div data-display="flex" data-gap="6">
-                <div>{alarm.generatedAt}</div>
+        <div
+          data-display="flex"
+          data-direction="column"
+          data-gap="12"
+          data-p="24"
+          data-bc="gray-200"
+          data-bw="1"
+          data-br="4"
+          data-shadow="sm"
+          {...UI.Colorful("surface-card").style.background}
+        >
+          <h2 data-display="flex" data-gap="12">
+            {t("dashboard.alarm.inactivity")}
+            <div className="c-badge">{props.loaderData.alarms.inactivity.length}</div>
+          </h2>
 
-                <div {...UI.Colorful("brand-600").style.color}>
-                  {t("alarm.inactivity.duration", { inactivityDays: String(alarm.inactivityDays) })}
+          <ul data-display="flex" data-direction="column" data-gap="12" data-mt="12">
+            {props.loaderData.alarms.inactivity.map((alarm) => (
+              <li key={alarm.id}>
+                <div data-display="flex" data-gap="6">
+                  <div>{alarm.generatedAt}</div>
+
+                  <div {...UI.Colorful("brand-600").style.color}>
+                    {t("dashboard.alarm.inactivity.duration", {
+                      inactivityDays: String(alarm.inactivityDays),
+                    })}
+                  </div>
+
+                  <div data-ml="12">"{alarm.advice}"</div>
                 </div>
+              </li>
+            ))}
+          </ul>
+        </div>
 
-                <div data-ml="12">"{alarm.advice}"</div>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
+        <div
+          data-display="flex"
+          data-direction="column"
+          data-gap="12"
+          data-mt="24"
+          data-p="24"
+          data-bc="gray-200"
+          data-bw="1"
+          data-br="4"
+          data-shadow="sm"
+          {...UI.Colorful("surface-card").style.background}
+        >
+          <h2 data-display="flex" data-gap="12">
+            {t("dashboard.alarm.entry")}
+            <div className="c-badge">{props.loaderData.alarms.entry.length}</div>
+          </h2>
 
-      <div
-        data-display="flex"
-        data-direction="column"
-        data-gap="12"
-        data-mt="24"
-        data-p="24"
-        data-bc="gray-200"
-        data-bw="1"
-        data-br="4"
-        data-shadow="sm"
-        {...UI.Colorful("surface-card").style.background}
-      >
-        <h2 data-display="flex" data-gap="12">
-          {t("alarm.entry")}
-          <div className="c-badge">{props.loaderData.alarms.entry.length}</div>
-        </h2>
+          <ul data-display="flex" data-direction="column" data-gap="12" data-mt="12">
+            {props.loaderData.alarms.entry.map((alarm) => (
+              <li key={alarm.id}>
+                <div data-display="flex" data-gap="6">
+                  <div>{alarm.generatedAt}</div>
 
-        <ul data-display="flex" data-direction="column" data-gap="12" data-mt="12">
-          {props.loaderData.alarms.entry.map((alarm) => (
-            <li key={alarm.id}>
-              <div data-display="flex" data-gap="6">
-                <div>{alarm.generatedAt}</div>
+                  <div {...UI.Colorful("brand-600").style.color}>
+                    {t(`dashboard.alarm.entry.${alarm.name}.description`, {
+                      emotionLabel: t(`entry.emotion.label.value.${alarm.emotionLabel}`),
+                    })}
+                  </div>
 
-                <div {...UI.Colorful("brand-600").style.color}>
-                  {t(`alarm.entry.${alarm.name}.description`, {
-                    emotionLabel: t(`entry.emotion.label.value.${alarm.emotionLabel}`),
-                  })}
+                  <div data-ml="12">"{alarm.advice}"</div>
                 </div>
-
-                <div data-ml="12">"{alarm.advice}"</div>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </section>
     </main>
   );
 }
