cd0e676d200c3f360c562401f82fee01915da3fe
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-21 22:12:14 +0200

    step: handle empty dashboard states

--- frontend/app/routes/dashboard.tsx
+++ frontend/app/routes/dashboard.tsx
@@ -25,6 +25,8 @@ export async function loader({ request }: Route.LoaderArgs) {
 export default function Dashboard({ loaderData }: Route.ComponentProps) {
   const t = UI.useTranslations();
 
+  console.log(loaderData.entries.topReactions[0]);
+
   return (
     <main data-display="flex" data-direction="column">
       <ul data-display="flex" data-p="12" data-ml="6">
@@ -128,70 +130,94 @@ export default function Dashboard({ loaderData }: Route.ComponentProps) {
             </div>
           </Components.DashboardCell>
 
-          <Components.DashboardCell data-mt="24">
-            <h2 data-display="flex" data-gap="12">
-              {t("dashboard.entries.top_emotions")}
-            </h2>
+          {loaderData.entries.topEmotions.today[0] &&
+            loaderData.entries.topEmotions.lastWeek[0] &&
+            loaderData.entries.topEmotions.all[0] && (
+              <Components.DashboardCell data-mt="24">
+                <h2 data-display="flex" data-gap="12">
+                  {t("dashboard.entries.top_emotions")}
+                </h2>
 
-            <div data-display="flex" data-main="between" data-px="12">
-              <div data-display="flex" data-direction="column" data-cross="center" data-gap="12" data-fs="12">
-                <div {...UI.Colorful("brand-500").style.color}>{t("dashboard.entries.today")}</div>
+                <div data-display="flex" data-main="between" data-px="12">
+                  <div
+                    data-display="flex"
+                    data-direction="column"
+                    data-cross="center"
+                    data-gap="12"
+                    data-fs="12"
+                  >
+                    <div {...UI.Colorful("brand-500").style.color}>{t("dashboard.entries.today")}</div>
 
-                <ul data-display="flex" data-direction="column" data-gap="24">
-                  {loaderData.entries.topEmotions.today.map((stat) => (
-                    <li data-display="flex" data-gap="6">
-                      <div className="c-badge">{stat.hits}</div>
-                      <div>{t(`entry.emotion.label.value.${stat.label}`)}</div>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-
-              <div data-display="flex" data-direction="column" data-cross="center" data-gap="12" data-fs="12">
-                <div {...UI.Colorful("brand-500").style.color}>{t("dashboard.entries.last_week")}</div>
-
-                <ul data-display="flex" data-direction="column" data-gap="24">
-                  {loaderData.entries.topEmotions.lastWeek.map((stat) => (
-                    <li data-display="flex" data-gap="6">
-                      <div className="c-badge">{stat.hits}</div>
-                      <div>{t(`entry.emotion.label.value.${stat.label}`)}</div>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-
-              <div data-display="flex" data-direction="column" data-cross="center" data-gap="12" data-fs="12">
-                <div {...UI.Colorful("brand-500").style.color}>{t("dashboard.entries.all")}</div>
-
-                <ul data-display="flex" data-direction="column" data-gap="24">
-                  {loaderData.entries.topEmotions.all.map((stat) => (
-                    <li data-display="flex" data-gap="6">
-                      <div className="c-badge">{stat.hits}</div>
-                      <div>{t(`entry.emotion.label.value.${stat.label}`)}</div>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          </Components.DashboardCell>
-
-          <Components.DashboardCell data-mt="24">
-            <h2 data-display="flex" data-gap="12">
-              {t("dashboard.entries.reactions")}
-            </h2>
-
-            <ul data-display="flex" data-direction="column" data-gap="24">
-              {loaderData.entries.topReactions.map((reaction) => (
-                <li data-display="flex" data-direction="column" data-gap="12">
-                  <div data-display="flex" data-gap="12">
-                    <div className="c-badge">{reaction.reactionEffectiveness} / 5</div>
-                    <div>{t(`entry.reaction.type.value.${reaction.reactionType}`)}</div>
+                    <ul data-display="flex" data-direction="column" data-gap="24">
+                      {loaderData.entries.topEmotions.today.map((stat) => (
+                        <li data-display="flex" data-gap="6">
+                          <div className="c-badge">{stat.hits}</div>
+                          <div>{t(`entry.emotion.label.value.${stat.label}`)}</div>
+                        </li>
+                      ))}
+                    </ul>
                   </div>
-                  <div data-ml="12">"{reaction.reactionDescription}"</div>
-                </li>
-              ))}
-            </ul>
-          </Components.DashboardCell>
+
+                  <div
+                    data-display="flex"
+                    data-direction="column"
+                    data-cross="center"
+                    data-gap="12"
+                    data-fs="12"
+                  >
+                    <div {...UI.Colorful("brand-500").style.color}>{t("dashboard.entries.last_week")}</div>
+
+                    <ul data-display="flex" data-direction="column" data-gap="24">
+                      {loaderData.entries.topEmotions.lastWeek.map((stat) => (
+                        <li data-display="flex" data-gap="6">
+                          <div className="c-badge">{stat.hits}</div>
+                          <div>{t(`entry.emotion.label.value.${stat.label}`)}</div>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+
+                  <div
+                    data-display="flex"
+                    data-direction="column"
+                    data-cross="center"
+                    data-gap="12"
+                    data-fs="12"
+                  >
+                    <div {...UI.Colorful("brand-500").style.color}>{t("dashboard.entries.all")}</div>
+
+                    <ul data-display="flex" data-direction="column" data-gap="24">
+                      {loaderData.entries.topEmotions.all.map((stat) => (
+                        <li data-display="flex" data-gap="6">
+                          <div className="c-badge">{stat.hits}</div>
+                          <div>{t(`entry.emotion.label.value.${stat.label}`)}</div>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              </Components.DashboardCell>
+            )}
+
+          {loaderData.entries.topReactions[0] && (
+            <Components.DashboardCell data-mt="24">
+              <h2 data-display="flex" data-gap="12">
+                {t("dashboard.entries.reactions")}
+              </h2>
+
+              <ul data-display="flex" data-direction="column" data-gap="24">
+                {loaderData.entries.topReactions.map((reaction) => (
+                  <li data-display="flex" data-direction="column" data-gap="12">
+                    <div data-display="flex" data-gap="12">
+                      <div className="c-badge">{reaction.reactionEffectiveness} / 5</div>
+                      <div>{t(`entry.reaction.type.value.${reaction.reactionType}`)}</div>
+                    </div>
+                    <div data-ml="12">"{reaction.reactionDescription}"</div>
+                  </li>
+                ))}
+              </ul>
+            </Components.DashboardCell>
+          )}
         </section>
       </div>
     </main>
