f314c44f7870467bbcb2693ce23282aa3a4676c3
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-24 12:12:04 +0200

    fix: count alarms for entry query filename

--- modules/emotions/queries/index.ts
+++ modules/emotions/queries/index.ts
@@ -1,4 +1,4 @@
-export * from "./count-alarms-for-user";
+export * from "./count-alarms-for-entry";
 export * from "./count-entries-per-week-for-user";
 export * from "./count-todays-alarms-for-user";
 export * from "./get-latest-entry-timestamp-for-user";
