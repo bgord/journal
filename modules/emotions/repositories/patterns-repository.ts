8f8ffc4a1b4a6dc95def79e41ff9943f88a15ccf
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-24 12:16:38 +0200

    step: implement pattern repo findInWeekForUser

--- modules/emotions/repositories/patterns-repository.ts
+++ modules/emotions/repositories/patterns-repository.ts
@@ -1,6 +1,9 @@
+import type * as Auth from "+auth";
 import * as Patterns from "+emotions/services/patterns";
 import { db } from "+infra/db";
 import * as Schema from "+infra/schema";
+import * as tools from "@bgord/tools";
+import { and, eq } from "drizzle-orm";
 
 export class PatternsRepository {
   static async create(event: Patterns.PatternDetectionEventType) {
@@ -11,5 +14,12 @@ export class PatternsRepository {
       createdAt: event.createdAt,
       weekIsoId: event.payload.weekIsoId,
     });
+  }
+
+  static async findInWeekForUser(week: tools.Week, userId: Auth.VO.UserIdType) {
+    return db
+      .select()
+      .from(Schema.patternDetections)
+      .where(and(eq(Schema.patternDetections.weekIsoId, week.toIsoId()), eq(Schema.entries.userId, userId)));
   }
 }
