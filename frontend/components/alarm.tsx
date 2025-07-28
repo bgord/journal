59df57032574d85926df0a30853009c750af51d1
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-28 12:19:00 +0200

    step: make alarm icons smaller

--- frontend/components/alarm.tsx
+++ frontend/components/alarm.tsx
@@ -6,14 +6,15 @@ export function Alarm(props: EntryType["alarms"][number]) {
   const t = UI.useTranslations();
 
   return (
-    <li key={props.id} data-disp="flex" data-gap="4" data-mt="3">
-      <div data-disp="flex" data-gap="1" data-fs="sm" data-color="neutral-300">
-        <Icons.Alarm data-size="md" data-color="danger-200" />
+    <li key={props.id} data-disp="flex" data-gap="2" data-mt="3">
+      <div data-disp="flex" data-cross="center" data-gap="2" data-fs="sm" data-color="neutral-300">
+        <Icons.Alarm data-size="sm" data-color="danger-200" />
         {t(`alarm.name.${props.name}`)} alarm for
         <div className="c-badge">{t(`entry.emotion.label.value.${props.emotionLabel}`)}</div>
       </div>
 
-      <div data-fs="sm" data-color="brand-100" data-ml="8">
+      <div data-disp="flex" data-cross="center" data-gap="2" data-fs="sm" data-color="brand-100">
+        <Icons.Sparks data-size="sm" />
         {props.advice}
       </div>
     </li>
