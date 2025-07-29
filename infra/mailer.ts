f24e0b9ac7533c2ea417867ac2eeb1d52d47bf24
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-29 23:09:12 +0200

    step: add runtime mailer adapter choosing

--- infra/mailer.ts
+++ infra/mailer.ts
@@ -1,9 +1,13 @@
-import { Env } from "+infra/env";
+import { Env, MailerAdapter } from "+infra/env";
+import { logger } from "+infra/logger";
 import * as bg from "@bgord/bun";
 
-export const Mailer = new bg.SmtpMailerAdapter({
-  SMTP_HOST: Env.SMTP_HOST,
-  SMTP_PORT: Env.SMTP_PORT,
-  SMTP_USER: Env.SMTP_USER,
-  SMTP_PASS: Env.SMTP_PASS,
-});
+export const Mailer = {
+  [MailerAdapter.smtp]: new bg.SmtpMailerAdapter({
+    SMTP_HOST: Env.SMTP_HOST,
+    SMTP_PORT: Env.SMTP_PORT,
+    SMTP_USER: Env.SMTP_USER,
+    SMTP_PASS: Env.SMTP_PASS,
+  }),
+  [MailerAdapter.noop]: new bg.NoopMailerAdapter(logger),
+}[Env.MAILER_ADAPTER];
