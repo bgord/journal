2df5cab45cb3814f99b6ed5a971ab9219cc152b3
Bartosz Gordon <gordonbartosz@gmail.com>
2025-08-01 23:40:45 +0200

    step: setup CountShareableLinksPerOwner

---
+++ modules/publishing/queries/count-shareable-links-per-owner.ts
@@ -0,0 +1,8 @@
+import * as Auth from "+auth";
+
+export class CountShareableLinksPerOwner {
+  static async execute(_ownerid: Auth.VO.UserIdType): Promise<number> {
+    // TODO: Implement it
+    return 0;
+  }
+}
