35694c1fe5b2514d2e189fe2c6bf06955e94f1d9
Bartosz Gordon <gordonbartosz@gmail.com>
2025-08-02 23:26:40 +0200

    fix: set conditional alias to resolve react-dom server to node

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,16 +4,16 @@ import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  plugins: [
-    reactRouter(),
-    tsconfigPaths(),
-    ValidateEnv({ validator: "builtin", schema: { VITE_API_URL: Schema.string() } }),
-  ],
-  resolve: {
-    dedupe: ["react", "react-dom", "react/jsx-runtime"],
-    alias: {
-      "react-dom/server": "react-dom/server.node",
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [
+      reactRouter(),
+      tsconfigPaths(),
+      ValidateEnv({ validator: "builtin", schema: { VITE_API_URL: Schema.string() } }),
+    ],
+    resolve: {
+      dedupe: ["react", "react-dom", "react/jsx-runtime"],
+      alias: command === "build" ? { "react-dom/server": "react-dom/server.node" } : undefined,
     },
-  },
+  };
 });
