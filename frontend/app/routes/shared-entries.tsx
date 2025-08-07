6d77484491e20be69ccadfbcc5a9379d147ccf13
Bartosz Gordon <gordonbartosz@gmail.com>
2025-08-07 23:13:51 +0200

    step: handle empty shared entries

--- frontend/app/routes/shared-entries.tsx
+++ frontend/app/routes/shared-entries.tsx
@@ -1,5 +1,7 @@
+import * as UI from "@bgord/ui";
 import type { SelectEntriesWithAlarms } from "../../../infra/schema";
 import { API } from "../../api";
+import NotebookSvg from "../../assets/notebook.svg";
 import * as Components from "../../components";
 import type { Route } from "./+types/shared-entries";
 
@@ -9,6 +11,8 @@ export async function loader({ params }: Route.LoaderArgs) {
 }
 
 export default function SharedEntries({ loaderData }: Route.ComponentProps) {
+  const t = UI.useTranslations();
+
   return (
     <main data-stack="y">
       <header data-stack="x" data-main="between" data-cross="center" data-p="3">
@@ -21,6 +25,18 @@ export default function SharedEntries({ loaderData }: Route.ComponentProps) {
           <Components.ReadOnlyEntry key={entry.id} {...entry} />
         ))}
       </ul>
+
+      {loaderData.length === 0 && (
+        <div data-stack="y" data-cross="center">
+          <img
+            src={NotebookSvg}
+            data-animation="grow-fade-in"
+            height="300px"
+            alt={t("entry.list.empty.alt")}
+          />
+          <div data-color="brand-300">{t("shared_entries.list.empty")}</div>
+        </div>
+      )}
     </main>
   );
 }
