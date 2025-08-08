5279b97496d1401fb97ea65faf2e91d948da9c36
Bartosz Gordon <gordonbartosz@gmail.com>
2025-08-08 23:54:21 +0200

    step: make bucket dependant on context

--- modules/ai/value-objects/quota-rule.ts
+++ modules/ai/value-objects/quota-rule.ts
@@ -1,10 +1,11 @@
 import { QuotaWindow } from "./quota-window";
+import { RequestContext } from "./request-context";
 import { UsageCategoryType } from "./usage-category";
 
 export type QuotaRule = {
   id: string;
   window: QuotaWindow;
   limit: number;
-  bucket: () => string;
+  bucket: (context: RequestContext) => string;
   appliesTo: (category: UsageCategoryType) => boolean;
 };
