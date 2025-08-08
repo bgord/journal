d0cca9a13f5f598465f3ce7ff11b12d3beae6212
Bartosz Gordon <gordonbartosz@gmail.com>
2025-08-08 13:39:26 +0200

    step: setup schedule time capsule entry command

---
+++ modules/emotions/commands/SCHEDULE_TIME_CAPSULE_ENTRY.ts
@@ -0,0 +1,29 @@
+import * as Auth from "+auth";
+import * as Entities from "+emotions/entities";
+import * as VO from "+emotions/value-objects";
+import { SupportedLanguages } from "+infra/i18n";
+import * as bg from "@bgord/bun";
+import * as tools from "@bgord/tools";
+import { z } from "zod/v4";
+
+export const SCHEDULE_TIME_CAPSULE_ENTRY_COMMAND = "SCHEDULE_TIME_CAPSULE_ENTRY_COMMAND";
+
+export const ScheduleTimeCapsuleEntryCommand = z.object({
+  id: bg.UUID,
+  correlationId: bg.UUID,
+  createdAt: tools.Timestamp,
+  name: z.literal(SCHEDULE_TIME_CAPSULE_ENTRY_COMMAND),
+  payload: z.object({
+    entryId: VO.EntryId,
+    situation: z.instanceof(Entities.Situation),
+    emotion: z.instanceof(Entities.Emotion),
+    reaction: z.instanceof(Entities.Reaction),
+    language: z.enum(SupportedLanguages),
+    userId: Auth.VO.UserId,
+    scheduledAt: tools.Timestamp,
+    // TODO : > now
+    scheduledFor: tools.Timestamp,
+  }),
+});
+
+export type ScheduleTimeCapsuleEntryCommandType = z.infer<typeof ScheduleTimeCapsuleEntryCommand>;
