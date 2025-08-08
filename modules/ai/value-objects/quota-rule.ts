d9d5ab5d982e1fc99ee273254dd1ad6a9fc17dc4
Bartosz Gordon <gordonbartosz@gmail.com>
2025-08-08 23:53:45 +0200

    step: make quota appliesTo base on categories

--- modules/ai/value-objects/quota-rule.ts
+++ modules/ai/value-objects/quota-rule.ts
@@ -1,9 +1,10 @@
 import { QuotaWindow } from "./quota-window";
+import { UsageCategoryType } from "./usage-category";
 
 export type QuotaRule = {
   id: string;
   window: QuotaWindow;
   limit: number;
   bucket: () => string;
-  appliesTo: () => boolean;
+  appliesTo: (category: UsageCategoryType) => boolean;
 };
