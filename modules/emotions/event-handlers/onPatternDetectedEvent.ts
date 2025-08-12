aa8656f4ecfeca45beeb177e71f4e34b8c6538a5
Bartosz Gordon <gordonbartosz@gmail.com>
2025-08-12 20:21:18 +0200

    refactor: cleanup event handlers

--- modules/emotions/event-handlers/onPatternDetectedEvent.ts
+++ modules/emotions/event-handlers/onPatternDetectedEvent.ts
@@ -1,5 +1,13 @@
 import * as Emotions from "+emotions";
+import { db } from "+infra/db";
+import * as Schema from "+infra/schema";
 
 export const onPatternDetectedEvent = async (event: Emotions.Services.Patterns.PatternDetectionEventType) => {
-  await Emotions.Repos.PatternsRepository.create(event);
+  await db.insert(Schema.patternDetections).values({
+    id: event.id,
+    name: event.payload.name,
+    userId: event.payload.userId,
+    createdAt: event.createdAt,
+    weekIsoId: event.payload.weekIsoId,
+  });
 };
