3cc22d7b362867e54a1df1354341cee4895f6ae9
Bartosz Gordon <gordonbartosz@gmail.com>
2025-08-13 16:33:08 +0200

    step: clean up auth bc imports

--- modules/auth/repositories/user-repository.ts
+++ modules/auth/repositories/user-repository.ts
@@ -1,17 +1,17 @@
+import * as VO from "+auth/value-objects";
 import { db } from "+infra/db";
 import * as Schema from "+infra/schema";
 import { eq } from "drizzle-orm";
-import { UserIdType } from "../value-objects/user-id";
 
 export class UserRepository {
-  static async getEmailFor(userId: UserIdType) {
+  static async getEmailFor(userId: VO.UserIdType) {
     return db.query.users.findFirst({
       where: eq(Schema.users.id, userId),
       columns: { email: true },
     });
   }
 
-  static async listIds(): Promise<UserIdType[]> {
+  static async listIds(): Promise<VO.UserIdType[]> {
     const result = await db.query.users.findMany({
       columns: { id: true },
     });
