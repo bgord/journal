0f8bffdb164fe3271c4f8a70b7295cb615546500
Bartosz Gordon <gordonbartosz@gmail.com>
2025-08-16 09:37:16 +0200

    step: define a port for history writer

--- modules/history/services/history-writer.ts
+++ modules/history/services/history-writer.ts
@@ -1,34 +1,6 @@
-import * as bg from "@bgord/bun";
-import * as tools from "@bgord/tools";
 import * as History from "+history";
-import { EventStore } from "+infra/event-store";
 
-export class HistoryWriter {
-  static async populate(history: History.VO.HistoryType) {
-    const event = History.Events.HistoryPopulatedEvent.parse({
-      id: crypto.randomUUID(),
-      correlationId: bg.CorrelationStorage.get(),
-      createdAt: tools.Time.Now().value,
-      name: History.Events.HISTORY_POPULATED_EVENT,
-      stream: "history",
-      version: 1,
-      payload: history,
-    } satisfies History.Events.HistoryPopulatedEventType);
-
-    return EventStore.save([event]);
-  }
-
-  static async clear(subject: History.VO.HistorySubjectType) {
-    const event = History.Events.HistoryClearedEvent.parse({
-      id: crypto.randomUUID(),
-      correlationId: bg.CorrelationStorage.get(),
-      createdAt: tools.Time.Now().value,
-      name: History.Events.HISTORY_CLEARED_EVENT,
-      stream: "history",
-      version: 1,
-      payload: { subject },
-    }) satisfies History.Events.HistoryClearedEventType;
-
-    return EventStore.save([event]);
-  }
+export interface HistoryWriterPort {
+  populate(history: History.VO.HistoryType): Promise<void>;
+  clear(subject: History.VO.HistorySubjectType): Promise<void>;
 }
