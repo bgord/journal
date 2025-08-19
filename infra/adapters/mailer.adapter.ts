12e2419ced0c401f450bb3668dc29579b3fc0262
Bartosz Gordon <gordonbartosz@gmail.com>
2025-08-19 21:50:01 +0200

    refactor: use new mailer adapter names

--- infra/adapters/mailer.adapter.ts
+++ infra/adapters/mailer.adapter.ts
@@ -3,11 +3,11 @@ import { Env, MailerAdapter } from "+infra/env";
 import { logger } from "+infra/logger";
 
 export const Mailer = {
-  [MailerAdapter.smtp]: new bg.SmtpMailerAdapter({
+  [MailerAdapter.smtp]: new bg.MailerSmtpAdapter({
     SMTP_HOST: Env.SMTP_HOST,
     SMTP_PORT: Env.SMTP_PORT,
     SMTP_USER: Env.SMTP_USER,
     SMTP_PASS: Env.SMTP_PASS,
   }),
-  [MailerAdapter.noop]: new bg.NoopMailerAdapter(logger),
+  [MailerAdapter.noop]: new bg.MailerNoopAdapter(logger),
 }[Env.MAILER_ADAPTER];
