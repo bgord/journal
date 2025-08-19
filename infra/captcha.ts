33b04b358cf6218a2a86bac6aa1fa8c97f72180a
Bartosz Gordon <gordonbartosz@gmail.com>
2025-08-19 22:09:19 +0200

    fix: key parsing

--- infra/captcha.ts
+++ infra/captcha.ts
@@ -1,9 +1,11 @@
 import * as bg from "@bgord/bun";
 import { Env } from "+infra/env";
 
+const HCAPTCHA_SECRET_KEY = Env.HCAPTCHA_SECRET_KEY as bg.HCaptchaSecretKeyType;
+
 export const CaptchaShield = {
-  [bg.NodeEnvironmentEnum.local]: new bg.CaptchaShieldHcaptchaLocal(Env.HCAPTCHA_SECRET_KEY),
+  [bg.NodeEnvironmentEnum.local]: new bg.CaptchaShieldHcaptchaLocal(HCAPTCHA_SECRET_KEY),
   [bg.NodeEnvironmentEnum.test]: new bg.CaptchaShieldNoop(),
-  [bg.NodeEnvironmentEnum.staging]: new bg.CaptchaShieldHcaptcha(Env.HCAPTCHA_SECRET_KEY),
-  [bg.NodeEnvironmentEnum.production]: new bg.CaptchaShieldHcaptcha(Env.HCAPTCHA_SECRET_KEY),
+  [bg.NodeEnvironmentEnum.staging]: new bg.CaptchaShieldHcaptcha(HCAPTCHA_SECRET_KEY),
+  [bg.NodeEnvironmentEnum.production]: new bg.CaptchaShieldHcaptcha(HCAPTCHA_SECRET_KEY),
 }[Env.type];
