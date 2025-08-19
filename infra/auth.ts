c4008267046e4041c7a76c574de01a182f2d792b
Bartosz Gordon <gordonbartosz@gmail.com>
2025-08-19 22:19:56 +0200

    step: setup better auth hcaptcha

--- infra/auth.ts
+++ infra/auth.ts
@@ -2,7 +2,7 @@ import * as bg from "@bgord/bun";
 import * as tools from "@bgord/tools";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { haveIBeenPwned, openAPI } from "better-auth/plugins";
+import { captcha, haveIBeenPwned, openAPI } from "better-auth/plugins";
 import * as Auth from "+auth";
 import { Mailer } from "+infra/adapters";
 import { db } from "./db";
@@ -73,9 +73,13 @@ export const auth = betterAuth({
   },
   autoSignIn: false,
   trustedOrigins: ["http://localhost:5173", "http://localhost:3000"],
-  plugins: [openAPI(), Env.type === bg.NodeEnvironmentEnum.production ? haveIBeenPwned() : undefined].filter(
-    (plugin) => plugin !== undefined,
-  ),
+  plugins: [
+    openAPI(),
+    Env.type === bg.NodeEnvironmentEnum.production
+      ? captcha({ provider: "hcaptcha", secretKey: Env.HCAPTCHA_SECRET_KEY })
+      : undefined,
+    Env.type === bg.NodeEnvironmentEnum.production ? haveIBeenPwned() : undefined,
+  ].filter((plugin) => plugin !== undefined),
   logger: new bg.BetterAuthLogger(logger).attach(),
 });
 
