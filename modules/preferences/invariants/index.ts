2f52b0bbd5e16fbb4a2ba6fb68d45ae510ec69be
Bartosz Gordon <gordonbartosz@gmail.com>
2025-08-22 17:29:00 +0200

    step: add user language has changed event

---
+++ modules/preferences/invariants/index.ts
@@ -0,0 +1,1 @@
+export * from "./user-language-has-changed";
---
+++ modules/preferences/invariants/user-language-has-changed.ts
@@ -0,0 +1,26 @@
+import * as bg from "@bgord/bun";
+import type { ContentfulStatusCode } from "hono/utils/http-status";
+import type { SupportedLanguages } from "+languages";
+
+class UserLanguageHasChangedError extends Error {
+  constructor() {
+    super();
+    Object.setPrototypeOf(this, UserLanguageHasChangedError.prototype);
+  }
+}
+
+type UserLanguageHasChangedConfigType = { current: SupportedLanguages; next: SupportedLanguages };
+
+class UserLanguageHasChangedFactory extends bg.Invariant<UserLanguageHasChangedConfigType> {
+  fails(config: UserLanguageHasChangedConfigType) {
+    return config.current === config.next;
+  }
+
+  message = "UserLanguageHasChanged";
+
+  error = UserLanguageHasChangedError;
+
+  code = 403 as ContentfulStatusCode;
+}
+
+export const UserLanguageHasChanged = new UserLanguageHasChangedFactory();
