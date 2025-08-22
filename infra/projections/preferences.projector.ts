f32c8ede2306b65efadc61c4f6c99fe3137fa574
Bartosz Gordon <gordonbartosz@gmail.com>
2025-08-22 19:29:53 +0200

    step: implement projector

--- infra/projections/preferences.projector.ts
+++ infra/projections/preferences.projector.ts
@@ -1,6 +1,8 @@
 import type * as bg from "@bgord/bun";
 import * as Preferences from "+preferences";
+import { db } from "+infra/db";
 import type { EventBus } from "+infra/event-bus";
+import * as Schema from "+infra/schema";
 
 export class PreferencesProjector {
   constructor(eventBus: typeof EventBus, EventHandler: bg.EventHandler) {
@@ -10,5 +12,18 @@ export class PreferencesProjector {
     );
   }
 
-  async onUserLanguageSetEvent(_event: Preferences.Events.UserLanguageSetEventType) {}
+  async onUserLanguageSetEvent(event: Preferences.Events.UserLanguageSetEventType) {
+    await db
+      .insert(Schema.userPreferences)
+      .values({
+        userId: event.payload.userId,
+        preference: "language",
+        value: event.payload.language,
+        updatedAt: event.createdAt,
+      })
+      .onConflictDoUpdate({
+        target: [Schema.userPreferences.userId, Schema.userPreferences.preference],
+        set: { value: event.payload.language, updatedAt: event.createdAt },
+      });
+  }
 }
