9fc75d2e79493bf745cee1507a569ad40fe98666
Bartosz Gordon <gordonbartosz@gmail.com>
2025-08-22 18:58:45 +0200

    step: define user preferences schema

--- infra/drizzle/0000_lame_metal_master.sql
+++ infra/drizzle/0000_lame_metal_master.sql
@@ -156,6 +156,18 @@ CREATE TABLE `timeCapsuleEntries` (
 	FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
+CREATE TABLE `user_preferences` (
+	`id` text(36) PRIMARY KEY NOT NULL,
+	`userId` text(36) NOT NULL,
+	`preference` text NOT NULL,
+	`value` text NOT NULL,
+	`updatedAt` integer NOT NULL,
+	FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `user_preferences_userId_preference_uidx` ON `user_preferences` (`userId`,`preference`);--> statement-breakpoint
+CREATE INDEX `user_preferences_userId_idx` ON `user_preferences` (`userId`);--> statement-breakpoint
+CREATE INDEX `user_preferences_preference_idx` ON `user_preferences` (`preference`);--> statement-breakpoint
 CREATE TABLE `users` (
 	`id` text PRIMARY KEY NOT NULL,
 	`name` text NOT NULL,
