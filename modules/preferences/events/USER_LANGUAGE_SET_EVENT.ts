658b2553cd51d78f95dbaa7dc0680352a793a431
Bartosz Gordon <gordonbartosz@gmail.com>
2025-08-22 16:49:19 +0200

    step: add user language set event

---
+++ modules/preferences/events/USER_LANGUAGE_SET_EVENT.ts
@@ -0,0 +1,17 @@
+import * as bg from "@bgord/bun";
+import { z } from "zod/v4";
+import * as Auth from "+auth";
+import { SupportedLanguages } from "+languages";
+
+export const USER_LANGUAGE_SET_EVENT = "USER_LANGUAGE_SET_EVENT";
+
+export const UserLanguageSetEvent = z.object({
+  ...bg.EventEnvelopeSchema,
+  name: z.literal(USER_LANGUAGE_SET_EVENT),
+  payload: z.object({
+    userId: Auth.VO.UserId,
+    language: z.enum(SupportedLanguages),
+  }),
+});
+
+export type UserLanguageSetEventType = z.infer<typeof UserLanguageSetEvent>;
---
+++ modules/preferences/events/index.ts
@@ -0,0 +1,1 @@
+export * from "./USER_LANGUAGE_SET_EVENT";
