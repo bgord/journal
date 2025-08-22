f289c5e0814a7ec2f0ec231eef57d9c6e4545f57
Bartosz Gordon <gordonbartosz@gmail.com>
2025-08-22 19:32:07 +0200

    step: implement query

--- infra/adapters/preferences/user-language-query.adapter.ts
+++ infra/adapters/preferences/user-language-query.adapter.ts
@@ -1,10 +1,20 @@
+import { and, eq } from "drizzle-orm";
 import type * as Auth from "+auth";
-import { SupportedLanguages } from "+languages";
+import type { SupportedLanguages } from "+languages";
 import type * as Preferences from "+preferences";
+import { db } from "+infra/db";
+import * as Schema from "+infra/schema";
 
 class UserLanguageQueryAdapterDrizzle implements Preferences.Ports.UserLanguageQueryPort {
-  async get(_userId: Auth.VO.UserIdType) {
-    return SupportedLanguages.en;
+  async get(userId: Auth.VO.UserIdType) {
+    const row = await db.query.userPreferences.findFirst({
+      where: and(
+        eq(Schema.userPreferences.userId, userId),
+        eq(Schema.userPreferences.preference, "language"),
+      ),
+    });
+
+    return row ? (row.value as SupportedLanguages) : null;
   }
 }
 
