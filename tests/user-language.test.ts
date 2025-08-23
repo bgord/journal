66cc47bd692673afca08a6b7f2c4d29c568a4219
Bartosz Gordon <gordonbartosz@gmail.com>
2025-08-23 16:35:08 +0200

    step: improve tests

--- tests/user-language.test.ts
+++ tests/user-language.test.ts
@@ -4,25 +4,44 @@ import * as Preferences from "+preferences";
 import { UserLanguageQueryAdapter } from "+infra/adapters/preferences";
 import * as mocks from "./mocks";
 
-export const UserLanguageOHQ = new Preferences.OHQ.UserLanguageAdapter(
-  UserLanguageQueryAdapter,
-  new Preferences.VO.SupportedLanguagesSet(SUPPORTED_LANGUAGES),
-  new Preferences.Ports.UserLanguageResolverThrowIfMissing(),
-);
-
 describe("UserLanguageOHQ", () => {
   test("happy path", async () => {
     spyOn(UserLanguageQueryAdapter, "get").mockResolvedValue(SupportedLanguages.pl);
+
+    const UserLanguageOHQ = new Preferences.OHQ.UserLanguageAdapter(
+      UserLanguageQueryAdapter,
+      new Preferences.VO.SupportedLanguagesSet(SUPPORTED_LANGUAGES),
+      new Preferences.Ports.UserLanguageResolverThrowIfMissing(),
+    );
 
     const language = await UserLanguageOHQ.get(mocks.userId);
     expect(language).toEqual(SupportedLanguages.pl);
   });
 
-  test("no language", async () => {
+  test("UserLanguageResolverThrowIfMissing", async () => {
     spyOn(UserLanguageQueryAdapter, "get").mockResolvedValue(null);
+
+    const UserLanguageOHQ = new Preferences.OHQ.UserLanguageAdapter(
+      UserLanguageQueryAdapter,
+      new Preferences.VO.SupportedLanguagesSet(SUPPORTED_LANGUAGES),
+      new Preferences.Ports.UserLanguageResolverThrowIfMissing(),
+    );
 
     expect(async () => UserLanguageOHQ.get(mocks.userId)).toThrow(
       Preferences.Ports.UserLanguagePreferenceMissingError,
     );
+  });
+
+  test("UserLanguageResolverThrowIfMissing", async () => {
+    spyOn(UserLanguageQueryAdapter, "get").mockResolvedValue(null);
+
+    const UserLanguageOHQ = new Preferences.OHQ.UserLanguageAdapter(
+      UserLanguageQueryAdapter,
+      new Preferences.VO.SupportedLanguagesSet(SUPPORTED_LANGUAGES),
+      new Preferences.Ports.UserLanguageResolverSystemDefaultFallback(SupportedLanguages.en),
+    );
+
+    const result = await UserLanguageOHQ.get(mocks.userId);
+    expect(result).toEqual(SupportedLanguages.en);
   });
 });
