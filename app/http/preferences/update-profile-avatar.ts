438db1592c895fe25349995f87ef83dcc9d4096b
Bartosz Gordon <gordonbartosz@gmail.com>
2025-08-25 23:54:51 +0200

    refactor: update the command

--- app/http/preferences/update-profile-avatar.ts
+++ app/http/preferences/update-profile-avatar.ts
@@ -1,27 +1,25 @@
-import path from "node:path";
 import * as bg from "@bgord/bun";
+import * as tools from "@bgord/tools";
 import type hono from "hono";
 import type * as infra from "+infra";
 import * as Preferences from "+preferences";
 import { CommandBus } from "+infra/command-bus";
+import { temporaryFile } from "+infra/temporary-file.adapter";
 
-// TODO Filename VO to @bgord/bun?
 export async function UpdateProfileAvatar(c: hono.Context<infra.HonoConfig>) {
   const userId = "xxx";
   const body = await c.req.formData();
   const file = body.get("file") as File;
 
-  const current = path.parse(file.name);
-  // TODO add the dir to prerequisites
-  const temporary = `infra/profile-avatars/${userId}${current.ext}`;
+  const uploaded = tools.Filename.fromString(file.name);
+  const filename = uploaded.withBasename(tools.BasenameSchema.parse(userId));
 
-  // TODO: extract to an adapter - reusable part that will be used to bgord/bun
-  await Bun.write(temporary, file);
+  const temporary = await temporaryFile.write(filename, file);
 
   const command = Preferences.Commands.UpdateProfileAvatarCommand.parse({
     ...bg.createCommandEnvelope(),
     name: Preferences.Commands.UPDATE_PROFILE_AVATAR_COMMAND,
-    payload: { userId: userId, extension: current.ext },
+    payload: { userId: userId, absoluteFilePath: temporary.path.get() },
   } satisfies Preferences.Commands.UpdateProfileAvatarCommandType);
 
   await CommandBus.emit(command.name, command);
--- modules/preferences/commands/UPDATE_PROFILE_AVATAR_COMMAND.ts
+++ modules/preferences/commands/UPDATE_PROFILE_AVATAR_COMMAND.ts
@@ -6,7 +6,7 @@ export const UPDATE_PROFILE_AVATAR_COMMAND = "UPDATE_PROFILE_AVATAR_COMMAND";
 export const UpdateProfileAvatarCommand = z.object({
   ...bg.CommandEnvelopeSchema,
   name: z.literal(UPDATE_PROFILE_AVATAR_COMMAND),
-  payload: z.object({ userId: bg.UUID, extension: z.string().min(1).max(4) }),
+  payload: z.object({ userId: bg.UUID, absoluteFilePath: z.string().min(1) }),
 });
 
 export type UpdateProfileAvatarCommandType = z.infer<typeof UpdateProfileAvatarCommand>;
