b9505ced20ef6181948ead3b28bd6409e85ce6eb
Bartosz Gordon <gordonbartosz@gmail.com>
2025-08-26 16:40:39 +0200

    step: add test adapter

--- infra/adapters/image-formatted.adapter.ts
+++ infra/adapters/image-formatted.adapter.ts
@@ -1,3 +1,11 @@
 import * as bg from "@bgord/bun";
+import { Env } from "+infra/env";
 
-export const ImageFormatter = new bg.ImageFormatterSharpAdapter();
+export const ImageFormatterSharp = new bg.ImageFormatterSharpAdapter();
+
+export const ImageFormatter: bg.ImageFormatterPort = {
+  [bg.NodeEnvironmentEnum.local]: ImageFormatterSharp,
+  [bg.NodeEnvironmentEnum.test]: new bg.ImageFormatterNoopAdapter(),
+  [bg.NodeEnvironmentEnum.staging]: ImageFormatterSharp,
+  [bg.NodeEnvironmentEnum.production]: ImageFormatterSharp,
+}[Env.type];
