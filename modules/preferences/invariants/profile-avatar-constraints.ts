837923a0f5729f1258ebb47f6a949205482b0a20
Bartosz Gordon <gordonbartosz@gmail.com>
2025-08-26 20:22:47 +0200

    step: add profile avatar constraints invariant

---
+++ modules/preferences/invariants/profile-avatar-constraints.ts
@@ -0,0 +1,36 @@
+import * as bg from "@bgord/bun";
+import type * as tools from "@bgord/tools";
+import type { ContentfulStatusCode } from "hono/utils/http-status";
+import * as VO from "+preferences/value-objects";
+
+class ProfileAvatarConstraintsError extends Error {
+  constructor() {
+    super();
+    Object.setPrototypeOf(this, ProfileAvatarConstraintsError.prototype);
+  }
+}
+
+// TODO: use from bgord/bun
+type ProfileAvatarConstraintsConfigType = {
+  height: tools.HeightType;
+  width: tools.WidthType;
+  mime: tools.Mime;
+  size: tools.Size;
+};
+
+class ProfileAvatarConstraintsFactory extends bg.Invariant<ProfileAvatarConstraintsConfigType> {
+  fails(config: ProfileAvatarConstraintsConfigType) {
+    if (config.height > VO.ProfileAvatarMaxSide) return false;
+    if (config.width > VO.ProfileAvatarMaxSide) return false;
+    if (config.size.isGreaterThan(VO.ProfileAvatarMaxSize)) return false;
+    return VO.ProfileAvatarMimeTypes.every((allowed) => !allowed.isSatisfiedBy(config.mime));
+  }
+
+  message = "ProfileAvatarConstraints";
+
+  error = ProfileAvatarConstraintsError;
+
+  code = 400 as ContentfulStatusCode;
+}
+
+export const ProfileAvatarConstraints = new ProfileAvatarConstraintsFactory();
