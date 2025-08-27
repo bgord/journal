15cac4cb290365c68373e604f6208a50c08d31ff
Bartosz Gordon <gordonbartosz@gmail.com>
2025-08-27 14:12:57 +0200

    step: add tmp file storage

--- infra/adapters/remote-file-storage.ts
+++ infra/adapters/remote-file-storage.ts
@@ -1,18 +1,21 @@
 import * as bg from "@bgord/bun";
+import * as tools from "@bgord/tools";
 import { Env } from "+infra/env";
 import { LoggerWinstonLocalAdapter } from "../logger.adapter";
+import { FileHash } from "./file-hash.adapter";
+
+const tmpFileStorage = new bg.RemoteFileStorageDiskAdapter({
+  hasher: FileHash,
+  root: tools.DirectoryPathAbsoluteSchema.parse("/tmp"),
+});
 
 export const RemoteFileStorage: bg.RemoteFileStoragePort = {
-  [bg.NodeEnvironmentEnum.local]: new bg.RemoteFileStorageNoopAdapter({
-    logger: LoggerWinstonLocalAdapter,
-  }),
+  [bg.NodeEnvironmentEnum.local]: tmpFileStorage,
   [bg.NodeEnvironmentEnum.test]: new bg.RemoteFileStorageNoopAdapter({
     logger: LoggerWinstonLocalAdapter,
   }),
   [bg.NodeEnvironmentEnum.staging]: new bg.RemoteFileStorageNoopAdapter({
     logger: LoggerWinstonLocalAdapter,
   }),
-  [bg.NodeEnvironmentEnum.production]: new bg.RemoteFileStorageNoopAdapter({
-    logger: LoggerWinstonLocalAdapter,
-  }),
+  [bg.NodeEnvironmentEnum.production]: tmpFileStorage,
 }[Env.type];
