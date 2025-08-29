b5365ef5e07d6215195605a7df6fafc1e09e8795
Bartosz Gordon <gordonbartosz@gmail.com>
2025-08-29 16:11:01 +0200

    step: add av binary prereq

--- infra/prerequisites.ts
+++ infra/prerequisites.ts
@@ -8,10 +8,7 @@ import { TemporaryFileDirectory } from "+infra/temporary-file.adapter";
 
 export const prerequisites = [
   new bg.PrerequisitePort({ label: "port", port: Env.PORT }),
-  new bg.PrerequisiteTimezoneUTC({
-    label: "timezone",
-    timezone: tools.Timezone.parse(Env.TZ),
-  }),
+  new bg.PrerequisiteTimezoneUTC({ label: "timezone", timezone: tools.Timezone.parse(Env.TZ) }),
   new bg.PrerequisiteRAM({
     label: "RAM",
     enabled: Env.type !== bg.NodeEnvironmentEnum.local,
@@ -21,10 +18,7 @@ export const prerequisites = [
     label: "disk-space",
     minimum: new tools.Size({ unit: tools.SizeUnit.MB, value: 512 }),
   }),
-  new bg.PrerequisiteNode({
-    label: "node",
-    version: tools.PackageVersion.fromStringWithV("v24.4.1"),
-  }),
+  new bg.PrerequisiteNode({ label: "node", version: tools.PackageVersion.fromStringWithV("v24.4.1") }),
   new bg.PrerequisiteBun({
     label: "bun",
     version: tools.PackageVersion.fromString("1.2.19"),
@@ -39,17 +33,12 @@ export const prerequisites = [
     logger: LoggerWinstonProductionAdapter,
     enabled: Env.type === bg.NodeEnvironmentEnum.production,
   }),
-  new bg.PrerequisitePath({
-    label: "temporary-files dir",
-    path: TemporaryFileDirectory,
-  }),
+  new bg.PrerequisitePath({ label: "temporary-files dir", path: TemporaryFileDirectory }),
   new bg.PrerequisiteJobs({
     label: "jobs",
     jobs,
     enabled: Env.type === bg.NodeEnvironmentEnum.production,
   }),
-  new bg.PrerequisiteTranslations({
-    label: "translations",
-    supportedLanguages: SupportedLanguages,
-  }),
+  new bg.PrerequisiteTranslations({ label: "translations", supportedLanguages: SupportedLanguages }),
+  new bg.PrerequisiteBinary({ label: "clamav", binary: "clamscan" }),
 ];
