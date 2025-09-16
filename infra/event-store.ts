d6ab8585ea1569fd55638ea3fea90c7ee8ba3aa4
Bartosz Gordon <gordonbartosz@gmail.com>
2025-09-16 13:16:17 +0200

    refactor: update graceful shutdown

--- infra/event-store.ts
+++ infra/event-store.ts
@@ -69,9 +69,7 @@ export const EventStore = new bg.DispatchingEventStore<AcceptedEvent>(
           await tx.insert(schema.events).values(rows);
           return rows;
         } catch (e: any) {
-          if (e.code === "SQLITE_CONSTRAINT") {
-            throw new tools.RevisionMismatchError();
-          }
+          if (e.code === "SQLITE_CONSTRAINT") throw new tools.RevisionMismatchError();
           throw e;
         }
       });
