9853fdd4804b55522cf6c11d3e0a8e91e269910d
Bartosz Gordon <gordonbartosz@gmail.com>
2025-09-17 23:44:11 +0200

    fix: e2e test

--- infra/e2e/home.spec.ts
+++ infra/e2e/home.spec.ts
@@ -1,9 +1,8 @@
 import { expect, test } from "@playwright/test";
 
-test("Home - layout", async ({ page }) => {
+test("Sign in - layout", async ({ page }) => {
   await page.goto("/");
 
-  const homeLink = page.getByRole("banner").getByRole("link", { name: "Journal" });
-  await expect(homeLink).toBeVisible();
-  await expect(homeLink).toHaveAttribute("href", "/");
+  const header = page.getByText("Journal");
+  await expect(header).toBeVisible();
 });
