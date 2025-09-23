3af65256528d8c4e6a3e329bd171d9cd090908dc
Bartosz Gordon <gordonbartosz@gmail.com>
2025-09-23 11:07:14 +0200

    step: add logger compact adapter

--- infra/adapters/logger.adapter.ts
+++ infra/adapters/logger.adapter.ts
@@ -3,7 +3,10 @@ import { Env } from "+infra/env";
 
 const app = "lobbygow";
 
-const redactor = new bg.RedactorMaskAdapter(bg.RedactorMaskAdapter.DEFAULT_KEYS);
+const redactor = new bg.RedactorCompositeAdapter([
+  new bg.RedactorMaskAdapter(bg.RedactorMaskAdapter.DEFAULT_KEYS),
+  new bg.RedactorCompactAdapter(),
+]);
 
 export const LoggerWinstonLocalAdapter = new bg.LoggerWinstonLocalAdapter({ app, redactor }).create(
   Env.LOGS_LEVEL,
@@ -13,11 +16,11 @@ export const LoggerWinstonProductionAdapter = new bg.LoggerWinstonProductionAdap
   app,
   AXIOM_API_TOKEN: Env.AXIOM_API_TOKEN,
   redactor,
-});
+}).create(Env.LOGS_LEVEL);
 
 export const Logger: bg.LoggerPort = {
   [bg.NodeEnvironmentEnum.local]: LoggerWinstonLocalAdapter,
   [bg.NodeEnvironmentEnum.test]: new bg.LoggerNoopAdapter(),
   [bg.NodeEnvironmentEnum.staging]: new bg.LoggerNoopAdapter(),
-  [bg.NodeEnvironmentEnum.production]: LoggerWinstonProductionAdapter.create(Env.LOGS_LEVEL),
+  [bg.NodeEnvironmentEnum.production]: LoggerWinstonProductionAdapter,
 }[Env.type];
