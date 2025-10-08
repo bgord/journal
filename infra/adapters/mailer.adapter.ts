d240062a37be3ad15b962dfa0d2733fa3dddbc95
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-08 11:44:49 +0200

    refactor: use smtp mailer with logger adapter

--- infra/adapters/mailer.adapter.ts
+++ infra/adapters/mailer.adapter.ts
@@ -1,13 +1,16 @@
 import * as bg from "@bgord/bun";
 import * as Adapters from "+infra/adapters";
+import { Logger } from "+infra/adapters/logger.adapter";
 import { Env, MailerAdapter } from "+infra/env";
 
+const smtpMailer = new bg.MailerSmtpAdapter({
+  SMTP_HOST: Env.SMTP_HOST,
+  SMTP_PORT: Env.SMTP_PORT,
+  SMTP_USER: Env.SMTP_USER,
+  SMTP_PASS: Env.SMTP_PASS,
+});
+
 export const Mailer = {
-  [MailerAdapter.smtp]: new bg.MailerSmtpAdapter({
-    SMTP_HOST: Env.SMTP_HOST,
-    SMTP_PORT: Env.SMTP_PORT,
-    SMTP_USER: Env.SMTP_USER,
-    SMTP_PASS: Env.SMTP_PASS,
-  }),
+  [MailerAdapter.smtp]: new bg.MailerSmtpWithLoggerAdapter({ smtpMailer: smtpMailer, logger: Logger }),
   [MailerAdapter.noop]: new bg.MailerNoopAdapter(Adapters.Logger),
 }[Env.MAILER_ADAPTER];
