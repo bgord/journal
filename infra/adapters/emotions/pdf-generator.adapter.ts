74f9dd65064e725c3be9624b8517d5aad8f22dbf
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-08 11:51:54 +0200

    step: enable react pdf generator in prod

--- infra/adapters/emotions/pdf-generator.adapter.ts
+++ infra/adapters/emotions/pdf-generator.adapter.ts
@@ -3,7 +3,9 @@ import { LoggerWinstonLocalAdapter } from "+infra/adapters";
 import { Env } from "+infra/env";
 import { PdfGeneratorReact } from "./pdf-generator-react.adapter";
 
-export const PdfGenerator =
-  Env.type === bg.NodeEnvironmentEnum.local
-    ? new PdfGeneratorReact()
-    : new bg.PdfGeneratorNoop(LoggerWinstonLocalAdapter);
+export const PdfGenerator: bg.PdfGeneratorPort = {
+  [bg.NodeEnvironmentEnum.local]: new PdfGeneratorReact(),
+  [bg.NodeEnvironmentEnum.test]: new bg.PdfGeneratorNoop(LoggerWinstonLocalAdapter),
+  [bg.NodeEnvironmentEnum.staging]: new bg.PdfGeneratorNoop(LoggerWinstonLocalAdapter),
+  [bg.NodeEnvironmentEnum.production]: new PdfGeneratorReact(),
+}[Env.type];
