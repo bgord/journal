4296e99cf61232c78d6daed2462a6f32982beff8
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-16 12:11:31 +0200

    fix: rate limiters

--- infra/rate-limiters.ts
+++ infra/rate-limiters.ts
@@ -1,12 +1,14 @@
 import * as bg from "@bgord/bun";
 import * as tools from "@bgord/tools";
 
-export const HealthcheckStore = new bg.RateLimitStoreNodeCache(tools.Duration.Seconds(5));
+export const HealthcheckStore = new bg.RateLimitStoreNodeCacheAdapter(tools.Duration.Seconds(5));
 
-export const EntriesDataStore = new bg.RateLimitStoreNodeCache(tools.Duration.Minutes(1));
-export const EntriesEntriesStore = new bg.RateLimitStoreNodeCache(tools.Duration.Minutes(1));
+export const EntriesDataStore = new bg.RateLimitStoreNodeCacheAdapter(tools.Duration.Minutes(1));
+export const EntriesEntriesStore = new bg.RateLimitStoreNodeCacheAdapter(tools.Duration.Minutes(1));
 
-export const WeeklyReviewExportEmailStore = new bg.RateLimitStoreNodeCache(tools.Duration.Minutes(1));
-export const WeeklyReviewExportDownloadStore = new bg.RateLimitStoreNodeCache(tools.Duration.Minutes(1));
+export const WeeklyReviewExportEmailStore = new bg.RateLimitStoreNodeCacheAdapter(tools.Duration.Minutes(1));
+export const WeeklyReviewExportDownloadStore = new bg.RateLimitStoreNodeCacheAdapter(
+  tools.Duration.Minutes(1),
+);
 
-export const ShareableLinkCreateStore = new bg.RateLimitStoreNodeCache(tools.Duration.Minutes(1));
+export const ShareableLinkCreateStore = new bg.RateLimitStoreNodeCacheAdapter(tools.Duration.Minutes(1));
