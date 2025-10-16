217ca25bd3fb2e7286d6710efce4f47aef0b72e8
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-16 12:13:50 +0200

    fix: captcha adapter

--- infra/adapters/captcha.adapter.ts
+++ infra/adapters/captcha.adapter.ts
@@ -4,8 +4,8 @@ import { Env } from "+infra/env";
 const HCAPTCHA_SECRET_KEY = Env.HCAPTCHA_SECRET_KEY as bg.HCaptchaSecretKeyType;
 
 export const CaptchaShield = {
-  [bg.NodeEnvironmentEnum.local]: new bg.ShieldCaptchaHcaptchaLocal(HCAPTCHA_SECRET_KEY),
-  [bg.NodeEnvironmentEnum.test]: new bg.ShieldCaptchaNoop(),
-  [bg.NodeEnvironmentEnum.staging]: new bg.ShieldCaptchaHcaptcha(HCAPTCHA_SECRET_KEY),
-  [bg.NodeEnvironmentEnum.production]: new bg.ShieldCaptchaNoop(),
+  [bg.NodeEnvironmentEnum.local]: new bg.ShieldCaptchaHcaptchaLocalAdapter(HCAPTCHA_SECRET_KEY),
+  [bg.NodeEnvironmentEnum.test]: new bg.ShieldCaptchaNoopAdapter(),
+  [bg.NodeEnvironmentEnum.staging]: new bg.ShieldCaptchaHcaptchaAdapter(HCAPTCHA_SECRET_KEY),
+  [bg.NodeEnvironmentEnum.production]: new bg.ShieldCaptchaNoopAdapter(),
 }[Env.type];
