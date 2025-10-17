6f1d2b87af71ed191746b4b3b4e0b74ad6240edc
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-17 12:11:16 +0200

    step: add protected

---
+++ fullstack/protected.tsx
@@ -0,0 +1,22 @@
+import * as RR from "react-router";
+import { getSession } from "./auth.server";
+
+function redirectTarget(request: Request) {
+  const url = new URL(request.url);
+  // send them back to where they tried to go
+  return `/login?from=${encodeURIComponent(url.pathname + url.search)}`;
+}
+
+export async function loader({ request }: RR.LoaderFunctionArgs) {
+  const { json } = await getSession(request);
+
+  const user = json?.data?.user ?? null;
+
+  if (!user) throw RR.redirect(redirectTarget(request));
+
+  return { user };
+}
+
+export default function Protected() {
+  return <RR.Outlet />;
+}
