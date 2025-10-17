8b911e669f0caab1c2647a72a2b3bac0d8e90460
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-17 23:09:59 +0200

    step: add entry server

---
+++ fullstack/entry-server.tsx
@@ -0,0 +1,11 @@
+import { createRequestHandler, defaultRenderHandler } from "@tanstack/react-router/ssr/server";
+import { createRouter } from "./router";
+
+export async function handleSsr(request: Request): Promise<Response> {
+  const handler = createRequestHandler({
+    request,
+    createRouter: () => createRouter({}),
+  });
+
+  return handler(defaultRenderHandler);
+}
