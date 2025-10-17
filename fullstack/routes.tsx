644a04b9a5c7e00d35706de073abcfce63d09555
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-17 11:34:38 +0200

    step: define fullstack/routes

---
+++ fullstack/routes.tsx
@@ -0,0 +1,19 @@
+import { RouteObject, Link } from "react-router";
+
+import { Login } from "./login";
+import { Home } from "./home";
+
+function NotFound() {
+  return (
+    <main>
+      <h1>404</h1>
+      <Link to="/">Go home</Link>
+    </main>
+  );
+}
+
+export const routes: RouteObject[] = [
+  { path: "/", element: <Home /> },
+  { path: "/login", element: <Login /> },
+  { path: "*", element: <NotFound /> },
+];
