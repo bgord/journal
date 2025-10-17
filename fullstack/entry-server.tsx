316383b80ad2547cbfb4615c7bab46f15a8ad385
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-18 00:11:33 +0200

    step: pass user to handler

--- fullstack/entry-server.tsx
+++ fullstack/entry-server.tsx
@@ -1,11 +1,12 @@
 import { createRequestHandler, defaultRenderHandler } from "@tanstack/react-router/ssr/server";
+import { getSession } from "./auth.server";
 import { createRouter } from "./router";
 
 export async function handleSsr(request: Request): Promise<Response> {
-  const handler = createRequestHandler({
-    request,
-    createRouter: () => createRouter({}),
-  });
+  const { json } = await getSession(request);
+  const user = json?.user ?? null;
+
+  const handler = createRequestHandler({ request, createRouter: () => createRouter({ user }) });
 
   return handler(defaultRenderHandler);
 }
