e2b954e91edf2d87a1baa054ff9b9e6078b82ae0
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-21 23:21:29 +0200

    refactor: cleanup aliases

--- fullstack/app.tsx
+++ fullstack/app.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ErrorBoundary, LocationProvider, lazy, Route, Router } from "preact-iso";
 import { Navigation } from "./navigation";
 
@@ -6,15 +7,19 @@ const Weekly = lazy(() => import("./weekly").then((module) => module.Weekly));
 
 export type AppProps = { translations: Record<string, string>; language: string };
 
+const queryClient = new QueryClient();
+
 export function App(_props: AppProps) {
   return (
     <LocationProvider>
       <ErrorBoundary>
-        <Navigation />
-        <Router>
-          <Route path="/" component={Home} />
-          <Route path="/weekly" component={Weekly} />
-        </Router>
+        <QueryClientProvider client={queryClient}>
+          <Navigation />
+          <Router>
+            <Route path="/" component={Home} />
+            <Route path="/weekly" component={Weekly} />
+          </Router>
+        </QueryClientProvider>
       </ErrorBoundary>
     </LocationProvider>
   );
