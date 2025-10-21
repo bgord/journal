0f823084a92463a7bc60a029d4b99dd0798d4d6c
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-21 21:49:21 +0200

    step: add navigation

--- app.tsx
+++ app.tsx
@@ -7,6 +7,24 @@ export function AppShell() {
   return (
     <LocationProvider>
       <ErrorBoundary>
+        <nav data-stack="y">
+          <header data-stack="x" data-cross="center" data-gap="6" data-p="3">
+            <div className="logo" data-fs="4xl" data-fw="bold" data-ls="wider" data-color="brand-600">
+              <a href="/">Journal</a>
+            </div>
+
+            <a href="/dashboard" className="c-link" data-transform="uppercase" data-ml="auto">
+              Dashboard
+            </a>
+
+            <form action="/api/auth/sign-out" method="post">
+              <button class="c-link" type="submit">
+                Logout
+              </button>
+            </form>
+          </header>
+        </nav>
+
         <Router>
           <Route path="/" component={Home} />
           <Route path="/weekly" component={Weekly} />
