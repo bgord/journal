fb5d062c63eb60613bff3b25eda595398413d2c5
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-21 21:49:28 +0200

    step: add styles

--- ssr.tsx
+++ ssr.tsx
@@ -1,4 +1,5 @@
 import { prerender as ssr } from "preact-iso";
+// @ts-expect-error
 import { locationStub } from "preact-iso/prerender";
 import { AppShell } from "./app";
 
@@ -10,10 +11,25 @@ export async function renderHtml(path: string) {
     <html lang="en">
       <head>
         <meta charset="utf-8" />
-        <link rel="preload" as="style" href="/public/main.min.css" />
-        <link rel="preload" as="style" href="/public/custom.css" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+        <meta name="description" content="Journal" />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+          rel="stylesheet"
+        />
+
+        <link as="style" rel="stylesheet preload" href="/public/main.min.css" />
+        <link as="style" rel="stylesheet preload" href="/public/custom.css" />
+
         <title>Journal</title>
       </head>
+
       <body data-mx="auto" data-bg="neutral-950">
         <div id="root">${html}</div>
         <script type="module" src="/public/client-entry.js"></script>
