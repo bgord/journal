6b96fe921e5aea2135516b8d10dc0ee9b0316377
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-22 11:30:40 +0200

    step: add use state call

--- fullstack/home.tsx
+++ fullstack/home.tsx
@@ -1,8 +1,15 @@
+import { useState } from "preact/hooks";
+
 export function Home() {
+  const [result] = useState(123);
+
   return (
-    <div>
+    <div data-color="neutral-200">
       Home
       <a href="/weekly">weekly</a>
+      <div data-stack="y" data-mt="5">
+        Value: {result}
+      </div>
     </div>
   );
 }
