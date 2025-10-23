d8bd6f74d0216ca82d950f95bfda921bff3d099c
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-23 17:10:09 +0200

    refactor: implement language change

--- fullstack/components/language-selector.tsx
+++ fullstack/components/language-selector.tsx
@@ -1,13 +1,25 @@
-import { useSupportedLanguages, useTranslations } from "@bgord/ui";
+import { useLanguage, useSupportedLanguages, useTranslations } from "@bgord/ui";
+import { useRouter } from "@tanstack/react-router";
+import Cookie from "js-cookie";
+import { rootRoute } from "../router";
 import { Select } from "./select";
 
 export function LanguageSelector() {
+  const router = useRouter();
+  const language = useLanguage();
   const supportedLanguages = useSupportedLanguages();
   const t = useTranslations();
 
   return (
     <div data-stack="x" data-cross="center" data-gap="3">
-      <Select data-disp="flex">
+      <Select
+        data-disp="flex"
+        defaultValue={language}
+        onChange={async (event) => {
+          Cookie.set("language", event.target.value);
+          await router.invalidate({ filter: (r) => r.routeId === rootRoute.id, sync: true });
+        }}
+      >
         {Object.keys(supportedLanguages).map((language) => (
           <option key={language} value={language}>
             {t(`profile.change_language.${language}.value`)}
