d1b42084debd627a00a50f9022bb14634f3f3697
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-26 13:22:40 +0100

    step: wire up home entry export section

--- fullstack/pages/home.tsx
+++ fullstack/pages/home.tsx
@@ -1,11 +1,27 @@
+import * as UI from "@bgord/ui";
+import { MoreHoriz } from "iconoir-react";
 import { HomeEntryAdd } from "../sections/home-entry-add";
+import { HomeEntryExport } from "../sections/home-entry-export";
 
 export function Home() {
+  const exportEntries = UI.useToggle({ name: "entry-export" });
+
   return (
     <main data-p="6" data-stack="y" data-maxw="md" data-mx="auto" data-gap="3">
-      <div data-stack="x" data-main="end">
+      <div data-stack="x" data-main="end" data-gap="3">
+        <button
+          type="button"
+          className="c-button"
+          data-variant="with-icon"
+          data-ml="auto"
+          onClick={exportEntries.toggle}
+        >
+          <MoreHoriz data-size="md" />
+        </button>
         <HomeEntryAdd />
       </div>
+
+      {exportEntries.on && <HomeEntryExport />}
     </main>
   );
 }
