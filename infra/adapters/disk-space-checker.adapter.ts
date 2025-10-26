d081aaa819d599f8c2f57a1363a69f228b3a96b7
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-26 13:13:00 +0100

    refactor: use bun disk space checker adapter

--- infra/adapters/disk-space-checker.adapter.ts
+++ infra/adapters/disk-space-checker.adapter.ts
@@ -2,12 +2,12 @@ import * as bg from "@bgord/bun";
 import * as tools from "@bgord/tools";
 import { Env } from "+infra/env";
 
-const DiskSpaceCheckerNoopAdapter = new bg.DiskSpaceCheckerNoopAdapter(tools.Size.fromMB(10));
-const DiskSpaceCheckerPackageAdapter = new bg.DiskSpaceCheckerPackageAdapter();
+const DiskSpaceCheckerNoopAdapter = new bg.DiskSpaceCheckerNoopAdapter(tools.Size.fromGB(10));
+const DiskSpaceCheckerBunAdapter = new bg.DiskSpaceCheckerBunAdapter();
 
 export const DiskSpaceChecker: bg.DiskSpaceCheckerPort = {
-  [bg.NodeEnvironmentEnum.local]: DiskSpaceCheckerPackageAdapter,
+  [bg.NodeEnvironmentEnum.local]: DiskSpaceCheckerBunAdapter,
   [bg.NodeEnvironmentEnum.test]: DiskSpaceCheckerNoopAdapter,
   [bg.NodeEnvironmentEnum.staging]: DiskSpaceCheckerNoopAdapter,
-  [bg.NodeEnvironmentEnum.production]: DiskSpaceCheckerPackageAdapter,
+  [bg.NodeEnvironmentEnum.production]: DiskSpaceCheckerBunAdapter,
 }[Env.type];
