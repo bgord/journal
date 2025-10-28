119b8ee216d37b5afce49e087edb6e05e236efe6
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-28 20:43:40 +0100

    step: return entry alarms

--- app/http/get-dashboard.ts
+++ app/http/get-dashboard.ts
@@ -11,9 +11,13 @@ type DashboardAlarmInactivityType = Pick<Emotions.VO.AlarmSnapshot, "id" | "advi
   generatedAt: string;
 };
 
+type DashboardAlarmEntryType = Pick<Emotions.VO.AlarmSnapshot, "id" | "advice" | "name" | "emotionLabel"> & {
+  generatedAt: string;
+};
+
 export type DashboardDataType = {
   heatmap: { t: 0 | 1; c: "200" | "400" | "600" }[];
-  alarms: { inactivity: DashboardAlarmInactivityType[]; entries: [] };
+  alarms: { inactivity: DashboardAlarmInactivityType[]; entry: DashboardAlarmEntryType[] };
 };
 
 export async function GetDashboard(c: hono.Context<infra.HonoConfig>) {
@@ -53,9 +57,30 @@ export async function GetDashboard(c: hono.Context<infra.HonoConfig>) {
     generatedAt: tools.DateFormatters.datetime(alarm.generatedAt),
   }));
 
+  const entryAlarmsResponse = await db.query.alarms.findMany({
+    where: and(
+      eq(Schema.alarms.userId, userId),
+      eq(Schema.alarms.name, Emotions.VO.AlarmNameOption.INACTIVITY_ALARM),
+      not(eq(Schema.alarms.status, "cancelled")),
+      not(isNull(Schema.alarms.advice)),
+      not(isNull(Schema.alarms.emotionLabel)),
+    ),
+    orderBy: desc(Schema.alarms.generatedAt),
+    limit: 5,
+    columns: { id: true, generatedAt: true, advice: true, emotionLabel: true, name: true },
+  });
+
+  const entry = entryAlarmsResponse.map((alarm) => ({
+    ...alarm,
+    advice: alarm.advice as AI.AdviceType,
+    name: Emotions.VO.AlarmName.parse(alarm.name),
+    emotionLabel: Emotions.VO.EmotionLabelSchema.parse(alarm.emotionLabel),
+    generatedAt: tools.DateFormatters.datetime(alarm.generatedAt),
+  }));
+
   const result: DashboardDataType = {
     heatmap,
-    alarms: { inactivity, entries: [] },
+    alarms: { inactivity, entry },
   };
 
   return c.json(result);
