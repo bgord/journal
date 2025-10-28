4c2660053fb8391f8b9d85042be270cd6b71a4cc
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-28 20:16:52 +0100

    step: migrate dashboard heatmap section

--- fullstack/sections/dashboard-heatmap.tsx
+++ fullstack/sections/dashboard-heatmap.tsx
@@ -2,11 +2,11 @@ import { Rhythm } from "@bgord/ui";
 import { dashboardRoute } from "../router";
 
 export function DashboardHeatmap() {
-  const { heatmap } = dashboardRoute.useLoaderData();
+  const dashboard = dashboardRoute.useLoaderData();
 
   return (
     <ul data-stack="x" data-p="5" data-gap="1">
-      {heatmap.map((point, index) => (
+      {dashboard?.heatmap.map((point, index) => (
         <li
           key={`heatmap-${point}-${index}`}
           data-bg={point.t ? `positive-${point.c}` : `danger-${point.c}`}
