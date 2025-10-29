d16a4f886637cdd8c2c83548818de6c4b444f50c
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-29 16:16:19 +0100

    step: render entry filter list options

--- app/services/home-entry-list-form.ts
+++ app/services/home-entry-list-form.ts
@@ -1,7 +1,10 @@
 import { EntryListFilterOptions } from "../../modules/emotions/value-objects/entry-list-filter-options";
 
-export const HomeEntryListForm = {
-  filter: { options: Object.keys(EntryListFilterOptions), defaultValue: EntryListFilterOptions.today },
+export const Form = {
+  filter: {
+    options: Object.keys(EntryListFilterOptions),
+    field: { name: "filter", defaultValue: EntryListFilterOptions.today },
+  },
 };
 
 export type * as types from "../../modules/emotions/value-objects";
--- fullstack/sections/home-entry-list.tsx
+++ fullstack/sections/home-entry-list.tsx
@@ -1,12 +1,24 @@
-import * as UI from "../components";
+import { useTextField, useTranslations } from "@bgord/ui";
+import { Form } from "../../app/services/home-entry-list-form";
+import * as Components from "../components";
 import { homeRoute } from "../router";
 import { HomeEntry } from "./home-entry";
 
 export function HomeEntryList() {
+  const t = useTranslations();
   const { entries } = homeRoute.useLoaderData();
+  const filter = useTextField({ name: Form.filter.field.name, defaultValue: Form.filter.field.defaultValue });
 
   return (
     <div data-stacky="y">
+      <Components.Select {...filter.input.props}>
+        {Form.filter.options.map((option) => (
+          <option key={option} value={option}>
+            {t(`entry.list.filter.${option}`)}
+          </option>
+        ))}
+      </Components.Select>
+
       {entries[0] && (
         <ul data-stack="y" data-gap="5" data-mt="6">
           {entries.map((entry) => (
@@ -15,7 +27,7 @@ export function HomeEntryList() {
         </ul>
       )}
 
-      {!entries[0] && <UI.EntryListEmpty />}
+      {!entries[0] && <Components.EntryListEmpty />}
     </div>
   );
 }
