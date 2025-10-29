0242516f5bfc4e53c33ef46bafff641456d71caf
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-29 12:11:26 +0100

    step: migrate weekly review export

--- fullstack/sections/dashboard-weekly-review-email-send.tsx
+++ fullstack/sections/dashboard-weekly-review-email-send.tsx
@@ -1,13 +1,47 @@
-import { SendMail } from "iconoir-react";
+import { Check, SendMail } from "iconoir-react";
+import React from "react";
 import type { DashboardDataType } from "../api";
+import { RequestState } from "../ui";
+
+const DELAY_MS = 1500;
 
 export function DashboardWeeklyReviewEmailSend(props: DashboardDataType["weeklyReviews"][number]) {
+  const [state, setState] = React.useState<RequestState>(RequestState.idle);
+
+  async function sendWeeklyReviewEmail(event: React.FormEvent) {
+    event.preventDefault();
+
+    if (state === RequestState.loading) return;
+
+    setState(RequestState.loading);
+
+    const response = await fetch(`/api/weekly-review/${props.id}/export/email`, {
+      method: "POST",
+      credentials: "include",
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (!response.ok) return setState(RequestState.error);
+
+    setState(RequestState.done);
+    setTimeout(() => setState(RequestState.idle), DELAY_MS);
+  }
+
   return (
-    <form>
-      <input type="hidden" name="intent" value="export_weekly_review_by_email" />
-      <input type="hidden" name="weeklyReviewId" value={props.id} />
-      <button type="submit" className="c-button" data-variant="bare" data-pt="2" data-color="brand-500">
-        <SendMail data-size="lg" />
+    <form onSubmit={sendWeeklyReviewEmail}>
+      <button
+        type="submit"
+        className="c-button"
+        data-variant="bare"
+        data-pt="2"
+        disabled={[RequestState.loading, RequestState.done].includes(state)}
+      >
+        {state !== RequestState.done && (
+          <SendMail data-color="brand-500" data-size="lg" data-animation="grow-fade-in" />
+        )}
+        {state === RequestState.done && (
+          <Check data-color="positive-400" data-size="lg" data-animation="grow-fade-in" />
+        )}
       </button>
     </form>
   );
