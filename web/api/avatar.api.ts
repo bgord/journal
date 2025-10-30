37026732319d30642671db97ef16956d7dc90c53
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-30 21:27:29 +0100

    step: add console logs

--- web/api/avatar.api.ts
+++ web/api/avatar.api.ts
@@ -7,10 +7,14 @@ export class Avatar {
   private static readonly BASE = "/api/profile-avatar/get";
 
   static async getEtag(request: Request | null): Promise<AvatarEtagType | null> {
+    console.log(request);
     const url = request ? new URL(Avatar.BASE, request.url) : Avatar.BASE;
+    console.log(url);
     const headers = request ? { cookie: Cookies.extractFrom(request) } : undefined;
+    console.log(headers);
 
     const response = await fetch(url, { headers, credentials: "include" });
+    console.log(response);
 
     if (!response?.ok) return null;
     return response.headers.get("etag");
