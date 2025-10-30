32d2dd0227a8ae5b1bbe666e726ec6b19b08abf7
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-30 14:04:36 +0100

    step: bump bun prereq version

--- infra/prerequisites.ts
+++ infra/prerequisites.ts
@@ -30,7 +30,7 @@ export const prerequisites = [
   }),
   new bg.PrerequisiteBun({
     label: "bun",
-    version: tools.PackageVersion.fromString("1.2.22"),
+    version: tools.PackageVersion.fromString("1.3.1"),
     current: Bun.version,
   }),
   new bg.PrerequisiteMemory({ label: "memory-consumption", maximum: tools.Size.fromMB(300) }),
