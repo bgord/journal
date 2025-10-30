9af5e08e20c068e1a601d38bff9629a6675af437
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-30 08:39:32 +0100

    step: remove rr fe

--- frontend/components/select.tsx
+++
@@ -1,7 +0,0 @@
-export function Select(props: React.JSX.IntrinsicElements["select"]) {
-  return (
-    <div className="c-select-wrapper">
-      <select className="c-select" {...props} />
-    </div>
-  );
-}
--- frontend/components/separator.tsx
+++
@@ -1,3 +0,0 @@
-export function Separator() {
-  return <div data-width="100%" data-bg="neutral-800" style={{ height: "2px" }} />;
-}
