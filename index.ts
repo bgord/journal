8daf40c8ef68843d10911416cfb6adcdbcfdf845
Bartosz Gordon <gordonbartosz@gmail.com>
2025-11-01 19:33:14 +0100

    step: add static files from bgord bun

--- index.ts
+++ index.ts
@@ -1,9 +1,6 @@
 import * as bg from "@bgord/bun";
 import * as tools from "@bgord/tools";
 import { migrate } from "drizzle-orm/bun-sqlite/migrator";
-import { type Context, Hono } from "hono";
-import { serveStatic } from "hono/bun";
-import { etag } from "hono/etag";
 import * as infra from "+infra";
 import { Logger } from "+infra/adapters/logger.adapter";
 import { db } from "+infra/db";
@@ -11,30 +8,6 @@ import { Env } from "+infra/env";
 import { prerequisites } from "+infra/prerequisites";
 import { server, startup } from "./server";
 import { handler } from "./web/entry-server";
-
-type StaticFilesStrategy = (path: string, context: Context) => Promise<void> | void;
-
-const StaticFileStrategyNoop: StaticFilesStrategy = () => {};
-
-const StaticFileStrategyMustRevalidate: StaticFilesStrategy = (_path, c) => {
-  c.header("Cache-Control", `public, max-age=${tools.Duration.Minutes(5).ms}, must-revalidate`);
-};
-
-export class StaticFiles {
-  static handle(path: string, strategy: StaticFilesStrategy) {
-    return {
-      [path]: new Hono().use(
-        path,
-        etag(),
-        serveStatic({
-          root: "./",
-          precompressed: true,
-          onFound: strategy,
-        }),
-      ).fetch,
-    };
-  }
-}
 
 (async function main() {
   await new bg.Prerequisites(Logger).check(prerequisites);
@@ -45,11 +18,11 @@ export class StaticFiles {
     idleTimeout: infra.IDLE_TIMEOUT,
     routes: {
       "/favicon.ico": Bun.file("public/favicon.ico"),
-      ...StaticFiles.handle(
+      ...bg.StaticFiles.handle(
         "/public/*",
         Env.type === bg.NodeEnvironmentEnum.production
-          ? StaticFileStrategyMustRevalidate
-          : StaticFileStrategyNoop,
+          ? bg.StaticFileStrategyMustRevalidate(tools.Duration.Minutes(5))
+          : bg.StaticFileStrategyNoop,
       ),
       "/api/*": server.fetch,
       "/*": handler,
