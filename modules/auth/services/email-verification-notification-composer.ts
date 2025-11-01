392d99cab53f014b4794edaa6adfb1311c32ebe3
Bartosz Gordon <gordonbartosz@gmail.com>
2025-11-01 23:17:46 +0100

    refactor: remove logs

--- modules/auth/services/email-verification-notification-composer.ts
+++ modules/auth/services/email-verification-notification-composer.ts
@@ -3,14 +3,8 @@ import { Env } from "+infra/env";
 
 export class EmailVerificationNotificationComposer {
   compose(url: string): tools.NotificationTemplate {
-    console.log(url);
-    console.log({ callback: `${Env.BETTER_AUTH_URL}/auth/login` });
-    console.log({ BETTER_AUTH_URL: Env.BETTER_AUTH_URL });
-
     const callbackUrl = new URL(url);
     callbackUrl.searchParams.set("callbackURL", `${Env.BETTER_AUTH_URL}/auth/login`);
-
-    console.log("link", callbackUrl.toString());
 
     return new tools.NotificationTemplate(
       "Verify your Journal account",
