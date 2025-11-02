249cac76a5c59c5762e2463199db83d255e6b7bf
Bartosz Gordon <gordonbartosz@gmail.com>
2025-11-02 21:15:02 +0100

    step: adjust access context

--- modules/publishing/value-objects/access-context.ts
+++ modules/publishing/value-objects/access-context.ts
@@ -1,4 +1,4 @@
 import type * as bg from "@bgord/bun";
 import type * as tools from "@bgord/tools";
 
-export type AccessContext = { timestamp: tools.TimestampType; visitorId: bg.VisitorIdPort };
+export type AccessContext = { timestamp: tools.TimestampVO; visitorId: bg.VisitorIdPort };
