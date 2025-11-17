44833fb57f4feaf1a0c5c95c64fdf1e1dc546fa5
Bartosz Gordon <gordonbartosz@gmail.com>
2025-11-17 12:54:26 +0100

    step: add entry count

--- web/pages/home.tsx
+++ web/pages/home.tsx
@@ -1,15 +1,33 @@
-import { useToggle } from "@bgord/ui";
+import { usePluralize, useToggle, useTranslations } from "@bgord/ui";
 import { Outlet } from "@tanstack/react-router";
 import { MoreHoriz } from "iconoir-react";
+import { homeRoute } from "../router";
 import { HomeEntryAdd, HomeEntryExport, HomeEntryList } from "../sections";
 
 /** @public */
 export function Home() {
+  const { entries } = homeRoute.useLoaderData();
+  const t = useTranslations();
+  const pluralize = usePluralize();
   const exportEntries = useToggle({ name: "entry-export" });
 
   return (
     <main data-stack="y" data-gap="3" data-maxw="md" data-mx="auto" data-mb="8">
       <div data-stack="x" data-gap="3">
+        <div data-stack="x" data-gap="2" data-cross="center" data-color="neutral-400">
+          <div className="c-badge" data-variant="primary" data-p="1">
+            {entries.length}
+          </div>
+          <div data-fs="sm">
+            {pluralize({
+              value: entries.length,
+              singular: t("app.entry.singular"),
+              plural: t("app.entry.plural"),
+              genitive: t("app.entry.genitive"),
+            })}
+          </div>
+        </div>
+
         <button
           type="button"
           className="c-button"
