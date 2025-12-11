b665cb658b2197f7d3bd9723e5a51f11d8f8132a
Bartosz Gordon <gordonbartosz@gmail.com>
2025-12-11 22:33:26 +0100

    refactor: cleanup prereq

--- infra/healthcheck.ts
+++ infra/healthcheck.ts
@@ -3,8 +3,5 @@ import { prerequisites } from "+infra/prerequisites";
 
 export const healthcheck = [
   new bg.PrerequisiteSelf({ label: "self" }),
-  new bg.PrerequisiteBinary({ label: "httpie", binary: bg.Binary.parse("http") }),
-  new bg.PrerequisiteBinary({ label: "sqlite3", binary: bg.Binary.parse("sqlite3") }),
-  new bg.PrerequisiteBinary({ label: "tar", binary: bg.Binary.parse("tar") }),
   ...prerequisites.filter((prerequisite) => prerequisite.label !== "port"),
 ];
--- infra/prerequisites.ts
+++ infra/prerequisites.ts
@@ -60,4 +60,7 @@ export const prerequisites = [
     Timekeeper: Adapters.Timekeeper,
   }),
   new bg.PrerequisiteOs({ label: "os", accepted: ["Darwin", "Linux"] }),
+  new bg.PrerequisiteBinary({ label: "httpie", binary: bg.Binary.parse("http") }),
+  new bg.PrerequisiteBinary({ label: "sqlite3", binary: bg.Binary.parse("sqlite3") }),
+  new bg.PrerequisiteBinary({ label: "tar", binary: bg.Binary.parse("tar") }),
 ];
