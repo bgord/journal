ffc8fc619213f1399fc78f993b5f9c3bb8bc685e
Bartosz Gordon <gordonbartosz@gmail.com>
2025-12-13 10:36:55 +0100

    refactor: remove mailer adapter env

--- infra/env.ts
+++ infra/env.ts
@@ -8,11 +8,6 @@ export enum AiClientAdapter {
   noop = "noop",
 }
 
-export enum MailerAdapter {
-  smtp = "smtp",
-  noop = "noop",
-}
-
 const EnvironmentSchema = z
   .object({
     PORT: bg.Port,
@@ -29,7 +24,6 @@ const EnvironmentSchema = z
     ANTHROPIC_AI_API_KEY: z.string().min(1).max(256).trim(),
     AXIOM_API_TOKEN: z.string().length(41),
     AI_CLIENT_ADAPTER: z.enum(AiClientAdapter),
-    MAILER_ADAPTER: z.enum(MailerAdapter),
     BETTER_AUTH_SECRET: z.string().length(32).trim(),
     BETTER_AUTH_URL: z.url().trim(),
     HCAPTCHA_SECRET_KEY: z.string().trim(),
