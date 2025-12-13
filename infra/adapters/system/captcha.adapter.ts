81eb1db1029969fb4939d9c65e1cb19242c7fc1d
Bartosz Gordon <gordonbartosz@gmail.com>
2025-12-13 20:34:48 +0100

    refactor: remove captcha type casting

--- infra/adapters/system/captcha.adapter.ts
+++ infra/adapters/system/captcha.adapter.ts
@@ -2,13 +2,10 @@ import * as bg from "@bgord/bun";
 import type { EnvironmentType } from "+infra/env";
 
 export function createShieldCaptcha(Env: EnvironmentType): bg.ShieldPort {
-  // TODO: Fix type casting
-  const HCAPTCHA_SECRET_KEY = Env.HCAPTCHA_SECRET_KEY as bg.HCaptchaSecretKeyType;
-
   return {
-    [bg.NodeEnvironmentEnum.local]: new bg.ShieldCaptchaHcaptchaLocalAdapter(HCAPTCHA_SECRET_KEY),
+    [bg.NodeEnvironmentEnum.local]: new bg.ShieldCaptchaHcaptchaLocalAdapter(Env.HCAPTCHA_SECRET_KEY),
     [bg.NodeEnvironmentEnum.test]: new bg.ShieldNoopAdapter(),
-    [bg.NodeEnvironmentEnum.staging]: new bg.ShieldCaptchaHcaptchaAdapter(HCAPTCHA_SECRET_KEY),
+    [bg.NodeEnvironmentEnum.staging]: new bg.ShieldCaptchaHcaptchaAdapter(Env.HCAPTCHA_SECRET_KEY),
     [bg.NodeEnvironmentEnum.production]: new bg.ShieldNoopAdapter(),
   }[Env.type];
 }
