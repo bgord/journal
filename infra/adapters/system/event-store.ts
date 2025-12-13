8a88029e51baf539ea68145ffd55dc3efd3010c8
Bartosz Gordon <gordonbartosz@gmail.com>
2025-12-13 12:01:52 +0100

    refactor: update event store

--- infra/adapters/system/event-store.ts
+++ infra/adapters/system/event-store.ts
@@ -11,13 +11,15 @@ import type {
   WeeklyReviewExportByEmailRequestedEvent,
 } from "+emotions/events";
 import type { PatternDetectionEvent } from "+emotions/services/patterns";
+import type { createEventBus } from "+infra/adapters/system/event-bus";
 import { db } from "+infra/db";
-import { EventBus } from "+infra/event-bus";
 import * as schema from "+infra/schema";
 import type { ProfileAvatarRemovedEvent, ProfileAvatarUpdatedEvent } from "+preferences/events";
 import type { ShareableLinkEvent } from "+publishing/aggregates";
 import type { ShareableLinkAccessedEvent } from "+publishing/events";
 import type { HourHasPassedEvent } from "+system/events";
+
+type Dependencies = { EventBus: ReturnType<typeof createEventBus> };
 
 export type AcceptedEvent =
   | EntryEvent
@@ -40,40 +42,42 @@ export type AcceptedEvent =
   | typeof ProfileAvatarUpdatedEvent
   | typeof ProfileAvatarRemovedEvent;
 
-export const EventStore = new bg.DispatchingEventStore<AcceptedEvent>(
-  {
-    finder: (stream: bg.EventStreamType, acceptedEventsNames: bg.EventNameType[]) =>
-      db
-        .select()
-        .from(schema.events)
-        .orderBy(asc(schema.events.revision))
-        .where(and(eq(schema.events.stream, stream), inArray(schema.events.name, acceptedEventsNames))),
-
-    inserter: async (incoming: z.infer<bg.GenericParsedEventSchema>[]) => {
-      const { stream } = incoming[0] as { stream: bg.EventStreamType };
-
-      return db.transaction(async (tx) => {
-        const current = await tx
-          .select({ max: sql<number>`max(${schema.events.revision})` })
+export function createEventStore(deps: Dependencies) {
+  return new bg.DispatchingEventStore<AcceptedEvent>(
+    {
+      finder: (stream: bg.EventStreamType, acceptedEventsNames: bg.EventNameType[]) =>
+        db
+          .select()
           .from(schema.events)
-          .where(eq(schema.events.stream, stream));
+          .orderBy(asc(schema.events.revision))
+          .where(and(eq(schema.events.stream, stream), inArray(schema.events.name, acceptedEventsNames))),
 
-        const max = current[0]?.max ?? bg.DispatchingEventStore.EMPTY_STREAM_REVISION;
+      inserter: async (incoming: z.infer<bg.GenericParsedEventSchema>[]) => {
+        const { stream } = incoming[0] as { stream: bg.EventStreamType };
 
-        const rows: z.infer<bg.GenericParsedEventSchema>[] = incoming.map((event, order) => ({
-          ...event,
-          revision: max + order + 1,
-        }));
+        return db.transaction(async (tx) => {
+          const current = await tx
+            .select({ max: sql<number>`max(${schema.events.revision})` })
+            .from(schema.events)
+            .where(eq(schema.events.stream, stream));
 
-        try {
-          await tx.insert(schema.events).values(rows);
-          return rows;
-        } catch (e: any) {
-          if (e.code === "SQLITE_CONSTRAINT") throw new Error(tools.RevisionError.Mismatch);
-          throw e;
-        }
-      });
+          const max = current[0]?.max ?? bg.DispatchingEventStore.EMPTY_STREAM_REVISION;
+
+          const rows: z.infer<bg.GenericParsedEventSchema>[] = incoming.map((event, order) => ({
+            ...event,
+            revision: max + order + 1,
+          }));
+
+          try {
+            await tx.insert(schema.events).values(rows);
+            return rows;
+          } catch (e: any) {
+            if (e.code === "SQLITE_CONSTRAINT") throw new Error(tools.RevisionError.Mismatch);
+            throw e;
+          }
+        });
+      },
     },
-  },
-  EventBus,
-);
+    deps.EventBus,
+  );
+}
