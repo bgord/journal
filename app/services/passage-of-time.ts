cba9374f39393f6f768d3e85de3949a2a0074044
Bartosz Gordon <gordonbartosz@gmail.com>
2025-12-13 12:18:49 +0100

    refactor: update jobs

--- app/services/passage-of-time.ts
+++ app/services/passage-of-time.ts
@@ -1,22 +1,28 @@
 import * as bg from "@bgord/bun";
 import * as System from "+system";
-import { Clock, IdProvider } from "+infra/adapters";
-import { EventStore } from "+infra/event-store";
 
-export class PassageOfTime {
+type Dependencies = {
+  EventStore: bg.EventStoreLike<System.Events.HourHasPassedEventType>;
+  Clock: bg.ClockPort;
+  IdProvider: bg.IdProviderPort;
+};
+
+export class PassageOfTime implements bg.UnitOfWork {
+  constructor(private readonly deps: Dependencies) {}
+
   static cron = bg.Jobs.SCHEDULES.EVERY_HOUR;
 
-  static label = "PassageOfTime";
+  label = "PassageOfTime";
 
-  static async process() {
-    const timestamp = Clock.nowMs();
+  async process() {
+    const timestamp = this.deps.Clock.nowMs();
 
     const event = System.Events.HourHasPassedEvent.parse({
-      ...bg.createEventEnvelope("passage_of_time", { IdProvider, Clock }),
+      ...bg.createEventEnvelope("passage_of_time", this.deps),
       name: System.Events.HOUR_HAS_PASSED_EVENT,
       payload: { timestamp },
     } satisfies System.Events.HourHasPassedEventType);
 
-    await EventStore.save([event]);
+    await this.deps.EventStore.save([event]);
   }
 }
--- infra/bootstrap.ts
+++ infra/bootstrap.ts
@@ -2,12 +2,15 @@ import { createSystemAdapters } from "+infra/adapters/system";
 import type { EnvironmentType } from "+infra/env";
 import { I18nConfig } from "+infra/i18n";
 import { createPrerequisites } from "+infra/prerequisites";
+import { createJobs } from "./jobs";
 import { ResponseCache } from "./response-cache";
 
 export async function bootstrap(Env: EnvironmentType) {
   const System = createSystemAdapters(Env);
 
+  const Jobs = createJobs(System);
+
   const prerequisites = createPrerequisites(Env, { ...System, Jobs });
 
-  return { Env, Adapters: { System }, Tools: { Prerequisites, I18nConfig, ResponseCache } };
+  return { Env, Adapters: { System }, Tools: { prerequisites, I18nConfig, ResponseCache } };
 }
