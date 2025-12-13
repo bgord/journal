d89ce7d3ecc16249bacae80e9c67289f028945d2
Bartosz Gordon <gordonbartosz@gmail.com>
2025-12-13 18:05:27 +0100

    fix: auth variables

--- infra/index.ts
+++ infra/index.ts
@@ -1,6 +1,6 @@
 import type * as bg from "@bgord/bun";
 import type { TimingVariables } from "hono/timing";
-import type { AuthVariables } from "./auth";
+import type { AuthVariables } from "+infra/adapters/system/shield-auth.adapter";
 import type { I18nVariables } from "./i18n";
 
 export type Config = {
