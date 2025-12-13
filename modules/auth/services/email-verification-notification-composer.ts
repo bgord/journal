8bbd7fa15a75701c2ad11054956ae77f37287ae4
Bartosz Gordon <gordonbartosz@gmail.com>
2025-12-13 18:06:29 +0100

    fix: email verification notification composer

--- modules/auth/services/email-verification-notification-composer.ts
+++ modules/auth/services/email-verification-notification-composer.ts
@@ -1,10 +1,11 @@
 import * as tools from "@bgord/tools";
-import { Env } from "+infra/env";
 
 export class EmailVerificationNotificationComposer {
+  constructor(private readonly BETTER_AUTH_URL: string) {}
+
   compose(url: string): tools.NotificationTemplate {
     const callbackUrl = new URL(url);
-    callbackUrl.searchParams.set("callbackURL", `${Env.BETTER_AUTH_URL}/auth/login`);
+    callbackUrl.searchParams.set("callbackURL", `${this.BETTER_AUTH_URL}/auth/login`);
 
     return new tools.NotificationTemplate(
       "Verify your Journal account",
