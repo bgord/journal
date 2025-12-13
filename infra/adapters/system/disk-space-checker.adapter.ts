f68ec02d675a4a363ae98ec3fcd6b672832e339d
Bartosz Gordon <gordonbartosz@gmail.com>
2025-12-13 11:12:50 +0100

    refactor: update disk space checker

---
+++ infra/adapters/system/disk-space-checker.adapter.ts
@@ -0,0 +1,15 @@
+import * as bg from "@bgord/bun";
+import * as tools from "@bgord/tools";
+import type { EnvironmentType } from "+infra/env";
+
+export function createDiskSpaceChecker(Env: EnvironmentType): bg.DiskSpaceCheckerPort {
+  const DiskSpaceCheckerNoopAdapter = new bg.DiskSpaceCheckerNoopAdapter(tools.Size.fromGB(10));
+  const DiskSpaceCheckerBunAdapter = new bg.DiskSpaceCheckerBunAdapter();
+
+  return {
+    [bg.NodeEnvironmentEnum.local]: DiskSpaceCheckerBunAdapter,
+    [bg.NodeEnvironmentEnum.test]: DiskSpaceCheckerNoopAdapter,
+    [bg.NodeEnvironmentEnum.staging]: DiskSpaceCheckerNoopAdapter,
+    [bg.NodeEnvironmentEnum.production]: DiskSpaceCheckerBunAdapter,
+  }[Env.type];
+}
