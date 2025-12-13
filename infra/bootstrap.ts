8e4b3a2ae7a5a89d81691dc2605107fe17012020
Bartosz Gordon <gordonbartosz@gmail.com>
2025-12-13 12:17:36 +0100

    refactor: update prereqs

--- infra/bootstrap.ts
+++ infra/bootstrap.ts
@@ -1,10 +1,13 @@
 import { createSystemAdapters } from "+infra/adapters/system";
 import type { EnvironmentType } from "+infra/env";
 import { I18nConfig } from "+infra/i18n";
+import { createPrerequisites } from "+infra/prerequisites";
 import { ResponseCache } from "./response-cache";
 
 export async function bootstrap(Env: EnvironmentType) {
   const System = createSystemAdapters(Env);
 
-  return { Env, Adapters: { System }, Tools: { I18nConfig, ResponseCache } };
+  const prerequisites = createPrerequisites(Env, { ...System, Jobs });
+
+  return { Env, Adapters: { System }, Tools: { Prerequisites, I18nConfig, ResponseCache } };
 }
