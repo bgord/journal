2ed6ab2e1762a5527aca1998d3e63b82327ca1ba
Bartosz Gordon <gordonbartosz@gmail.com>
2025-12-15 19:30:28 +0100

    step: add gitleaks local prereq

--- infra/prerequisites.ts
+++ infra/prerequisites.ts
@@ -16,12 +16,13 @@ type Dependencies = {
 };
 
 export function createPrerequisites(Env: EnvironmentType, deps: Dependencies) {
-  const enabled = Env.type === bg.NodeEnvironmentEnum.production;
+  const production = Env.type === bg.NodeEnvironmentEnum.production;
+  const local = Env.type === bg.NodeEnvironmentEnum.local;
 
   return [
     new bg.PrerequisitePort({ label: "port", port: Env.PORT }),
     new bg.PrerequisiteTimezoneUTC({ label: "timezone", timezone: tools.Timezone.parse(Env.TZ) }),
-    new bg.PrerequisiteRAM({ label: "RAM", minimum: tools.Size.fromMB(128), enabled }),
+    new bg.PrerequisiteRAM({ label: "RAM", minimum: tools.Size.fromMB(128), enabled: production }),
     new bg.PrerequisiteSpace({ label: "disk-space", minimum: tools.Size.fromMB(512) }, deps),
     new bg.PrerequisiteNode({
       label: "node",
@@ -34,31 +35,35 @@ export function createPrerequisites(Env: EnvironmentType, deps: Dependencies) {
       current: Bun.version,
     }),
     new bg.PrerequisiteMemory({ label: "memory-consumption", maximum: tools.Size.fromMB(300) }),
-    new bg.PrerequisiteLogFile({ label: "log-file", enabled }, deps),
+    new bg.PrerequisiteLogFile({ label: "log-file", enabled: production }, deps),
     new bg.PrerequisiteDirectory({
       label: "temporary-files dir",
       directory: deps.TemporaryFile.root,
-      enabled,
+      enabled: production,
     }),
     new bg.PrerequisiteDirectory({
       label: "remote-file-storage dir",
       directory: deps.RemoteFileStorage.root,
-      enabled,
+      enabled: production,
     }),
     new bg.PrerequisiteJobs({ label: "jobs", Jobs: deps.Jobs }),
     new bg.PrerequisiteTranslations({ label: "translations", supportedLanguages: SupportedLanguages }, deps),
-    new bg.PrerequisiteMailer({ label: "mailer", enabled }, deps),
-    new bg.PrerequisiteOutsideConnectivity({ label: "outside-connectivity", enabled }),
-    new bg.PrerequisiteRunningUser({ label: "user", username: "bgord", enabled }),
-    new bg.PrerequisiteSQLite({ label: "sqlite", sqlite, enabled }),
+    new bg.PrerequisiteMailer({ label: "mailer", enabled: production }, deps),
+    new bg.PrerequisiteOutsideConnectivity({ label: "outside-connectivity", enabled: production }),
+    new bg.PrerequisiteRunningUser({ label: "user", username: "bgord", enabled: production }),
+    new bg.PrerequisiteSQLite({ label: "sqlite", sqlite, enabled: production }),
     new bg.PrerequisiteSSLCertificateExpiry(
-      { label: "ssl", hostname: "journal.bgord.dev", days: 7, enabled },
+      { label: "ssl", hostname: "journal.bgord.dev", days: 7, enabled: production },
       deps,
     ),
-    new bg.PrerequisiteClockDrift({ label: "clock-drift", enabled, skew: tools.Duration.Minutes(1) }, deps),
+    new bg.PrerequisiteClockDrift(
+      { label: "clock-drift", enabled: production, skew: tools.Duration.Minutes(1) },
+      deps,
+    ),
     new bg.PrerequisiteOs({ label: "os", accepted: ["Darwin", "Linux"] }),
-    new bg.PrerequisiteBinary({ label: "httpie", binary: bg.Binary.parse("http"), enabled }),
-    new bg.PrerequisiteBinary({ label: "sqlite3", binary: bg.Binary.parse("sqlite3"), enabled }),
-    new bg.PrerequisiteBinary({ label: "tar", binary: bg.Binary.parse("tar"), enabled }),
+    new bg.PrerequisiteBinary({ label: "httpie", binary: bg.Binary.parse("http"), enabled: production }),
+    new bg.PrerequisiteBinary({ label: "sqlite3", binary: bg.Binary.parse("sqlite3"), enabled: production }),
+    new bg.PrerequisiteBinary({ label: "tar", binary: bg.Binary.parse("tar"), enabled: production }),
+    new bg.PrerequisiteBinary({ label: "gitleaks", binary: bg.Binary.parse("gitleaks"), enabled: local }),
   ];
 }
