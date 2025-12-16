76b7bb4b9b0252e7b0a6997090f5abb8feedd46c
Bartosz Gordon <gordonbartosz@gmail.com>
2025-12-16 23:36:32 +0100

    refactor: remove log

--- infra/bootstrap.ts
+++ infra/bootstrap.ts
@@ -15,8 +15,6 @@ export async function bootstrap() {
   const EnvironmentLoader = await createEnvironmentLoader();
   const Env = await EnvironmentLoader.load();
 
-  console.log(Env);
-
   const System = createSystemAdapters(Env);
 
   const AI = createAuthAdapter(Env, System);
