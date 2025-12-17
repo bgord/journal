00f2d5fc75e830d5d29bd9313edd44ec325f0d79
Bartosz Gordon <gordonbartosz@gmail.com>
2025-12-18 00:11:25 +0100

    refactor: disable cache response

--- infra/cache-response.ts
+++ infra/cache-response.ts
@@ -1,9 +1,9 @@
 import * as bg from "@bgord/bun";
 import * as tools from "@bgord/tools";
 
-export function createCacheResponse() {
+export function createCacheResponse(): bg.CacheResponse {
   const CacheRepository = new bg.CacheRepositoryNodeCacheAdapter({ ttl: tools.Duration.Hours(1) });
   const CacheResolver = new bg.CacheResolverSimpleAdapter({ CacheRepository });
 
-  return new bg.CacheResponse({ enabled: true, subject: bg.CacheResponseSubjectUrl }, { CacheResolver });
+  return new bg.CacheResponse({ enabled: false, subject: bg.CacheResponseSubjectUrl }, { CacheResolver });
 }
