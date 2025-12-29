577cde1149e1164dd3c08043b472184130f89b98
Bartosz Gordon <gordonbartosz@gmail.com>
2025-12-29 20:36:28 +0100

    refactor: update register event handlers

--- infra/register-event-handlers.ts
+++ infra/register-event-handlers.ts
@@ -6,49 +6,48 @@ import type { BootstrapType } from "+infra/bootstrap";
 import * as Projections from "+infra/projections";
 import * as PublishingPolicies from "+publishing/policies";
 
-export function registerEventHandlers({ Adapters, Env }: BootstrapType) {
+export function registerEventHandlers({ Adapters, Env, Tools }: BootstrapType) {
+  const deps = { ...Adapters.System, ...Tools };
+
   // Projections
-  new Projections.EntryProjector(Adapters.System);
-  new Projections.AlarmProjector(Adapters.System);
-  new Projections.PatternDetectionProjector(Adapters.System);
-  new Projections.WeeklyReviewProjector(Adapters.System);
-  new Projections.ShareableLinkProjector(Adapters.System);
-  new Projections.AiUsageCounterProjector(Adapters.System);
-  new Projections.HistoryProjector({ ...Adapters.System, ...Adapters.History });
-  new Projections.ShareableLinkHitProjector(Adapters.System);
-  new Projections.PreferencesProjector(Adapters.System);
-  new Projections.ProfileAvatarsProjector(Adapters.System);
+  new Projections.EntryProjector(deps);
+  new Projections.AlarmProjector(deps);
+  new Projections.PatternDetectionProjector(deps);
+  new Projections.WeeklyReviewProjector(deps);
+  new Projections.ShareableLinkProjector(deps);
+  new Projections.AiUsageCounterProjector(deps);
+  new Projections.HistoryProjector({ ...deps, ...Adapters.History });
+  new Projections.ShareableLinkHitProjector(deps);
+  new Projections.PreferencesProjector(deps);
+  new Projections.ProfileAvatarsProjector(deps);
 
   // Policies
-  new PublishingPolicies.ShareableLinksExpirer({ ...Adapters.System, ...Adapters.Publishing });
-  new EmotionsPolicies.EntryAlarmDetector(Adapters.System);
-  new EmotionsPolicies.WeeklyReviewScheduler({ ...Adapters.System, ...Adapters.Auth });
+  new PublishingPolicies.ShareableLinksExpirer({ ...deps, ...Adapters.Publishing });
+  new EmotionsPolicies.EntryAlarmDetector(deps);
+  new EmotionsPolicies.WeeklyReviewScheduler({ ...deps, ...Adapters.Auth });
   new EmotionsPolicies.InactivityAlarmScheduler({
-    ...Adapters.System,
+    ...deps,
     UserDirectoryOHQ: Adapters.Auth.UserDirectoryOHQ,
     GetLatestEntryTimestampForUserQuery: Adapters.Emotions.GetLatestEntryTimestampForUserQuery,
   });
   new EmotionsPolicies.InactivityAlarmScheduler({
-    ...Adapters.System,
+    ...deps,
     UserDirectoryOHQ: Adapters.Auth.UserDirectoryOHQ,
     GetLatestEntryTimestampForUserQuery: Adapters.Emotions.GetLatestEntryTimestampForUserQuery,
   });
-  new EmotionsPolicies.TimeCapsuleEntriesScheduler({ ...Adapters.System, ...Adapters.Emotions });
+  new EmotionsPolicies.TimeCapsuleEntriesScheduler({ ...deps, ...Adapters.Emotions });
   new EmotionsPolicies.TimeCapsuleEntryNotifier({
-    ...Adapters.System,
+    ...deps,
     UserContactOHQ: Adapters.Auth.UserContactOHQ,
     UserLanguageOHQ: Adapters.Preferences.UserLanguageOHQ,
     EMAIL_FROM: Env.EMAIL_FROM,
   });
-  new EmotionsPolicies.EntryHistoryPublisher({ ...Adapters.System, ...Adapters.History });
-  new Preferences.Policies.SetDefaultUserLanguage<typeof SUPPORTED_LANGUAGES>(
-    SupportedLanguages.en,
-    Adapters.System,
-  );
+  new EmotionsPolicies.EntryHistoryPublisher({ ...deps, ...Adapters.History });
+  new Preferences.Policies.SetDefaultUserLanguage<typeof SUPPORTED_LANGUAGES>(SupportedLanguages.en, deps);
 
   // Sagas
   new EmotionsSagas.AlarmOrchestrator({
-    ...Adapters.System,
+    ...deps,
     AiGateway: Adapters.AI.AiGateway,
     AlarmCancellationLookup: Adapters.Emotions.AlarmCancellationLookup,
     EntrySnapshot: Adapters.Emotions.EntrySnapshot,
@@ -57,7 +56,7 @@ export function registerEventHandlers({ Adapters, Env }: BootstrapType) {
     EMAIL_FROM: Env.EMAIL_FROM,
   });
   new EmotionsSagas.WeeklyReviewProcessing({
-    ...Adapters.System,
+    ...deps,
     AiGateway: Adapters.AI.AiGateway,
     EntrySnapshot: Adapters.Emotions.EntrySnapshot,
     UserContactOHQ: Adapters.Auth.UserContactOHQ,
@@ -65,7 +64,7 @@ export function registerEventHandlers({ Adapters, Env }: BootstrapType) {
     EMAIL_FROM: Env.EMAIL_FROM,
   });
   new EmotionsSagas.WeeklyReviewExportByEmail({
-    ...Adapters.System,
+    ...deps,
     PdfGenerator: Adapters.Emotions.PdfGenerator,
     UserContactOHQ: Adapters.Auth.UserContactOHQ,
     WeeklyReviewExportQuery: Adapters.Emotions.WeeklyReviewExportQuery,
