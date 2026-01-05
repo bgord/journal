15c412c0750842e35846f0815ce32e7a0cacffcf
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-05 12:34:57 +0100

    step: entry export file pdf

--- tests/entry-export-file-pdf.test.ts
+++ tests/entry-export-file-pdf.test.ts
@@ -1,19 +1,21 @@
-import { describe, expect, test } from "bun:test";
+import { describe, expect, test, spyOn } from "bun:test";
 import * as Emotions from "+emotions";
 import { bootstrap } from "+infra/bootstrap";
 import * as mocks from "./mocks";
 
 describe("EntryExportFilePdf", async () => {
   const di = await bootstrap();
+  const file = new Emotions.Services.EntryExportFilePdf([mocks.fullEntry], {
+    ...di.Adapters.System,
+    PdfGenerator: di.Adapters.Emotions.PdfGenerator,
+  });
 
   test("generates a PDF", async () => {
-    const file = new Emotions.Services.EntryExportFilePdf([mocks.fullEntry], {
-      ...di.Adapters.System,
-      PdfGenerator: di.Adapters.Emotions.PdfGenerator,
-    });
+    const pdfGeneratorRequest = spyOn(di.Adapters.Emotions.PdfGenerator, "request");
 
     const result = await file.create();
 
     expect(result).toEqual(mocks.PDF);
+    expect(pdfGeneratorRequest).toHaveBeenCalledWith("entry_export", { entries: [mocks.fullEntry] });
   });
 });
