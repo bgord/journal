e1acfa2eeee988ce841d483a9dff47ac91c95c70
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-05 13:38:05 +0100

    step: positive emotion with maladaptive reaction pattern

--- tests/positive-emotion-with-maladaptive-reaction-pattern.test.ts
+++ tests/positive-emotion-with-maladaptive-reaction-pattern.test.ts
@@ -25,7 +25,7 @@ describe("PositiveEmotionWithMaladaptiveReactionPattern", async () => {
     });
   });
 
-  test("false", () => {
+  test("false - not enough", () => {
     const result = detector.detect({
       entries: [mocks.positiveMaladaptiveEntry, mocks.positiveMaladaptiveEntry],
       patterns: [Emotions.Services.Patterns.PositiveEmotionWithMaladaptiveReactionPattern],
@@ -36,9 +36,65 @@ describe("PositiveEmotionWithMaladaptiveReactionPattern", async () => {
     expect(result).toEqual([]);
   });
 
-  test("mixed (still false)", () => {
+  test("false - mixed", () => {
     const result = detector.detect({
       entries: [mocks.positiveMaladaptiveEntry, mocks.positiveMaladaptiveEntry, mocks.positiveAdaptiveEntry],
+      patterns: [Emotions.Services.Patterns.PositiveEmotionWithMaladaptiveReactionPattern],
+      week: mocks.week,
+      userId: mocks.userId,
+    });
+
+    expect(result).toEqual([]);
+  });
+
+  test("false - negative emotions", () => {
+    const result = detector.detect({
+      entries: [
+        mocks.positiveMaladaptiveEntry,
+        mocks.positiveMaladaptiveEntry,
+        { ...mocks.positiveMaladaptiveEntry, emotionLabel: Emotions.VO.GenevaWheelEmotion.anger },
+      ],
+      patterns: [Emotions.Services.Patterns.PositiveEmotionWithMaladaptiveReactionPattern],
+      week: mocks.week,
+      userId: mocks.userId,
+    });
+
+    expect(result).toEqual([]);
+  });
+
+  test("false - adaptive reaction", () => {
+    const result = detector.detect({
+      entries: [mocks.positiveMaladaptiveEntry, mocks.positiveMaladaptiveEntry, mocks.positiveAdaptiveEntry],
+      patterns: [Emotions.Services.Patterns.PositiveEmotionWithMaladaptiveReactionPattern],
+      week: mocks.week,
+      userId: mocks.userId,
+    });
+
+    expect(result).toEqual([]);
+  });
+
+  test("false - missing reaction", () => {
+    const result = detector.detect({
+      entries: [
+        mocks.positiveMaladaptiveEntry,
+        mocks.positiveMaladaptiveEntry,
+        { ...mocks.positiveMaladaptiveEntry, reactionType: null },
+      ],
+      patterns: [Emotions.Services.Patterns.PositiveEmotionWithMaladaptiveReactionPattern],
+      week: mocks.week,
+      userId: mocks.userId,
+    });
+
+    expect(result).toEqual([]);
+  });
+
+  test("false - missing emotion", () => {
+    const result = detector.detect({
+      entries: [
+        mocks.positiveMaladaptiveEntry,
+        mocks.positiveMaladaptiveEntry,
+        { ...mocks.positiveMaladaptiveEntry, emotionLabel: null },
+      ],
       patterns: [Emotions.Services.Patterns.PositiveEmotionWithMaladaptiveReactionPattern],
       week: mocks.week,
       userId: mocks.userId,
