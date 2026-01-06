d88e453c2e6315230f511918de7e0f56df59ae8c
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-06 23:51:43 +0100

    refactor: use new captcha naming

--- infra/tools/shield-captcha.strategy.ts
+++ infra/tools/shield-captcha.strategy.ts
@@ -3,9 +3,9 @@ import type { EnvironmentType } from "+infra/env";
 
 export function createShieldCaptcha(Env: EnvironmentType): bg.ShieldStrategy {
   return {
-    [bg.NodeEnvironmentEnum.local]: new bg.ShieldCaptchaHcaptchaLocalStrategy(Env.HCAPTCHA_SECRET_KEY),
+    [bg.NodeEnvironmentEnum.local]: new bg.ShieldHcaptchaLocalStrategy(Env.HCAPTCHA_SECRET_KEY),
     [bg.NodeEnvironmentEnum.test]: new bg.ShieldNoopStrategy(),
-    [bg.NodeEnvironmentEnum.staging]: new bg.ShieldCaptchaHcaptchaStrategy(Env.HCAPTCHA_SECRET_KEY),
+    [bg.NodeEnvironmentEnum.staging]: new bg.ShieldHcaptchaStrategy(Env.HCAPTCHA_SECRET_KEY),
     [bg.NodeEnvironmentEnum.production]: new bg.ShieldNoopStrategy(),
   }[Env.type];
 }
