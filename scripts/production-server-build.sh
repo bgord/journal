7b58ad65ad91680c4c0927da67b3f55e9d11625f
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-07 10:48:44 +0100

    refactor: remove external sharp

--- scripts/production-server-build.sh
+++ scripts/production-server-build.sh
@@ -54,7 +54,7 @@ step_end "Temporary file directory directory create"
 ./bgord-scripts/web-build-vite.sh
 
 step_start "App compile"
-bun build --compile --production --minify --sourcemap --external sharp --no-compile-autoload-dotenv index.ts --outfile "$OUTPUT_DIRECTORY"/journal
+bun build --compile --production --minify --sourcemap --no-compile-autoload-dotenv index.ts --outfile "$OUTPUT_DIRECTORY"/journal
 step_end "App compile"
 
 ./bgord-scripts/css-purge.sh
