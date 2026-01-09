ec429b0fbf7bfd5d788f5c99a73289e8f8a327c8
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-09 13:42:14 +0100

    step: make logo smaller

--- web/components/logo.tsx
+++ web/components/logo.tsx
@@ -6,7 +6,7 @@ export function Logo() {
   const t = useTranslations();
 
   return (
-    <div className="logo" data-fs="4xl" data-fw="bold" data-ls="wider" data-color="brand-600">
+    <div className="logo" data-fs="2xl" data-fw="bold" data-ls="wider" data-color="brand-600">
       <Link to="/" search={Form.default}>
         {t("app.name")}
       </Link>
