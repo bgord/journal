b95b4f4a6d23bca569b2fdbdb851900656c1b84b
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-14 17:26:14 +0100

    fix: add focus ring for logo

--- web/components/logo.tsx
+++ web/components/logo.tsx
@@ -7,7 +7,7 @@ export function Logo() {
 
   return (
     <div className="logo" data-fs="2xl" data-fw="bold" data-ls="wider" data-color="brand-600">
-      <Link to="/" search={Form.default}>
+      <Link to="/" search={Form.default} data-focus-ring="neutral">
         {t("app.name")}
       </Link>
     </div>
