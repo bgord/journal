b0febcfc1066a82f336354c93292845aae1eecdd
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-14 20:52:22 +0100

    fix: dashboard subheaders center

--- web/components/dashboard-subheader.tsx
+++ web/components/dashboard-subheader.tsx
@@ -1,3 +1,3 @@
 export function DashboardSubheader(props: React.JSX.IntrinsicElements["h2"]) {
-  return <h2 data-stack="x" data-gap="3" data-fs="base" {...props} />;
+  return <h2 data-stack="x" data-cross="center" data-gap="3" data-fs="base" {...props} />;
 }
