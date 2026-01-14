adbed577d4d722b1adf5aced515b07a88827c16f
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-14 17:43:09 +0100

    refactor: make interactive badges buttons

--- web/components/entry-emotion-label.tsx
+++ web/components/entry-emotion-label.tsx
@@ -2,14 +2,14 @@ import { useTranslations } from "@bgord/ui";
 import type { EntrySnapshotFormatted } from "../api";
 
 export function EntryEmotionLabel(
-  props: Pick<EntrySnapshotFormatted, "emotionLabel"> & React.JSX.IntrinsicElements["div"],
+  props: Pick<EntrySnapshotFormatted, "emotionLabel"> & React.JSX.IntrinsicElements["button"],
 ) {
   const t = useTranslations();
   const { emotionLabel, ...rest } = props;
 
   return (
-    <div className="c-badge" data-variant="primary" {...rest}>
+    <button className="c-badge" data-variant="primary" {...rest}>
       {t(`entry.emotion.label.value.${emotionLabel}`)}
-    </div>
+    </button>
   );
 }
--- web/components/entry-reaction-type.tsx
+++ web/components/entry-reaction-type.tsx
@@ -3,14 +3,14 @@ import type React from "react";
 import type { EntrySnapshotFormatted } from "../api";
 
 export function EntryReactionType(
-  props: Pick<EntrySnapshotFormatted, "reactionType"> & React.JSX.IntrinsicElements["div"],
+  props: Pick<EntrySnapshotFormatted, "reactionType"> & React.JSX.IntrinsicElements["button"],
 ) {
   const t = useTranslations();
   const { reactionType, ...rest } = props;
 
   return (
-    <div className="c-badge" data-variant="primary" {...rest}>
+    <button className="c-badge" data-variant="primary" {...rest}>
       {t(`entry.reaction.type.value.${reactionType}`)}
-    </div>
+    </button>
   );
 }
