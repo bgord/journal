db69865e0ee889ce9c8b13c337aba244b89fe28b
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-16 13:45:13 +0100

    step: setup tinypdf generator for local dev

--- infra/adapters/emotions/pdf-generator.adapter.ts
+++ infra/adapters/emotions/pdf-generator.adapter.ts
@@ -1,12 +1,13 @@
 import * as bg from "@bgord/bun";
 import type { EnvironmentType } from "+infra/env";
 import { PdfGeneratorReactAdapter } from "./pdf-generator-react.adapter";
+import { PdfGeneratorTinypdfAdapter } from "./pdf-generator-tinypdf.adapter";
 
 type Dependencies = { Logger: bg.LoggerPort };
 
 export function createPdfGenerator(Env: EnvironmentType, deps: Dependencies): bg.PdfGeneratorPort {
   return {
-    [bg.NodeEnvironmentEnum.local]: new PdfGeneratorReactAdapter(),
+    [bg.NodeEnvironmentEnum.local]: new PdfGeneratorTinypdfAdapter(),
     [bg.NodeEnvironmentEnum.test]: new bg.PdfGeneratorNoopAdapter(deps),
     [bg.NodeEnvironmentEnum.staging]: new bg.PdfGeneratorNoopAdapter(deps),
     [bg.NodeEnvironmentEnum.production]: new PdfGeneratorReactAdapter(),
