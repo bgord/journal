99b2b5eca295fad9c6a9fe2f54e7143a9a9e6917
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-17 22:11:32 +0100

    refactor: pass profile avatar mime registry to adapter

--- infra/adapters/system/hash-file.adapter.ts
+++ infra/adapters/system/hash-file.adapter.ts
@@ -1,5 +1,7 @@
 import * as bg from "@bgord/bun";
+import * as Preferences from "+preferences";
 
 export const HashFile = new bg.HashFileSha256BunAdapter({
   HashContent: new bg.HashContentSha256BunStrategy(),
+  MimeRegistry: Preferences.VO.ProfileAvatarMimeRegistry,
 });
--- infra/adapters/system/image-info.adapter.ts
+++ infra/adapters/system/image-info.adapter.ts
@@ -1,3 +1,6 @@
 import * as bg from "@bgord/bun";
+import * as Preferences from "+preferences";
 
-export const ImageInfo = new bg.ImageInfoSharpAdapter();
+export const ImageInfo = new bg.ImageInfoSharpAdapter({
+  MimeRegistry: Preferences.VO.ProfileAvatarMimeRegistry,
+});
