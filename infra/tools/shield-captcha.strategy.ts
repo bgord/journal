6450aacbafbbde48fbca6355363cbfffb783fbb4
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-18 16:02:13 +0100

    refactor: choose noop captcha adapter

--- infra/tools/shield-captcha.strategy.ts
+++ infra/tools/shield-captcha.strategy.ts
@@ -3,9 +3,9 @@ import type { EnvironmentType } from "+infra/env";
 
 export function createShieldCaptcha(Env: EnvironmentType): bg.ShieldStrategy {
   return {
-    [bg.NodeEnvironmentEnum.local]: new bg.ShieldHcaptchaLocalStrategy(Env.HCAPTCHA_SECRET_KEY),
+    [bg.NodeEnvironmentEnum.local]: new bg.ShieldNoopStrategy(),
     [bg.NodeEnvironmentEnum.test]: new bg.ShieldNoopStrategy(),
-    [bg.NodeEnvironmentEnum.staging]: new bg.ShieldHcaptchaStrategy(Env.HCAPTCHA_SECRET_KEY),
+    [bg.NodeEnvironmentEnum.staging]: new bg.ShieldNoopStrategy(),
     [bg.NodeEnvironmentEnum.production]: new bg.ShieldNoopStrategy(),
   }[Env.type];
 }
