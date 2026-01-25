7be9729ca4aaf95ccc21fff00f357027a9afeff5
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-25 18:04:39 +0100

    refactor: update pdf generator factory

--- infra/adapters/emotions/pdf-generator.adapter.ts
+++ infra/adapters/emotions/pdf-generator.adapter.ts
@@ -2,13 +2,18 @@ import * as bg from "@bgord/bun";
 import type { EnvironmentType } from "+infra/env";
 import { PdfGeneratorTinypdfAdapter } from "./pdf-generator-tinypdf.adapter";
 
-type Dependencies = { Logger: bg.LoggerPort };
+type Dependencies = { Logger: bg.LoggerPort; Clock: bg.ClockPort };
 
 export function createPdfGenerator(Env: EnvironmentType, deps: Dependencies): bg.PdfGeneratorPort {
+  const PdfGenerator = new bg.PdfGeneratorWithLoggerAdapter({
+    inner: new PdfGeneratorTinypdfAdapter(),
+    ...deps,
+  });
+
   return {
-    [bg.NodeEnvironmentEnum.local]: new PdfGeneratorTinypdfAdapter(),
-    [bg.NodeEnvironmentEnum.test]: new bg.PdfGeneratorNoopAdapter(deps),
-    [bg.NodeEnvironmentEnum.staging]: new bg.PdfGeneratorNoopAdapter(deps),
-    [bg.NodeEnvironmentEnum.production]: new PdfGeneratorTinypdfAdapter(),
+    [bg.NodeEnvironmentEnum.local]: PdfGenerator,
+    [bg.NodeEnvironmentEnum.test]: new bg.PdfGeneratorNoopAdapter(),
+    [bg.NodeEnvironmentEnum.staging]: new bg.PdfGeneratorNoopAdapter(),
+    [bg.NodeEnvironmentEnum.production]: PdfGenerator,
   }[Env.type];
 }
