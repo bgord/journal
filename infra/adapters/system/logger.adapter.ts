f06497c927c91f39923b36335cc4a288596a4c9a
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-26 17:28:20 +0100

    step: add logger sampling

--- infra/adapters/system/logger.adapter.ts
+++ infra/adapters/system/logger.adapter.ts
@@ -4,51 +4,45 @@ import type { EnvironmentType } from "+infra/env";
 
 type Dependencies = { Clock: bg.ClockPort };
 
-export class LoggerNoopAdapter implements bg.LoggerPort {
-  warn: bg.LoggerPort["warn"] = (_log): void => {};
-  error: bg.LoggerPort["error"] = (_log): void => {};
-  info: bg.LoggerPort["info"] = (_log): void => {};
-  http: bg.LoggerPort["http"] = (_log): void => {};
-  verbose: bg.LoggerPort["verbose"] = (_log): void => {};
-  debug: bg.LoggerPort["debug"] = (_log): void => {};
-  silly: bg.LoggerPort["silly"] = (_log): void => {};
-
-  close() {}
-
-  getStats(): bg.LoggerStatsSnapshot {
-    return { written: 0, dropped: 0, deliveryFailures: 0, state: bg.LoggerState.open };
-  }
-}
-
 export function createLogger(Env: EnvironmentType, deps: Dependencies) {
+  const app = "journal";
+
   const redactor = new bg.RedactorCompositeStrategy([
     new bg.RedactorMetadataCompactArrayStrategy({ maxItems: tools.IntegerPositive.parse(3) }),
     new bg.RedactorMaskStrategy(bg.RedactorMaskStrategy.DEFAULT_KEYS),
   ]);
-  const diagnostics = new bg.WoodchopperDiagnosticsConsoleError(redactor);
+
+  const sampling = new bg.WoodchopperSamplingComposite([
+    new bg.WoodchopperSamplingPasstrough([bg.LogLevelEnum.error, bg.LogLevelEnum.warn]),
+    new bg.WoodchoperSamplingCorrelationId(tools.IntegerPositive.parse(10)),
+    new bg.WoodchopperSamplingEveryNth(tools.IntegerPositive.parse(10)),
+  ]);
 
   return {
     [bg.NodeEnvironmentEnum.local]: new bg.Woodchopper(
       {
-        app: "journal",
+        app,
         environment: Env.type,
         level: Env.LOGS_LEVEL,
         redactor,
         dispatcher: new bg.WoodchopperDispatcherAsync(new bg.WoodchopperSinkStdoutHuman()),
-        diagnostics,
+        diagnostics: new bg.WoodchopperDiagnosticsConsoleError(),
       },
       deps,
     ),
-    [bg.NodeEnvironmentEnum.test]: new LoggerNoopAdapter(),
-    [bg.NodeEnvironmentEnum.staging]: new LoggerNoopAdapter(),
+    [bg.NodeEnvironmentEnum.test]: new bg.LoggerNoopAdapter(),
+    [bg.NodeEnvironmentEnum.staging]: new bg.LoggerNoopAdapter(),
     [bg.NodeEnvironmentEnum.production]: new bg.Woodchopper(
       {
-        app: "journal",
+        app,
         environment: Env.type,
         level: Env.LOGS_LEVEL,
         redactor,
-        dispatcher: new bg.WoodchopperDispatcherAsync(new bg.WoodchopperSinkStdout()),
-        diagnostics,
+        dispatcher: new bg.WoodchopperDispatcherSampling(
+          new bg.WoodchopperDispatcherAsync(new bg.WoodchopperSinkStdout()),
+          sampling,
+        ),
+        diagnostics: new bg.WoodchopperDiagnosticsConsoleError(redactor),
       },
       deps,
     ),
