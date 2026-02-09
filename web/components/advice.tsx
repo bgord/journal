714a03eac05754c05813e4bba57568b7167152f2
Bartosz Gordon <gordonbartosz@gmail.com>
2026-02-09 12:47:31 +0100

    refactor: simplify advice

--- web/components/advice.tsx
+++ web/components/advice.tsx
@@ -1,10 +1,10 @@
 import { Sparks } from "iconoir-react";
 
-export function Advice(props: React.JSX.IntrinsicElements["div"]) {
+export function Advice(props: React.JSX.IntrinsicElements["q"]) {
   const { children, ...rest } = props;
   return (
-    <div {...rest}>
-      <Sparks data-size="xs" data-color="brand-100" data-mr="1" /> "{children}"
-    </div>
+    <q {...rest}>
+      <Sparks data-size="xs" data-color="brand-100" data-mr="1" /> {children}
+    </q>
   );
 }
