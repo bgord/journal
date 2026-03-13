c42bf959124d0c9ce1df1cbaf9e9c7d7c51800e6
Bartosz Gordon <gordonbartosz@gmail.com>
2026-03-13 18:18:51 +0100

    step: test sse

---
+++ tests/sse.test.ts
@@ -0,0 +1,38 @@
+import { describe, expect, spyOn, test } from "bun:test";
+import * as bg from "@bgord/bun";
+import { bootstrap } from "+infra/bootstrap";
+import { registerCommandHandlers } from "+infra/register-command-handlers";
+import { registerEventHandlers } from "+infra/register-event-handlers";
+import { registerSseHandlers } from "+infra/register-sse-handlers";
+import { createServer } from "../server";
+import * as mocks from "./mocks";
+
+const url = "/api/sse";
+
+describe(`GET ${url}`, async () => {
+  const di = await bootstrap();
+  registerEventHandlers(di);
+  registerCommandHandlers(di);
+  registerSseHandlers(di);
+  const server = createServer(di);
+
+  test("validation - AccessDeniedAuthShieldError", async () => {
+    const response = await server.request(url, { method: "GET" }, mocks.ip);
+    const json = await response.json();
+
+    expect(response.status).toEqual(403);
+    expect(json).toEqual({ message: bg.ShieldAuthError.message, _known: true });
+  });
+
+  test("happy path - SseRegistry receives ALARM_GENERATED_EVENT from EventBus", async () => {
+    using sseRegistryEmit = spyOn(di.Tools.SseRegistry, "emit");
+
+    await bg.CorrelationStorage.run(mocks.correlationId, async () =>
+      di.Tools.EventBus.emit(mocks.GenericAlarmGeneratedEvent),
+    );
+
+    const identity = await di.Tools.HashContent.hash(mocks.userId);
+
+    expect(sseRegistryEmit).toHaveBeenCalledWith(identity.get(), mocks.GenericAlarmGeneratedEvent);
+  });
+});
