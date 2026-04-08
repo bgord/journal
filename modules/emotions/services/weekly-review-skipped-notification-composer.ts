39731b8c00584117e740d287d348ea8c8c417345
Bartosz Gordon <gordonbartosz@gmail.com>
2026-04-08 12:12:08 +0200

    refactor: use new date formatter

--- modules/emotions/services/weekly-review-skipped-notification-composer.ts
+++ modules/emotions/services/weekly-review-skipped-notification-composer.ts
@@ -1,20 +1,21 @@
 import * as bg from "@bgord/bun";
-import * as tools from "@bgord/tools";
+import type * as tools from "@bgord/tools";
 import * as v from "valibot";
 import type { LanguagesType } from "+languages";
+import { DateFormatter } from "../../../df";
 
 const notification: Record<LanguagesType, (week: tools.Week) => bg.MailerTemplateMessage> = {
   en: (week: tools.Week) => ({
     subject: v.parse(
       bg.MailerSubject,
-      `JOURNAL - weekly review ${tools.DateFormatters.date(week.getStart().ms)} - ${tools.DateFormatters.date(week.getEnd().ms)}`,
+      `JOURNAL - weekly review ${DateFormatter.date(week.getStart())} - ${DateFormatter.date(week.getEnd())}`,
     ),
     html: v.parse(bg.MailerContentHtml, "Come back and journal"),
   }),
   pl: (week: tools.Week) => ({
     subject: v.parse(
       bg.MailerSubject,
-      `JOURNAL - przegląd tygodnia ${tools.DateFormatters.date(week.getStart().ms)} - ${tools.DateFormatters.date(week.getEnd().ms)}`,
+      `JOURNAL - przegląd tygodnia ${DateFormatter.date(week.getStart())} - ${DateFormatter.date(week.getEnd())}`,
     ),
     html: v.parse(bg.MailerContentHtml, "Wróć do nas"),
   }),
