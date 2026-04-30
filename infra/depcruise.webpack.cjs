c4fca9b4b805d97434bd2c3ed86a2d1250dc3e5f
Bartosz Gordon <gordonbartosz@gmail.com>
2026-04-30 23:50:36 +0200

    step: update depcruiser

--- infra/depcruise.webpack.cjs
+++ infra/depcruise.webpack.cjs
@@ -5,13 +5,15 @@ module.exports = {
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".jsx"],
     alias: {
-      "+app": path.resolve(__dirname, "app"),
       "+infra": path.resolve(__dirname, "infra"),
-      "+ai": path.resolve(__dirname, "modules/ai"),
+      "+app": path.resolve(__dirname, "app"),
+      "+languages": path.resolve(__dirname, "modules/languages.ts"),
       "+auth": path.resolve(__dirname, "modules/auth"),
       "+emotions": path.resolve(__dirname, "modules/emotions"),
       "+publishing": path.resolve(__dirname, "modules/publishing"),
-      "+languages": path.resolve(__dirname, "modules/supported-languages.ts"),
+      "+ai": path.resolve(__dirname, "modules/ai"),
+      "+system": path.resolve(__dirname, "modules/system"),
+      "+preferences": path.resolve(__dirname, "modules/preferences"),
     },
   },
 };
