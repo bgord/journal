d334311413eb421a8d1a4694837ab3f77b0729b9
Bartosz Gordon <gordonbartosz@gmail.com>
2026-05-01 00:15:17 +0200

    fix: e2e spec

--- infra/e2e/home.spec.ts
+++ infra/e2e/home.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from "@playwright/test";
 
 test("Sign in - layout", async ({ page }) => {
-  await page.goto("/login");
+  await page.goto("/public/login.html");
 
   const header = page.getByText("Journal");
   await expect(header).toBeVisible();
